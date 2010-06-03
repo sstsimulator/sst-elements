428c7f1936a9ffc2d0e9459b7b8921469034ef34
Rolf Riesen <rolf.riesen@intel.com>
2010-06-03 20:00:05 +0000

    Disable some debugging output

--- sst/elements/nicmodel/netsim_model.cc
+++ sst/elements/nicmodel/netsim_model.cc
@@ -1,7 +1,7 @@
 #include <arpa/inet.h>		// For endian conversion
-#include <sst/core/cpunicEvent.h>
+#include <sst/cpunicEvent.h>
 #include "netsim_model.h"
-#include "user_includes/netsim/netsim_internal.h"
+#include "../../user_includes/netsim/netsim_internal.h"
 
 
 
