9b7763cc96d2c0bb4ac2d38716ae1ec2264947d5
Rolf Riesen <rolf.riesen@intel.com>
2010-07-16 20:33:36 +0000

    Start an example of a communication pattern generator.

---
+++ sst/elements/patterns/ghost.cc
@@ -0,0 +1,79 @@
+// Copyright 2009-2010 Sandia Corporation. Under the terms
+// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+// Government retains certain rights in this software.
+// 
+// Copyright (c) 2009-2010, Sandia Corporation
+// All rights reserved.
+// 
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+
+#include <sst_config.h>
+#include "sst/core/serialization/element.h"
+
+#include "ghost.h"
+#include <sst/core/cpunicEvent.h>
+
+int ghost_pattern_debug;
+
+typedef enum {COMPUTE, WAIT, DONE} state_t;
+typedef enum {COMPUTE_DONE, RECEIVE, FAIL, RESEND_MSG} pattern_event_t;
+
+bool
+Ghost_pattern::handle_events(Event *sst_event)
+{
+
+static state_t state= COMPUTE;
+pattern_event_t event;
+
+
+    _GHOST_PATTERN_DBG(3, "Rank %d got an event\n", my_rank);
+
+    /* Extract the pattern event type from the SST event */
+    event= COMPUTE_DONE;
+
+    switch (state)   {
+	case COMPUTE:
+	    switch (event)   {
+		case COMPUTE_DONE:
+		    /* Our time to compute is over */
+		    break;
+		case RECEIVE:
+		    /* We got a message from another rank */
+		    break;
+		case FAIL:
+		    /* We just failed. Deal with it! */
+		    break;
+		case RESEND_MSG:
+		    /* We have been ask to resend a previous msg to help another rank to recover */
+		    break;
+	    }
+	    break;
+
+	case WAIT:
+	    break;
+
+	case DONE:
+	    /* This rank has done all of its work */
+	    return true;
+	    break;
+    }
+
+    return false;
+
+}  /* end of handle_port_events() */
+
+
+
+extern "C" {
+Ghost_pattern *
+ghost_patternmodelAllocComponent(SST::ComponentId_t id,
+                          SST::Component::Params_t& params)
+{
+    return new Ghost_pattern(id, params);
+}
+}
+
+BOOST_CLASS_EXPORT(Ghost_pattern)
---
+++ sst/elements/patterns/ghost.h
@@ -0,0 +1,138 @@
+// Copyright 2009-2010 Sandia Corporation. Under the terms
+// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+// Government retains certain rights in this software.
+// 
+// Copyright (c) 2009-2010, Sandia Corporation
+// All rights reserved.
+// 
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+
+#ifndef _GHOST_PATTERN_H
+#define _GHOST_PATTERN_H
+
+#include <string.h>
+#include <sst/core/eventFunctor.h>
+#include <sst/core/component.h>
+#include <sst/core/link.h>
+
+
+using namespace SST;
+
+#define DBG_GHOST_PATTERN 1
+#if DBG_GHOST_PATTERN
+#define _GHOST_PATTERN_DBG(lvl, fmt, args...)\
+    if (ghost_pattern_debug >= lvl)   { \
+	printf("%d:Ghost_pattern::%s():%d: " fmt, _debug_rank, __FUNCTION__, __LINE__, ## args); \
+    }
+#else
+#define _GHOST_PATTERN_DBG(lvl, fmt, args...)
+#endif
+
+extern int ghost_pattern_debug;
+
+class Ghost_pattern : public Component {
+    public:
+        Ghost_pattern(ComponentId_t id, Params_t& params) :
+            Component(id),
+            params(params)
+        { 
+
+            Params_t::iterator it= params.begin();
+	    // Defaults
+	    ghost_pattern_debug= 0;
+
+            while (it != params.end())   {
+                _GHOST_PATTERN_DBG(1, "Ghost: key=%s value=%s\n", it->first.c_str(), it->second.c_str());
+
+		if (!it->first.compare("debug"))   {
+		    sscanf(it->second.c_str(), "%d", &ghost_pattern_debug);
+		}
+
+		if (!it->first.compare("rank"))   {
+		    sscanf(it->second.c_str(), "%d", &my_rank);
+		}
+
+		if (!it->first.compare("x_dim"))   {
+		    sscanf(it->second.c_str(), "%d", &x_dim);
+		}
+
+		if (!it->first.compare("y_dim"))   {
+		    sscanf(it->second.c_str(), "%d", &y_dim);
+		}
+
+                ++it;
+            }
+
+
+            // Create a handler for events
+            myHandler= new EventHandler<Ghost_pattern, bool, Event *>
+                (this, &Ghost_pattern::handle_events);
+
+	    net= LinkAdd("Network", myHandler);
+	    if (net == NULL)   {
+		_GHOST_PATTERN_DBG(0, "The ghost pattern generator expects a link to the network "
+		    "named \"Network\" which is missing!\n");
+		_ABORT(Ghost_pattern, "Check the input XML file!\n");
+	    }
+
+	    // Create a time converter
+	    tc= registerTimeBase("1ns", true);
+        }
+
+    private:
+
+        Ghost_pattern(const Ghost_pattern &c);
+	bool handle_events(Event *);
+	EventHandler_t *myHandler;
+
+	int my_rank;
+	int x_dim;
+	int y_dim;
+
+        Params_t params;
+	Link *net;
+	TimeConverter *tc;
+
+        friend class boost::serialization::access;
+        template<class Archive>
+        void serialize(Archive & ar, const unsigned int version )
+        {
+            ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Component);
+	    ar & BOOST_SERIALIZATION_NVP(myHandler);
+	    ar & BOOST_SERIALIZATION_NVP(params);
+	    ar & BOOST_SERIALIZATION_NVP(my_rank);
+	    ar & BOOST_SERIALIZATION_NVP(x_dim);
+	    ar & BOOST_SERIALIZATION_NVP(y_dim);
+	    ar & BOOST_SERIALIZATION_NVP(tc);
+        }
+
+        template<class Archive>
+        friend void save_construct_data(Archive & ar, 
+                                        const Ghost_pattern * t,
+                                        const unsigned int file_version)
+        {
+            _AR_DBG(Ghost_pattern,"\n");
+            ComponentId_t     id     = t->_id;
+            Params_t          params = t->params;
+            ar << BOOST_SERIALIZATION_NVP(id);
+            ar << BOOST_SERIALIZATION_NVP(params);
+        } 
+
+        template<class Archive>
+        friend void load_construct_data(Archive & ar, 
+                                        Ghost_pattern * t, 
+                                        const unsigned int file_version)
+        {
+            _AR_DBG(Ghost_pattern,"\n");
+            ComponentId_t     id;
+            Params_t          params;
+            ar >> BOOST_SERIALIZATION_NVP(id);
+            ar >> BOOST_SERIALIZATION_NVP(params);
+            ::new(t)Ghost_pattern(id, params);
+        } 
+};
+
+#endif
