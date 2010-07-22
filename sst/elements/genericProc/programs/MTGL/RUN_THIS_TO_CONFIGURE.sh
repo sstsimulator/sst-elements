dcfa80f9a38b369212d2af9dd5f8a116bab67eac
Kyle Wheeler <kbwheel@gmail.com>
2010-07-22 16:14:12 +0000

    helper script

---
+++ sst/elements/genericProc/programs/MTGL/RUN_THIS_TO_CONFIGURE.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+cprops=""
+qthreads=""
+while [ "$1" ] ; do
+	case "$1" in
+		--with-cprops=*)
+			cprops="$1"
+			shift
+			;;
+		--with-qthreads=*)
+			qthreads="$1"
+			shift
+			;;
+		--sstenv=*)
+			sstenv=${1#--sstenv=}
+			cprops="--with-cprops=$sstenv"
+			qthreads="--with-qthreads=$sstenv"
+			shift
+			;;
+		*)
+			break
+			;;
+	esac
+done
+if [ -z "$cprops" -o -z "$qthreads" ] ; then
+	echo "Need to specify both --with-cprops=/path/to/sst/cprops and"
+	echo "--with-qthreads=/path/to/sst/qthreads. Both of those are"
+	echo "probably the same, and if so, you can just use --sstenv=/path/to/sst/env"
+	echo
+	echo "Thus, an example commandline would be:"
+	echo '   ./RUN_THIS_TO_CONFIGURE.sh --sstenv=$HOME/sstenv'
+	exit
+fi
+./configure \
+	--host=powerpc-sst-darwin8.11.0 \
+	$cprops \
+	$qthreads \
+	"$@"
