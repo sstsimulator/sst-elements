b613dfd82115bec34729a0fc2ee19176fa770f10
Genie Heiseh <unknown@sst-simulator.org>
2010-08-04 15:50:16 +0000

    =cpu_data/myMemEvent.h added

---
+++ sst/elements/cpu_data/myMemEvent.h
@@ -0,0 +1,36 @@
+
+#ifndef _MYMEMEVENT_H
+#define _MYMEMEVENT_H
+
+#include <sst/core/event.h>
+
+namespace SST {
+
+class MyMemEvent : public Event {
+    public:
+        typedef enum { MEM_LOAD, MEM_LOAD_RESP, 
+                MEM_STORE, MEM_STORE_RESP } Type_t;
+        MyMemEvent() : Event() { }
+
+        unsigned long   address;
+        // this should be Type_t but SERIALIZATION barfs on it
+        int             type;
+        uint64_t        tag;
+    
+    private:
+	
+        friend class boost::serialization::access;
+        template<class Archive>
+        void serialize(Archive & ar, const unsigned int version )
+        {
+            ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Event);
+            ar & BOOST_SERIALIZATION_NVP( address );
+            ar & BOOST_SERIALIZATION_NVP( type );
+            ar & BOOST_SERIALIZATION_NVP( tag );
+        }
+};
+
+    
+} //namespace SST
+
+#endif
