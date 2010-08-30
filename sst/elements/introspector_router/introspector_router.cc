8d77528c45d74f4b7a8fdf1ef5bdabb231ef7b4d
Genie Heiseh <unknown@sst-simulator.org>
2010-08-30 20:30:16 +0000

    =added introspector that pulls power&delay from routermodel

---
+++ sst/elements/introspector_router/introspector_router.cc
@@ -0,0 +1,169 @@
+// Copyright 2009-2010 Sandia Corporation. Under the terms
+// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+// Government retains certain rights in this software.
+// 
+// Copyright (c) 2009-2010, Sandia Corporation
+// All rights reserved.
+// 
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#include <sst_config.h>
+#include "sst/core/serialization/element.h"
+
+#include <boost/mpi.hpp>
+
+#include "introspector_router.h"
+#include "sst/core/element.h"
+
+
+bool Introspector_router::pullData( Cycle_t current )
+{
+    //_INTROSPECTOR_DBG("id=%lu currentCycle=%lu \n", Id(), current );
+	IntrospectedComponent *c;
+  	totalRouterDelay = 0;
+	currentPower = leakagePower = runtimePower = totalPower = peakPower = 0;
+
+  	printf("introspector_router pulls data @ cycle %ld\n", (long int)current ); //current here specifies it's the #th call
+	for( Database_t::iterator iter = DatabaseInt.begin();
+                            iter != DatabaseInt.end(); ++iter )
+        {
+	    c = iter->second;
+	    if (iter->first == IntrospectedComponent::router_delay)
+	    {
+                //std::cout << "Pull data of component ID " << c->getId() << " with dataID = " << iter->first << " and data value = " << c->getIntData(iter->first) << std::endl;
+	        totalRouterDelay += c->getIntData(iter->first);
+	    }
+	}
+	std::cout << "introspector_router: @ cycle " << current << ", TOTAL router delay = " << totalRouterDelay << std::endl; 
+	
+	for( Database_t::iterator iter = DatabaseDouble.begin();
+                            iter != DatabaseDouble.end(); ++iter )
+    	{
+            c = iter->second;
+	    if (iter->first == IntrospectedComponent::current_power)
+	    {
+                //std::cout << "Pull data of component ID " << c->getId() << " with dataID = " << iter->first << " and data value = " << c->getDoubleData(iter->first) << std::endl;
+		currentPower += c->getDoubleData(iter->first);
+	    }
+	    if (iter->first == IntrospectedComponent::leakage_power)
+	    {
+		leakagePower += c->getDoubleData(iter->first);
+	    }
+	    if (iter->first == IntrospectedComponent::runtime_power)
+	    {
+		runtimePower += c->getDoubleData(iter->first);
+	    }
+	    if (iter->first == IntrospectedComponent::total_power)
+	    {
+		totalPower += c->getDoubleData(iter->first);
+	    }
+	    if (iter->first == IntrospectedComponent::peak_power)
+	    {
+		if (peakPower < c->getDoubleData(iter->first))  
+		    peakPower = c->getDoubleData(iter->first);
+	    }
+	}
+	std::cout << "introspector_router: @ cycle " << current << ", TOTAL current power = " << currentPower << std::endl; 
+	std::cout << "introspector_router: @ cycle " << current << ", TOTAL leakage power = " << leakagePower << std::endl;
+	std::cout << "introspector_router: @ cycle " << current << ", TOTAL runtime power = " << runtimePower << std::endl;
+	std::cout << "introspector_router: @ cycle " << current << ", TOTAL total power = " << totalPower << std::endl;
+	std::cout << "introspector_router: @ cycle " << current << ", peak power = " << peakPower << std::endl;
+
+	return false;
+}
+
+
+bool Introspector_router::mpiCollectInt( Cycle_t current )
+{
+	boost::mpi::communicator world;
+
+	
+	collectInt(REDUCE, intData, MINIMUM);
+	collectInt(REDUCE, intData, MAXIMUM);
+	//collectInt(ALLREDUCE, intData, SUM);
+        collectInt(BROADCAST, intData, NOT_APPLICABLE, 1); // rank 1 will broadcast the value 
+	collectInt(GATHER, intData, NOT_APPLICABLE);
+	//collectInt(ALLGATHER, intData, NOT_APPLICABLE);
+
+	if (world.rank() == 0){
+	    std::cout << " The minimum value of data is " << minvalue << std::endl;
+	    std::cout << " The maximum value of data is " << maxvalue << std::endl;
+
+	    std::cout << "Gather data into vector: ";
+   	    for(unsigned int ii=0; ii < arrayvalue.size(); ii++)
+            {
+                 std::cout << arrayvalue[ii] << " ";
+            }
+	    std::cout << std::endl;
+	}
+	//std::cout << " The sum of data is " << value << std::endl;
+	std::cout << " The value of the broadcast data is " << value << std::endl;
+	/*std::cout << "All_gather data into vector: ";
+   	for(int ii=0; ii < arrayvalue.size(); ii++)
+        {
+             std::cout << arrayvalue[ii] << " ";
+        }
+	std::cout << std::endl;*/
+
+	return (false);
+}
+
+//An example MPI collect functor that is put into the queue.
+//Introspector-writer implements their own MPI functor and pass that
+//to Introspector::oneTimeCollect().
+void Introspector_router::mpiOneTimeCollect( Event* e)
+{
+//option 1: utilize the built-in basic collective communication calls
+/*	boost::mpi::communicator world;
+
+	collectInt(REDUCE, intData, MINIMUM);
+
+	if (world.rank() == 0){
+	    std::cout << "One Time Collect:: The minimum value of data is " << minvalue << std::endl;
+	}
+*/
+
+//option2: implement it on your own 
+	boost::mpi::communicator world;
+
+	
+	if (world.rank() == 0){
+	    reduce( world, intData, maxvalue, boost::mpi::maximum<int>(), 0); 
+    	    std::cout << "One Time Collect: The maximum value is " << maxvalue << std::endl;
+  	} else {
+    	    reduce(world, intData, boost::mpi::maximum<int>(), 0);
+  	} 
+	return;
+}
+
+
+static Introspector*
+create_introspector_router(SST::Component::Params_t &params)
+{
+    return new Introspector_router(params);
+}
+
+static const ElementInfoIntrospector introspectors[] = {
+    { "introspector_router",
+      "ROUTER Introspector",
+      NULL,
+      create_introspector_router,
+    },
+    { NULL, NULL, NULL, NULL }
+};
+
+extern "C" {
+    ElementLibraryInfo introspector_router_eli =  {
+        "introspector_router",
+        "ROUTER Introspector",
+        NULL,
+        NULL,
+        introspectors
+    };
+}
+
+
+BOOST_CLASS_EXPORT(Introspector_router)
+
---
+++ sst/elements/introspector_router/introspector_router.h
@@ -0,0 +1,145 @@
+// Copyright 2009-2010 Sandia Corporation. Under the terms
+// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+// Government retains certain rights in this software.
+// 
+// Copyright (c) 2009-2010, Sandia Corporation
+// All rights reserved.
+// 
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#ifndef _INTROSPECTOR_ROUTER_H
+#define _INTROSPECTOR_ROUTER_H
+
+#include <sst/core/introspector.h>
+
+
+using namespace SST;
+
+#if DBG_INTROSPECTOR_ROUTER
+#define _INTROSPECTOR_ROUTER_DBG( fmt, args...)\
+         printf( "%d:Introspector_router::%s():%d: "fmt, _debug_rank, __FUNCTION__,__LINE__, ## args )
+#else
+#define _INTROSPECTOR_ROUTER_DBG( fmt, args...)
+#endif
+
+
+
+class Introspector_router : public Introspector {
+       
+    public:
+	Introspector_router(Component::Params_t& params ) :
+            Introspector(),
+            params( params ),
+            frequency( "1ns" )
+        {
+            _INTROSPECTOR_ROUTER_DBG( "new id=%lu\n", id );
+
+            Component::Params_t::iterator it = params.begin(); 
+            while( it != params.end() ) { 
+                _INTROSPECTOR_ROUTER_DBG("key=%s value=%s\n",
+                            it->first.c_str(),it->second.c_str());
+                if ( ! it->first.compare("period") ) {
+		    frequency = it->second;
+                }  
+		else if ( ! it->first.compare("model") ) {
+		    model = it->second;
+                }    
+                ++it;
+            } 
+            
+            totalRouterDelay = 0;
+	    currentPower = leakagePower = runtimePower = totalPower = peakPower = 0;
+
+            _INTROSPECTOR_ROUTER_DBG("-->frequency=%s\n",frequency.c_str());
+
+	    TimeConverter* tc = registerClock( frequency, new Clock::Handler<Introspector_router>(this, &Introspector_router::pullData) );
+
+	    ///registerClock( frequency, new Clock::Handler<Introspector_router>(this, &Introspector_router::mpiCollectInt) );
+	    ///mpionetimehandler = new Event::Handler< Introspector_router >
+	    ///                                    ( this, &Introspector_router::mpiOneTimeCollect );
+
+	    printf("INTROSPECTOR_ROUTER period: %ld\n",(long int)tc->getFactor());
+            _INTROSPECTOR_ROUTER_DBG("Done registering clock\n");
+            
+        }
+        int Setup() {
+	    std::pair<bool, int> pint, pdouble;
+	    
+	    //get a list of relevant component. Must be done after all components are created 
+	    MyCompList = getModels(model); 
+	    //std::cout << " introspector_router has MyCompList size = " << MyCompList.size() << std::endl;
+	    for (std::list<IntrospectedComponent*>::iterator i = MyCompList.begin();
+	        i != MyCompList.end(); ++i) {
+     		    // state that we will monitor those components 
+		    // (pass introspector's info to the component)
+     		    monitorComponent(*i);
+
+		    //check if the component counts the specified int/double data
+		    pint = (*i)->ifMonitorIntData("router_delay");
+		    if(pint.first){		
+			//store pointer to component and the dataID of the data of interest	
+			addToIntDatabase(*i, pint.second); 
+		    }
+		
+		    pdouble = (*i)->ifMonitorDoubleData("current_power");
+		    if(pdouble.first){		
+			addToDoubleDatabase(*i, pdouble.second); 
+		    }
+		    pdouble = (*i)->ifMonitorDoubleData("leakage_power");
+		    if(pdouble.first){		
+			addToDoubleDatabase(*i, pdouble.second); 
+		    }
+		    pdouble = (*i)->ifMonitorDoubleData("runtime_power");
+		    if(pdouble.first){		
+			addToDoubleDatabase(*i, pdouble.second); 
+		    }
+		    pdouble = (*i)->ifMonitorDoubleData("total_power");
+		    if(pdouble.first){		
+			addToDoubleDatabase(*i, pdouble.second); 
+		    }
+		    pdouble = (*i)->ifMonitorDoubleData("peak_power");
+		    if(pdouble.first){		
+			addToDoubleDatabase(*i, pdouble.second); 
+		    }
+		
+	     }
+	    ///oneTimeCollect(2000000, mpionetimehandler);
+            _INTROSPECTOR_ROUTER_DBG("\n");
+            return 0;
+        }
+        int Finish() {
+            _INTROSPECTOR_ROUTER_DBG("\n");
+            return 0;
+        }
+
+
+    private:
+        Introspector_router( const Introspector_router& c );
+	//~Introspector_router();
+	Introspector_router() :  Introspector() {} // for serialization only
+
+        bool pullData( Cycle_t );
+	bool mpiCollectInt( Cycle_t );
+	void mpiOneTimeCollect(Event* e);
+
+	Event::Handler< Introspector_router > *mpionetimehandler;
+        Component::Params_t    params;        
+	std::string frequency;
+	std::string model;
+	uint64_t totalRouterDelay, intData;
+	double currentPower, leakagePower, runtimePower, totalPower, peakPower;
+
+	friend class boost::serialization::access;
+    template<class Archive>
+    void serialize(Archive & ar, const unsigned int version )
+    {
+        ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Introspector);
+        ar & BOOST_SERIALIZATION_NVP(params);
+        ar & BOOST_SERIALIZATION_NVP(frequency);
+	ar & BOOST_SERIALIZATION_NVP(model);
+    }
+};
+
+#endif
