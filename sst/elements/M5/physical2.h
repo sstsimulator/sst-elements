45eea0a28d5079e33da81cd6e5617b0d84aaac85
Mike Levenhagen <mjleven@sandia.gov>
2011-04-01 18:34:28 +0000

    wrap M5::PhysicalMemory and overload init()

---
+++ sst/elements/M5/physical2.h
@@ -0,0 +1,15 @@
+
+#ifndef _physical2_h
+#define _physical2_h
+
+#include <mem/physical.hh>
+
+class PhysicalMemory2 : public PhysicalMemory 
+{
+  public:
+    PhysicalMemory2( const Params* p ) : PhysicalMemory( p ) {} 
+    // PhyscialMemory panics in init() if it is not connected
+    void init() {}
+};
+
+#endif
