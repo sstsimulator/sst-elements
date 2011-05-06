cfcc5145052dde247002aa1f0acc544639cd3a32
K. Scott Hemmert <kshemme@sandia.gov>
2011-05-06 13:28:42 +0000

    Added trig_cpu_event.h

---
+++ sst/elements/portals4_sm/trig_cpu/trig_cpu_event.h
@@ -0,0 +1,51 @@
+
+#ifndef _TRIG_CPU_EVENT_H
+#define _TRIG_CPU_EVENT_H
+
+#include <sst/core/event.h>
+
+namespace SST {
+    
+    
+    class trig_cpu_event : public Event {
+    public:
+
+	trig_cpu_event() : Event() {
+	    canceled = false;
+	    timeout = false;
+	}
+
+	trig_cpu_event(bool timeout) : Event(), timeout(timeout) {
+	    canceled = false;
+	}
+	
+        ~trig_cpu_event() {}
+
+	void cancel() {
+	    if (timeout) {
+		canceled = true;
+	    }
+	    else {
+		printf("Non-timeout trig_cpu_event cannot be canceled\n");
+		abort();
+	    }
+	}
+
+	bool isCanceled() {
+	    return canceled;
+	}
+
+	bool isTimeout() {
+	    return timeout;
+	}
+	
+    private:
+	bool canceled;
+	bool timeout;
+	
+    };
+
+    
+} //namespace SST
+
+#endif
