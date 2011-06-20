b20c5a129298d3d8b1e66a9b489c66c2d1997c08
Mike Levenhagen <mjleven@sandia.gov>
2011-06-20 20:59:03 +0000

    new tests

--- sst/elements/M5/app/portals4Tests/get.c
+++ sst/elements/M5/app/portals4Tests/get.c
@@ -4,19 +4,16 @@
 #include <unistd.h>
 #include <portals4.h>
 #include <m5rt.h>
+#include <assert.h>
 
 static void nid0( ptl_handle_ni_t, ptl_process_t* );
 static void nid1( ptl_handle_ni_t, ptl_process_t* );
-
-#define SYS_virt2phys 442
-
-static inline unsigned long virt2phys( void* addr ) {
-    return (unsigned long) syscall( SYS_virt2phys, addr );
-}    
+static void printEvent( ptl_event_t *event );
 
 //#define MSG_SIZE 0x2000 
-#define MSG_SIZE  100
+#define MSG_SIZE  200
 #define PTL_INDEX 10
+#define OFFSET_DIV 4 
 ptl_match_bits_t match_bits = 0xdeadbeef;
 
 static int rank;
@@ -72,59 +69,71 @@ int main( int argc, char* argv[] )
     PtlFini();
 
     printf("goodbye\n");
+    m5_barrier();
     return 0;
 }
 
 static void nid0( ptl_handle_ni_t ni_handle, ptl_process_t* id )
 {
+    int retval;
     ptl_handle_md_t md_handle;
     ptl_md_t md;
     ptl_process_t   target_id = *id;
-    printf("%s():%d ni_handle=%#lx\n",__func__,__LINE__,(unsigned long)ni_handle);
+    ptl_event_t event;
+    printf("%s():%d ni_handle=%#lx\n",__func__,__LINE__,
+                                    (unsigned long)ni_handle);
 
     md.start = malloc(MSG_SIZE);
     md.length = MSG_SIZE;
-    md.options = PTL_MD_EVENT_CT_SEND; 
-    md.eq_handle = PTL_EQ_NONE;
+    md.options = 0;
+    md.ct_handle = PTL_CT_NONE;
 
-
-    if ( PtlCTAlloc( ni_handle, &md.ct_handle ) != PTL_OK ) {
-        printf("PtlCTAlloc() failed\n"); abort();
-    } 
+    if ( ( retval = PtlEQAlloc( ni_handle, 2, &md.eq_handle ) ) != PTL_OK ) {
+        printf("PtlEQAlloc() failed %d\n",retval); abort();
+    }
 
     if ( PtlMDBind( ni_handle, &md, &md_handle ) != PTL_OK ) {
         printf("PtlMDBind() failed\n"); abort();
     } 
     target_id.phys.nid = 1;
-
     
     m5_barrier();
 
-    if ( PtlGet( md_handle, 0, MSG_SIZE, target_id, PTL_INDEX,
-                        match_bits, 0, NULL ) ) { 
+    ptl_size_t offset = md.length - ( md.length / OFFSET_DIV);
+    ptl_size_t length = md.length - offset;
+
+    if ( PtlGet( md_handle, offset, length, target_id, PTL_INDEX,
+                        match_bits, offset, &md ) ) { 
         printf("PtlGet() failed\n"); abort();
     } 
 
-    ptl_ct_event_t ct_event;
-    if ( PtlCTWait( md.ct_handle, 1, &ct_event) != PTL_OK ) {
-        printf("PtlCTWait() failed\n"); abort();
-    } 
+    if ( ( retval = PtlEQWait( md.eq_handle, &event ) ) != PTL_OK ) {
+        printf("PtlEQWait() failed %d\n",retval); abort();
+    }
+    //printEvent( &event );
+    assert( event.type == PTL_EVENT_REPLY );
+    assert( event.mlength == length ); 
+    assert( event.user_ptr == &md ); 
+    assert( event.ni_fail_type == PTL_NI_OK );
+    printf("got PTL_EVENT_REPLY, remote_offset=%lu\n", event.remote_offset);
 
     int i;
-    for ( i = 0; i < md.length; i++ ) {
-        if ( ((unsigned char*)md.start)[i] != i % 256) {
-            printf("%d != %d\n",i%256,((unsigned char*)md.start)[i]);
+    unsigned char* start = md.start + event.remote_offset;
+    for ( i = 0; i < event.mlength; i++ ) {
+        unsigned int tmp = (i + event.remote_offset ) % 256;
+        unsigned char c = start[ i ];
+        if ( c !=  tmp ) {
+            printf("%d != %d\n", tmp, c );
         }
     }
 
-    printf("ct_event sucess %d failure %d\n",
-                        (int)ct_event.success, (int)ct_event.failure);
     if ( PtlMDRelease( md_handle ) != PTL_OK ) {
         printf("PtlMDRelease() failed\n"); abort();
     } 
-    if ( PtlCTFree( md.ct_handle ) != PTL_OK ) {
-        printf("PtlCTFree() failed\n"); abort();
-    } 
+
+    if ( (retval = PtlEQFree( md.eq_handle ) ) != PTL_OK ) {
+        printf("PtlEQFree() failed %d\n",retval); abort();
+    }
 
     printf("%s():%d\n",__func__,__LINE__);
 }
@@ -155,8 +164,8 @@ static void nid1( ptl_handle_ni_t ni_handle, ptl_process_t* id )
     me.length = MSG_SIZE;
     me.ct_handle = PTL_CT_NONE;
     me.min_free = 0;
-    //me.ac = {}; 
-    me.options = PTL_ME_OP_PUT;
+    me.options = PTL_ME_OP_GET | PTL_ME_MANAGE_LOCAL;
+//    me.options = PTL_ME_OP_GET;
     me.match_id.phys.nid = 0;
     me.match_id.phys.pid = 1;
     me.match_bits = match_bits;
@@ -171,7 +180,7 @@ static void nid1( ptl_handle_ni_t ni_handle, ptl_process_t* id )
                         pt_index,
                         &me,
                         PTL_PRIORITY_LIST,
-                        NULL,
+                        &me,
                         &me_handle) ) != PTL_OK ) {
         printf("PtlMEAppend() failed %d\n",retval); abort();
     }
@@ -182,6 +191,18 @@ static void nid1( ptl_handle_ni_t ni_handle, ptl_process_t* id )
         printf("PtlEQWait() failed %d\n",retval); abort();
     }
 
+    //printEvent(&event);
+    assert( event.type == PTL_EVENT_GET );
+    assert( event.initiator.phys.nid == 0 );
+    assert( event.initiator.phys.pid == 1 );
+    assert( event.pt_index == pt_index );
+    assert( event.start == me.start + event.remote_offset );
+    assert( event.user_ptr == &me );
+    assert( event.mlength == me.length / OFFSET_DIV ); 
+    assert( event.user_ptr == &me ); 
+    assert( event.ni_fail_type == PTL_NI_OK );
+    printf("got PTL_EVENT_GET, remote_offset=%lu\n",
+                                            event.remote_offset);
 
     if ( ( retval = PtlMEUnlink( me_handle ) ) != PTL_OK ) {
         printf("PtlMEFree() failed %d\n",retval); abort();
@@ -196,3 +217,24 @@ static void nid1( ptl_handle_ni_t ni_handle, ptl_process_t* id )
         printf("PtlEQFree() failed %d\n",retval); abort();
     }
 }    
+
+static void printEvent( ptl_event_t *event )
+{
+    printf("type=%d\n",event->type);
+    printf("initiator.phys=%d:%d\n",event->initiator.phys.nid,
+                                event->initiator.phys.pid);
+    printf("pt_index=%d\n",event->pt_index);
+    printf("uid=%d\n",event->uid);
+    printf("jid=%d\n",event->jid);
+    printf("match_bits=%#lx\n",event->match_bits);
+    printf("rlength=%lu\n",event->rlength);
+    printf("mlength=%lu\n",event->mlength);
+    printf("remote_offset=%lu\n",event->remote_offset);
+    printf("start=%p\n",event->start);
+    printf("user_ptr=%p\n",event->user_ptr);
+    printf("hdr_data=%#lx\n",event->hdr_data);
+    printf("ni_fail_type=%d\n",event->ni_fail_type);
+    printf("atomic_operation=%d\n",event->atomic_operation);
+    printf("atomic_type=%d\n",event->atomic_type);
+}
+
