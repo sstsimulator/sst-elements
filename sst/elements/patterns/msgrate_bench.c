fad435338da7c2cc50f9b1b77b3c7ec3dd7b559a
Rolf Riesen <rolf.riesen@intel.com>
2011-09-15 10:24:36 +0000

    Add node stride

--- sst/elements/patterns/msgrate_bench.c
+++ sst/elements/patterns/msgrate_bench.c
@@ -31,13 +31,14 @@
 #define DEFAULT_MSG_LEN		(0)
 #define DEFAULT_NUM_MSGS	(200)
 #define MAX_MSG_LEN		(10 * 1024 * 1024)
+#define DEFAULT_STRIDE		(1)
 
 
 /* Local functions */
 static void usage(char *pname, int start_rank_default);
 static double Test1(int my_rank, int num_ranks, int num_msgs, int msg_len);
-static double Test2(int my_rank, int num_ranks, int num_msgs, int msg_len, int start_rank);
-static double Test3(int my_rank, int num_ranks, int num_msgs, int msg_len, int start_rank);
+static double Test2(int my_rank, int num_ranks, int num_msgs, int msg_len, int start_rank, int stride);
+static double Test3(int my_rank, int num_ranks, int num_msgs, int msg_len, int start_rank, int stride);
 char buf[MAX_MSG_LEN];
 
 
@@ -54,6 +55,8 @@ int i;
 double duration;
 double total_time;
 int start_rank;
+int stride;
+int num_sender;
 
 
 
@@ -84,10 +87,11 @@ int start_rank;
     num_msgs= DEFAULT_NUM_MSGS;
     msg_len= DEFAULT_MSG_LEN;
     start_rank= num_ranks / 2;
+    stride= DEFAULT_STRIDE;
 
 
     /* Check command line args */
-    while ((ch= getopt(argc, argv, ":l:n:s:")) != EOF)   {
+    while ((ch= getopt(argc, argv, ":l:n:s:i:")) != EOF)   {
         switch (ch)   {
             case 'l':
 		msg_len= strtol(optarg, (char **)NULL, 0);
@@ -120,6 +124,16 @@ int start_rank;
 		if ((start_rank < 1) || (start_rank > num_ranks))   {
 		    if (my_rank == 0)   {
 			fprintf(stderr, "Start node (-s) must be > 0 and < n - 1!\n");
+		    }
+		    error= TRUE;
+		}
+		break;
+
+            case 'i':
+		stride= strtol(optarg, (char **)NULL, 0);
+		if ((stride < 1) || (stride > num_ranks))   {
+		    if (my_rank == 0)   {
+			fprintf(stderr, "Node stride (-i) must be > 0 and < n - 1!\n");
 		    }
 		    error= TRUE;
 		}
@@ -172,32 +186,41 @@ int start_rank;
 	printf("\"\n");
     }
 
+    num_sender= num_ranks / 2;
     duration= Test1(my_rank, num_ranks, num_msgs, msg_len);
     MPI_Allreduce(&duration, &total_time, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     if (my_rank == 0)   {
 	printf("#  |||  Test 1: Ranks 0...%d will send %d messages of length %d to "
 	    "ranks %d...%d along %d paths\n", num_ranks / 2 - 1, num_msgs,
 	    msg_len, num_ranks / 2, num_ranks - 1, num_ranks / 2);
+	printf("#  |||  Test 1: %d senders, %d receivers\n", num_sender, num_sender);
 	printf("#  |||  Test 1: Average bi-section rate: %8.0f msgs/s\n",
 	    1.0 / ((total_time / (num_ranks / 2)) / num_msgs));
     }
 
 
-    duration= Test2(my_rank, num_ranks, num_msgs, msg_len, start_rank);
+    num_sender= ((num_ranks - start_rank) / stride);
+    if ((num_ranks - start_rank) % stride != 0)   {
+	num_sender++;
+    }
+
+    duration= Test2(my_rank, num_ranks, num_msgs, msg_len, start_rank, stride);
     MPI_Allreduce(&duration, &total_time, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     if (my_rank == 0)   {
-	printf("#  |||  Test 2: Rank 0 will send %d messages of length %d to ranks %d...%d\n",
-	    num_msgs, msg_len, start_rank, num_ranks - 1);
+	printf("#  |||  Test 2: Rank 0 will send %d messages of length %d to ranks %d...%d, stride %d\n",
+	    num_msgs, msg_len, start_rank, num_ranks - 1, stride);
+	printf("#  |||  Test 2: %d receivers\n", num_sender);
 	printf("#  |||  Test 2: Average send rate:       %8.0f msgs/s\n",
 	    1.0 / ((total_time / (num_ranks - start_rank)) / (num_msgs - 1)));
     }
 
 
-    duration= Test3(my_rank, num_ranks, num_msgs, msg_len, start_rank);
+    duration= Test3(my_rank, num_ranks, num_msgs, msg_len, start_rank, stride);
     MPI_Allreduce(&duration, &total_time, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
     if (my_rank == 0)   {
-	printf("#  |||  Test 3: Ranks %d...%d will send %d messages of length %d to rank 0\n",
-	    num_ranks - start_rank, num_ranks - 1, num_msgs, msg_len);
+	printf("#  |||  Test 3: Ranks %d...%d, stride %d, will send %d messages of length %d to rank 0\n",
+	    start_rank, num_ranks - 1, stride, num_msgs, msg_len);
+	printf("#  |||  Test 3: %d senders\n", num_sender);
 	printf("#  |||  Test 3: Average receive rate:    %8.0f msgs/s\n",
 	    1.0 / (duration / (num_msgs * (num_ranks - start_rank))));
     }
@@ -244,7 +267,7 @@ int i;
 
 
 static double
-Test2(int my_rank, int num_ranks, int num_msgs, int msg_len, int start_rank)
+Test2(int my_rank, int num_ranks, int num_msgs, int msg_len, int start_rank, int stride)
 {
 
 double t;
@@ -257,14 +280,14 @@ int dest;
 	dest= start_rank;
 	for (i= 0; i < num_msgs * (num_ranks - start_rank); i++)   {
 	    MPI_Send(buf, msg_len, MPI_CHAR, dest, 13, MPI_COMM_WORLD);
-	    dest++;
-	    if (dest % num_ranks == 0)   {
+	    dest= dest + stride;
+	    if (dest >= num_ranks)   {
 		dest= start_rank;
 	    }
 	}
 	return 0.0;
 
-    } else if (my_rank >= start_rank)   {
+    } else if ((my_rank >= start_rank) && ((my_rank - start_rank) % stride) == 0)   {
 	/* I am a receiver */
 	MPI_Recv(buf, msg_len, MPI_CHAR, 0, 13, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	t= MPI_Wtime();
@@ -282,22 +305,28 @@ int dest;
 
 
 static double
-Test3(int my_rank, int num_ranks, int num_msgs, int msg_len, int start_rank)
+Test3(int my_rank, int num_ranks, int num_msgs, int msg_len, int start_rank, int stride)
 {
 
 double t;
 int i;
 int dest;
+int num_sender;
 
 
     if (my_rank == 0)   {
 	/* I'm the receiver */
 	t= MPI_Wtime();
-	for (i= 0; i < num_msgs * (num_ranks - start_rank); i++)   {
+	num_sender= ((num_ranks - start_rank) / stride);
+	if ((num_ranks - start_rank) % stride != 0)   {
+	    num_sender++;
+	}
+
+	for (i= 0; i < num_msgs * num_sender; i++)   {
 	    MPI_Recv(buf, msg_len, MPI_CHAR, MPI_ANY_SOURCE, 14, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
 	return MPI_Wtime() - t;
-    } else if (my_rank >= start_rank)   {
+    } else if ((my_rank >= start_rank) && ((my_rank - start_rank) % stride) == 0)   {
 	/* I'm a sender */
 	dest= 0;
 	for (i= 0; i < num_msgs; i++)   {
@@ -315,10 +344,11 @@ static void
 usage(char *pname, int start_rank_default)
 {
 
-    fprintf(stderr, "Usage: %s [-l len] [-n num] [-s start]\n", pname);
+    fprintf(stderr, "Usage: %s [-l len] [-n num] [-s start] [-i stride]\n", pname);
     fprintf(stderr, "    -l len      Message length. Default %d\n", DEFAULT_MSG_LEN);
     fprintf(stderr, "    -n num      Number of messages per test. Default %d\n", DEFAULT_NUM_MSGS);
     fprintf(stderr, "    -s start    Rank 0 sends/recvs from \"start\" to n. Default %d\n",
 	    start_rank_default);
+    fprintf(stderr, "    -i stride   Node stride. Default %d\n", DEFAULT_STRIDE);
 
 }  /* end of usage() */
