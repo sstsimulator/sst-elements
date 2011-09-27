5c29b8a639d6df252a4596d24c18c322da4422e3
Mike Levenhagen <mjleven@sandia.gov>
2011-09-27 14:41:58 +0000

    remove commented out code

--- sst/elements/M5/m5.cpp
+++ sst/elements/M5/m5.cpp
@@ -161,8 +161,6 @@ void M5::selfEvent( SST::Event* )
 {
     m_armed = false;
 
-    //Cycle_t now = m_tc->convertToCoreTime( getCurrentSimTime(m_tc) );
-
     DBGX( 5, "currentTime=%lu cycles=%lu\n", m_event.time, m_event.cycles );
 
     if ( ! m_exitEvent ) {
