32ebe870382b841c7c9b5d84a5bc44fb87bfea9c
Rolf Riesen <rolf.riesen@intel.com>
2011-12-07 13:46:48 +0000

    Continue work on this. Allow for multiple streams.

--- sst/elements/patterns/benchmarks/router_bench.c
+++ sst/elements/patterns/benchmarks/router_bench.c
@@ -23,7 +23,8 @@
 #define MAX_TRIALS		(20000)
 
 #define DEFAULT_MSG_LEN		(1024)
-#define MSG_OPS			(100)
+#define MAX_MSG_LEN		(64 * 1024)
+#define MSG_OPS			(200)
 
 #define TAG_READY		(20)
 #define TAG_LATENCY		(21)
@@ -31,11 +32,11 @@
 
 #define	MAX_NUM_PREPOST	(16)
 
-static void experiment(int my_rank, int max_trials, int num_ops, int msg_len, int dest,
-    double req_precision, double (*do_one_trial)(int num_ops, int msg_len, int dest));
-static double do_one_Test2_trial(int num_ops, int msg_len, int dest);
+static void experiment(int my_rank, int max_trials, int num_ops, int msg_len, int num_streams,
+    double req_precision, double (*do_one_trial)(int num_ops, int msg_len, int num_streams));
+static double do_one_Test2_trial(int num_ops, int msg_len, int num_streams);
 void doit(int len, int trial, double *latency, int *msgs);
-double aligned_buf[DEFAULT_MSG_LEN/sizeof(double)];
+double aligned_buf[MAX_MSG_LEN/sizeof(double)];
 char *buf;
 int my_rank, num_nodes;
 
@@ -53,7 +54,7 @@ int max_trials;
 int stat_mode;
 double req_precision;
 int num_ops;
-int dest;
+int num_streams;
 
 
     MPI_Init(&argc, &argv);
@@ -74,7 +75,6 @@ int dest;
     len= DEFAULT_MSG_LEN;
     stat_mode= TRUE;
     req_precision= DEFAULT_PRECISION;
-    dest= num_nodes - 1;
 
 
     /* check command line args */
@@ -82,6 +82,12 @@ int dest;
 	switch (ch)   {
 	    case 'l':
 		len= strtol(optarg, (char **)NULL, 0);
+		if (len > MAX_MSG_LEN)   {
+		    if (my_rank == 0)   {
+			fprintf(stderr, "# length (-l %d) must be <= %d\n", len, MAX_MSG_LEN);
+		    }
+		    error= TRUE;
+		}
 		if (len < 0)   {
 		    if (my_rank == 0)   {
 			fprintf(stderr, "# length (-l %d) must be >= 0\n", len);
@@ -133,8 +139,9 @@ int dest;
     }
 
     num_ops= 1;
+    num_streams= num_nodes / 2;
     while (num_ops <= MSG_OPS)   {
-	experiment(my_rank, max_trials, num_ops, len, dest, req_precision,
+	experiment(my_rank, max_trials, num_ops, len, num_streams, req_precision,
 	    &do_one_Test2_trial);
 	num_ops++;
     }
@@ -148,26 +155,31 @@ int dest;
 
 
 static double
-do_one_Test2_trial(int num_ops, int msg_len, int dest)
+do_one_Test2_trial(int num_ops, int msg_len, int num_streams)
 {
 
 int j;
 double t, t2;
 MPI_Request latencyflag[MSG_OPS];
 int prepost;
+int src, dest;
+int stride= 6;
 
 
     /* Touch the memory we are going to use */
     memset(aligned_buf, 0xAA, msg_len);
     memset(aligned_buf, 0x55, msg_len);
 
-    /* Do a warm-up */
-
-
-
-    /* Post warm-up: Run the actual experiment */
+    /* Run the experiment */
     t2= 0.0;
-    if (my_rank == 0)   {
+    if (my_rank < num_streams)   {
+	dest= num_nodes - 1 - my_rank;
+	if (my_rank % stride != 0)   {
+	    // I don't participate
+	    return t2;
+	}
+
+
 	/* Wait for ACK from other node */
 	MPI_Recv(NULL, 0, MPI_BYTE, dest, TAG_READY, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
@@ -184,7 +196,14 @@ int prepost;
 	    MPI_Wait(&latencyflag[j], MPI_STATUS_IGNORE);
 	}
 
-    } else if (my_rank == dest)   {
+    } else if (my_rank >= (num_nodes - num_streams))   {
+	src= num_nodes - 1 - my_rank;
+	if ((my_rank + 1) % stride != 0)   {
+	    // I don't participate
+	    return t2;
+	}
+
+
 	/* Pre-post MAX_NUM_PREPOST receives */ 
 	if (num_ops >= MAX_NUM_PREPOST)   {
 	    prepost= MAX_NUM_PREPOST;
@@ -192,23 +211,23 @@ int prepost;
 	    prepost= num_ops;
 	}
 	for (j= 0; j < prepost; j++)   {
-	    MPI_Irecv(aligned_buf, msg_len, MPI_BYTE, 0, TAG_LATENCY, MPI_COMM_WORLD, &latencyflag[j]);
+	    MPI_Irecv(aligned_buf, msg_len, MPI_BYTE, src, TAG_LATENCY, MPI_COMM_WORLD, &latencyflag[j]);
 	}
 
 	/* Send ACK */
-	MPI_Send(NULL, 0, MPI_BYTE, 0, TAG_READY, MPI_COMM_WORLD);
+	MPI_Send(NULL, 0, MPI_BYTE, src, TAG_READY, MPI_COMM_WORLD);
 
 	for (j= 0; j < num_ops; j++)   {
 	    MPI_Wait(&latencyflag[j], MPI_STATUS_IGNORE);
 	    if (j + prepost < num_ops)   {
 		/* Post another */
-		MPI_Irecv(aligned_buf, msg_len, MPI_BYTE, 0, TAG_LATENCY, MPI_COMM_WORLD,
+		MPI_Irecv(aligned_buf, msg_len, MPI_BYTE, src, TAG_LATENCY, MPI_COMM_WORLD,
 		    &latencyflag[j + prepost]);
 	    }
 	}
 
 	/* Got them all */
-	MPI_Send(NULL, 0, MPI_BYTE, 0, TAG_DONE, MPI_COMM_WORLD);
+	MPI_Send(NULL, 0, MPI_BYTE, src, TAG_DONE, MPI_COMM_WORLD);
     }
 
     return t2;
@@ -218,8 +237,8 @@ int prepost;
 
 
 static void
-experiment(int my_rank, int max_trials, int num_ops, int msg_len, int dest,
-    double req_precision, double (*do_one_trial)(int num_ops, int msg_len, int dest))
+experiment(int my_rank, int max_trials, int num_ops, int msg_len, int num_streams,
+    double req_precision, double (*do_one_trial)(int num_ops, int msg_len, int num_streams))
 {
 
 double tot;
@@ -246,15 +265,15 @@ double *results;
 	cnt++;
 
 	/* Run one trial */
-	metric= do_one_trial(num_ops, msg_len, dest);
+	metric= do_one_trial(num_ops, msg_len, num_streams);
 	results[cnt - 1]= metric * 1000000.0;
 
 	tot= tot + metric;
 	tot_squared= tot_squared + metric * metric;
 	precision= stat_p(cnt, tot, tot_squared);
 
-	/* Need at least tree trials */
-	if ((cnt >= 3) && (precision < req_precision))   {
+	/* Need at least nine trials */
+	if ((cnt >= 9) && (precision < req_precision))   {
 	    /* Attained the required precisions. Done! */
 	    done= 1;
 	} else   {
