deb20434a4389ce84585b3debebae1e3ac3f2ae6
Mike Levenhagen <mjleven@sandia.gov>
2012-04-04 20:44:47 +0000

    fixup indentation

--- sst/elements/M5/app/rt.c
+++ sst/elements/M5/app/rt.c
@@ -8,22 +8,22 @@ int main( int argc, char* argv[] )
 {
     printf("hello mike\n");
 
-	int rank = cnos_get_rank();
-	int size = cnos_get_size();
+    int rank = cnos_get_rank();
+    int size = cnos_get_size();
 
-	printf("rank=%d size=%d\n",rank,size);
+    printf("rank=%d size=%d\n",rank,size);
 
-	cnos_nidpid_map_t* map;
+    cnos_nidpid_map_t* map;
 
-	assert( cnos_get_nidpid_map( &map ) == size );
+    assert( cnos_get_nidpid_map( &map ) == size );
 
-	int i;
-	for ( i = 0; i < size; i++) {
-		printf("%d: rank=%d nid=%d pid=%d\n", rank, i, map[i].nid, map[i].pid );
-	}
+    int i;
+    for ( i = 0; i < size; i++) {
+	printf("%d: rank=%d nid=%d pid=%d\n", rank, i, map[i].nid, map[i].pid );
+    }
 
     printf("call cnos_barrier\n");
-	cnos_barrier();
+    cnos_barrier();
     printf("goodby mike\n");
 
     return 0;
