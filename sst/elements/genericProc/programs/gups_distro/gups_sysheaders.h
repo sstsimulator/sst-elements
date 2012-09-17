19bc18218be160fa5674b049c8c36c0c4bba4aef
Jon Wilson <jwilso@sandia.gov>
2012-09-17 23:26:20 +0000

    Cleanup mainline for 2.2.0_pre branch

--- sst/elements/genericProc/programs/gups_distro/gups_sysheaders.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <sys/time.h>
-#include <math.h>
--- sst/elements/genericProc/programs/minimd-0.1/acinclude.m4
+++
@@ -1,2 +0,0 @@
-m4_include([config/mtgl_check_qthreads.m4])
-m4_include([config/ax_mpi.m4])
