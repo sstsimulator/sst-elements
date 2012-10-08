2f8e5eadaf96994fdf823c9263a70100bf50c1f1
John Van Dyke <jpvandy@sandia.gov>
2012-10-08 16:54:18 +0000

    Restore changes to portLink.h mistakenly removed in moving changes (differences) from the Branch to the Mainline

--- sst/elements/M5/portLink.h
+++ sst/elements/M5/portLink.h
@@ -15,15 +15,36 @@ class PortLink {
   private:
     inline void eventHandler( SST::Event* );     
 
-    inline  bool recv( void* data, size_t len ) {
-        DBGC(2,"\n");
+    inline void poke() {
+        DBGX(2,"\n");
+        assert( ! m_deferredQ.empty() ); 
+
+        while ( ! m_deferredQ.empty() ) {
+            RawEvent *event = m_deferredQ.front();
+            
+            if ( ! m_gem5EndPoint->send( event->data(), event->len() ) ) {
+                DBGX(2,"busy\n");
+                return;
+            }
+
+            m_deferredQ.pop_front();
+            delete event;
+        }
+    }
+
+    inline bool recv( void* data, size_t len ) {
+        DBGX(2,"\n");
         
         // Note we are not throttling events
         m_link->Send( new RawEvent( data, len ) );
         return true;
     }
 
-    static inline  bool recv( void *obj, void *data, size_t len ) {
+    static inline void poke( void* obj ) {
+        return ((PortLink*)obj)->poke( );
+    }
+
+    static inline bool recv( void *obj, void *data, size_t len ) {
         return ((PortLink*)obj)->recv( data, len );
     }
 
@@ -31,13 +52,14 @@ class PortLink {
     M5&             m_comp;
     MsgEndPoint*    m_gem5EndPoint;
     MsgEndPoint     m_myEndPoint;
+    std::deque<RawEvent*>  m_deferredQ;
 };
 
 PortLink::PortLink( M5& comp, Gem5Object_t& obj, const SST::Params& params ) :
     m_comp( comp ),
-    m_myEndPoint( this, recv )
+    m_myEndPoint( this, recv, poke )
 {
-    bool snoop = false;
+    bool snoopOut = false, snoopIn = false;
     std::string name = params.find_string("name");
     assert( ! name.empty() );
     std::string portName = params.find_string("portName");
@@ -45,8 +67,12 @@ PortLink::PortLink( M5& comp, Gem5Object_t& obj, const SST::Params& params ) :
     int idx = params.find_integer( "portIndex" );
     uint64_t addrStart = 0, addrEnd = -1;
 
-    if ( params.find_string("snoop").compare("true") == 0 ) {
-        snoop = true;
+    if ( params.find_string("snoopOut").compare("true") == 0 ) {
+        snoopOut = true;
+    }
+
+    if ( params.find_string("snoopIn").compare("true") == 0 ) {
+        snoopIn = true;
     }
 
     if ( params.find("range.start") != params.end() ) {
@@ -56,27 +82,34 @@ PortLink::PortLink( M5& comp, Gem5Object_t& obj, const SST::Params& params ) :
         addrEnd = params.find_integer("range.end");
     }
 
-    DBGC(2,"%s portName=%s index=%d\n", name.c_str(), portName.c_str(), idx );
+    DBGX(2,"%s portName=%s index=%d\n", name.c_str(), portName.c_str(), idx );
 
     m_link = comp.configureLink( name,
         new SST::Event::Handler<PortLink>( this, &PortLink::eventHandler ) );
     assert( m_link );
 
-    m_gem5EndPoint = GetPort(obj, m_myEndPoint, snoop, 
-                            addrStart, addrEnd, portName, idx ); 
+    m_gem5EndPoint = GetPort(obj, m_myEndPoint, snoopOut, snoopIn, 
+                            addrStart, addrEnd, name, portName, idx ); 
+
     assert( m_gem5EndPoint );
 }
 
 void PortLink::eventHandler( SST::Event *e )
 {
-    DBGC(2,"\n");
+    DBGX(2,"\n");
 
     RawEvent *event = static_cast<RawEvent*>( e ); 
+
+    if ( ! m_deferredQ.empty() ) {
+        DBGX(2,"blocked, defer event\n");
+        m_deferredQ.push_back( event );
+        return;
+    }
     
     if ( ! m_gem5EndPoint->send( event->data(), event->len() ) ) {
-        // What if Gem5 refuses to take this event? 
-        // We need to check for failure and respond to retries
-        abort();
+        DBGX(2,"busy, defer event\n");
+        m_deferredQ.push_back( event );
+        return;
     };
 
     delete e;
