b9011494e8b520c1257ff71e858802144ff84f44
Si Hammond <simon.hammond@gmail.com>
2013-02-28 17:30:04 +0000

    Example RNG component and can be used for testing.

---
+++ sst/elements/simpleRNGComponent/simpleRNGComponent.cc
@@ -0,0 +1,96 @@
+
+#include "sst_config.h"
+#include "sst/core/serialization/element.h"
+#include <assert.h>
+
+#include "sst/core/element.h"
+
+#include "simpleRNGComponent.h"
+
+using namespace SST;
+
+simpleRNGComponent::simpleRNGComponent(ComponentId_t id, Params_t& params) :
+  Component(id) {
+
+  unsigned int m_z = 0;
+  unsigned int  m_w = 0;
+
+  if( params.find("seed_w") != params.end() ) {
+	m_w = (unsigned int) strtoul( params["seed_w"].c_str(), NULL, 10);
+  }
+
+  if( params.find("seed_z") != params.end() ) {
+	m_z = (unsigned int) strtoul( params["seed_z"].c_str(), NULL, 10);
+  }
+
+  rng_count = 1000;
+  if( params.find("count") != params.end() ) {
+	rng_count = (unsigned int) atoi( params["count"].c_str() );
+  }
+
+  if(m_w == 0 || m_z == 0) {
+	rng = new SSTRandom();
+  } else {
+	rng = new SSTRandom(m_z, m_w);
+  }
+
+  rng_noseed = new SSTRandom();
+
+  // tell the simulator not to end without us
+  registerExit();
+
+  //set our clock
+  registerClock( "1GHz", 
+		 new Clock::Handler<simpleRNGComponent>(this, 
+			&simpleRNGComponent::tick ) );
+}
+
+simpleRNGComponent::simpleRNGComponent() :
+    Component(-1)
+{
+    // for serialization only
+}
+
+bool simpleRNGComponent::tick( Cycle_t ) {
+	rng_count--;
+
+	std::cout << "Next uniform random: " <<
+		rng->nextUniform() << " / No seed=" <<
+		rng_noseed->nextUniform() << std::endl;
+
+  	// return false so we keep going
+  	if(rng_count == 0) {
+		unregisterExit();
+  		return true;
+  	} else {
+  		return false;
+  	}
+}
+
+// Element Libarary / Serialization stuff
+    
+BOOST_CLASS_EXPORT(simpleRNGComponent)
+
+static Component*
+create_simpleRNGComponent(SST::ComponentId_t id, 
+                  SST::Component::Params_t& params)
+{
+    return new simpleRNGComponent( id, params );
+}
+
+static const ElementInfoComponent components[] = {
+    { "simpleRNGComponent",
+      "Random number generation component",
+      NULL,
+      create_simpleRNGComponent
+    },
+    { NULL, NULL, NULL, NULL }
+};
+
+extern "C" {
+    ElementLibraryInfo simpleRNGComponent_eli = {
+        "simpleRNGComponent",
+        "Random number generation component",
+        components,
+    };
+}
---
+++ sst/elements/simpleRNGComponent/simpleRNGComponent.h
@@ -0,0 +1,52 @@
+
+#ifndef _SIMPLERNGCOMPONENT_H
+#define _SIMPLERNGCOMPONENT_H
+
+#include <sst/core/event.h>
+#include <sst/core/sst_types.h>
+#include <sst/core/component.h>
+#include <sst/core/link.h>
+#include <sst/core/timeConverter.h>
+#include <sst/core/rng/sstrand.h>
+
+#include <cstring>
+#include <string>
+
+class simpleRNGComponent : public SST::Component {
+public:
+
+  simpleRNGComponent(SST::ComponentId_t id, SST::Component::Params_t& params);
+  int Setup()  { return 0; }
+  int Finish() { return 0; }
+
+private:
+  simpleRNGComponent();  // for serialization only
+  simpleRNGComponent(const simpleRNGComponent&); // do not implement
+  void operator=(const simpleRNGComponent&); // do not implement
+
+  virtual bool tick( SST::Cycle_t );
+
+  SSTRandom* rng;
+  SSTRandom* rng_noseed;
+  int rng_count;
+
+  friend class boost::serialization::access;
+  template<class Archive>
+  void save(Archive & ar, const unsigned int version) const
+  {
+    ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Component);
+    ar & BOOST_SERIALIZATION_NVP(rng_count);
+  }
+
+  template<class Archive>
+  void load(Archive & ar, const unsigned int version) 
+  {
+    ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Component);
+    ar & BOOST_SERIALIZATION_NVP(rng_count);
+  }
+    
+  BOOST_SERIALIZATION_SPLIT_MEMBER()
+ 
+};
+
+#endif /* _SIMPLERNGCOMPONENT_H */
