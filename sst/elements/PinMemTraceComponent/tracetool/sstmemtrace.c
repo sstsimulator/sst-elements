4b7f3d90c2b79b6dfa4d6b34a33776c0c3150f51
Si Hammond <simon.hammond@gmail.com>
2013-03-11 19:59:02 +0000

    Changes needed for prospero.

--- sst/elements/PinMemTraceComponent/tracetool/sstmemtrace.c
+++ sst/elements/PinMemTraceComponent/tracetool/sstmemtrace.c
@@ -3,19 +3,66 @@
 
 #include <stdio.h>
 #include "pin.H"
+#include <cstring>
 
 FILE * trace;
 
+uint64_t instruction_count;
+
+const char READ_OPERATION_CHAR = 0;
+const char WRITE_OPERATION_CHAR = 1;
+
+char RECORD_BUFFER[ sizeof(uint64_t) + sizeof(uint64_t) + sizeof(uint32_t) + sizeof(char) ];
+
+void copy(VOID* dest, const VOID* source, int destoffset, int count) {
+	char* dest_c = (char*) dest;
+	char* source_c = (char*) source;
+	int counter;
+
+	for(counter = 0; counter < count; counter++) {
+		dest_c[destoffset + counter] = source_c[counter];
+	}
+}
+
 // Print a memory read record
-VOID RecordMemRead(VOID * ip, VOID * addr)
+VOID RecordMemRead(VOID * addr, UINT32 size)
 {
-    fprintf(trace,"%p R %p\n", ip, addr);
+    UINT64 ma_addr = (UINT64) addr;
+
+/*    fwrite(&instruction_count, sizeof(instruction_count), 1, trace);
+    fwrite(&READ_OPERATION_CHAR, sizeof(char), 1, trace);
+    fwrite(&ma_addr, sizeof(ma_addr), 1, trace);
+    fwrite(&size, sizeof(UINT32), 1, trace); */
+
+    copy(RECORD_BUFFER, &instruction_count, 0, sizeof(uint64_t) );
+    copy(RECORD_BUFFER, &READ_OPERATION_CHAR, sizeof(uint64_t), sizeof(char) );
+    copy(RECORD_BUFFER, &ma_addr, sizeof(uint64_t) + sizeof(char), sizeof(uint64_t) );
+    copy(RECORD_BUFFER, &size, sizeof(uint64_t) + sizeof(char) + sizeof(uint64_t), sizeof(uint32_t) );
+
+   fwrite( RECORD_BUFFER, sizeof(uint64_t) + sizeof(uint64_t) + sizeof(uint32_t) + sizeof(char), 1, trace);
 }
 
 // Print a memory write record
-VOID RecordMemWrite(VOID * ip, VOID * addr)
+VOID RecordMemWrite(VOID * addr, UINT32 size)
 {
-    fprintf(trace,"%p W %p\n", ip, addr);
+    UINT64 ma_addr = (UINT64) addr;
+
+    /*fwrite(&instruction_count, sizeof(instruction_count), 1, trace);
+    fwrite(&WRITE_OPERATION_CHAR, sizeof(char), 1, trace);
+    fwrite(&ma_addr, sizeof(ma_addr), 1, trace);
+    fwrite(&size, sizeof(UINT32), 1, trace);*/
+
+    copy(RECORD_BUFFER, &instruction_count, 0, sizeof(uint64_t) );
+    copy(RECORD_BUFFER, &WRITE_OPERATION_CHAR, sizeof(uint64_t), sizeof(char) );
+    copy(RECORD_BUFFER, &ma_addr, sizeof(uint64_t) + sizeof(char), sizeof(uint64_t) );
+    copy(RECORD_BUFFER, &size, sizeof(uint64_t) + sizeof(char) + sizeof(uint64_t), sizeof(uint32_t) );
+
+   fwrite( RECORD_BUFFER, sizeof(uint64_t) + sizeof(uint64_t) + sizeof(uint32_t) + sizeof(char), 1, trace);
+
+}
+
+VOID IncrementInstructionCount() {
+    instruction_count++;
 }
 
 // Is called for every instruction and instruments reads and writes
@@ -32,12 +79,14 @@ VOID Instruction(INS ins, VOID *v)
     // Iterate over each memory operand of the instruction.
     for (UINT32 memOp = 0; memOp < memOperands; memOp++)
     {
+	USIZE mem_size = INS_MemoryOperandSize(ins, memOp);
+
         if (INS_MemoryOperandIsRead(ins, memOp))
         {
             INS_InsertPredicatedCall(
                 ins, IPOINT_BEFORE, (AFUNPTR)RecordMemRead,
-                IARG_INST_PTR,
                 IARG_MEMORYOP_EA, memOp,
+		IARG_UINT32, (UINT32) mem_size,
                 IARG_END);
         }
         // Note that in some architectures a single memory operand can be 
@@ -47,11 +96,14 @@ VOID Instruction(INS ins, VOID *v)
         {
             INS_InsertPredicatedCall(
                 ins, IPOINT_BEFORE, (AFUNPTR)RecordMemWrite,
-                IARG_INST_PTR,
                 IARG_MEMORYOP_EA, memOp,
+		IARG_UINT32, (UINT32) mem_size,
                 IARG_END);
         }
     }
+
+    INS_InsertPredicatedCall(
+	ins, IPOINT_BEFORE, (AFUNPTR) IncrementInstructionCount, IARG_END);
 }
 
 VOID Fini(INT32 code, VOID *v)
@@ -81,8 +133,10 @@ int main(int argc, char *argv[])
 #ifdef TRACE_STDOUT
     trace = stdout;
 #else
-    trace = fopen("pinatrace.out", "w");
+    trace = fopen("pinatrace.out", "wb");
 #endif
+
+    instruction_count = 0;
 
     INS_AddInstrumentFunction(Instruction, 0);
     PIN_AddFiniFunction(Fini, 0);
