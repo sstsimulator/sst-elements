4df7e9aa09a98c7e30bdcc18630f4da1a9ab2e5a
Si Hammond <simon.hammond@gmail.com>
2013-03-11 19:23:40 +0000

    Performs initial read in and then tick-chase through the stream and SST clock ticks.
    Detects correct time to insert memory requests, now just need to connect to upstream cache(s).

--- sst/elements/prospero/prospero.cc
+++ sst/elements/prospero/prospero.cc
@@ -39,16 +39,104 @@ prospero::prospero(ComponentId_t id, Params_t& params) :
 		std::cout << "TRACE:  Load trace information from: " << params[ "trace" ] << std::endl;
 	}
 
-	trace_input = fopen(params["trace"].c_str(), "rt");
+	trace_input = fopen(params["trace"].c_str(), "rb");
+
+	if(trace_input == NULL) {
+		std::cerr << "TRACE:  Unable to open trace file: " <<
+			params["trace"] << ", component will halt." << std::endl;
+		exit(-1);
+	}
+	//trace_input.open(params["trace"].c_str(), fstream::in);
+  }
+
+  if( params.find("request_limit") == params.end() ) {
+	pending_request_limit = 16;
+  } else {
+	pending_request_limit = atoi( params[ "request_limit" ].c_str() );
+
+	if(output_level > 0) {
+		std::cout << "TRACE:  Configuring traces for a pending request limit of " << pending_request_limit << std::endl;
+	}
+  }
+
+  if( params.find("max_ticks") == params.end() ) {
+	max_tick_count = 4611686018427390000;
+  } else {
+	max_tick_count = atol( params[ "max_ticks" ].c_str() );
+
+	if(output_level > 0) {
+		std::cout << "TRACE:  Max ticks configured for " << max_tick_count << std::endl;
+	}
+  }
+
+  if( params.find("clock") != params.end() ) {
+	string clock_rate = params[ "clock" ];
+
+  	// register the prospero clock for generating addresses
+  	registerClock( clock_rate, new Clock::Handler<prospero>(this, &prospero::tick ) );
+
+	if(output_level > 0) {
+		std::cout << "TRACE:  Component will tick at " << clock_rate << std::endl;
+        }
+  } else {
+	if(output_level > 0) {
+		std::cout << "TRACE:  Component will tick at 2GHz (not specified in input)." << std::endl;
+        }
+
+  	// register the prospero clock for generating addresses
+  	registerClock( "2GHz", new Clock::Handler<prospero>(this, &prospero::tick ) );
+
   }
 
   // tell the simulator not to end without us
   registerExit();
 
-  //set our clock
-  //registerClock( clock_frequency_str, 
-  //		 new Clock::Handler<prospero>(this, 
-  //			&prospero::tick ) );
+  next_request.memory_address = 0;
+  tick_count = 0;
+}
+
+void copy(void* dest, void* source, int source_offset, int length) {
+	char* dest_c = (char*) dest;
+	char* source_c = (char*) source;
+	int counter;
+
+	for(counter = 0; counter < length; counter++) {
+		dest_c[counter] = source_c[source_offset + counter];
+	}
+}
+
+read_trace_return prospero::readNextRequest(memory_request* req) {
+	if(feof(trace_input)) {
+		return READ_FAILED_EOF;
+	}
+
+	const int record_length = sizeof(uint64_t) + sizeof(char) + sizeof(uint64_t) + sizeof(uint32_t);
+	char record_buffer[ record_length ];
+	fread(record_buffer, record_length, 1, trace_input);
+
+	char op_type;
+
+	copy(&(req->instruction_count), record_buffer, 0, sizeof(uint64_t));
+	copy(&op_type, record_buffer, sizeof(uint64_t), sizeof(char));
+	copy(&(req->memory_address), record_buffer, sizeof(uint64_t) + sizeof(char), sizeof(uint64_t));
+	copy(&(req->size), record_buffer, sizeof(uint64_t) + sizeof(char) + sizeof(uint64_t), sizeof(uint32_t));
+
+	// decode memory operation to category the upstream components can use
+	if(op_type == 0) 
+		req->memory_op_type = READ;
+	else if (op_type == 1)
+		req->memory_op_type = WRITE;
+	else {
+		std::cerr << "TRACE:  Unknown memory operation type: " << op_type << ", component will exit." << std::endl;
+		exit(-1);
+	}
+
+	if(output_level > 0) {
+		std::cout << "TRACE: Next record: Ins=" << req->instruction_count <<
+			"/ MA=" << req->memory_address << "/ Size=" << req->size << std::endl;
+	}
+
+	return READ_SUCCESS;
 }
 
 prospero::prospero() :
@@ -58,11 +146,61 @@ prospero::prospero() :
 }
 
 bool prospero::tick( Cycle_t ) {
-	return true;
+	if(output_level > 0) {
+		std::cout << "TRACE: Tick count " << tick_count << std::endl;
+	}
+
+	// hold result for any trace reads we need to do
+	read_trace_return result;
+
+	if(tick_count == 0) {
+		if(output_level > 0) {
+			std::cout << "TRACE:  Reading initial memory request record..." << std::endl;
+		}
+
+		result = readNextRequest( &next_request );
+
+		if(result == READ_FAILED_EOF) {
+			std::cout << "TRACE: Read failed on first record, are you sure file was created successfully?" << std::endl;
+			exit(-1);
+		}
+	}
+
+	std::cout << "Request instruction: " << (int) next_request.instruction_count << ", current tick=" << 
+		(int) tick_count << std::endl;
+
+	if(next_request.instruction_count <= tick_count) {
+		std::cout << "TRACE:  Process request on this iteration." << std::endl;
+
+		int continue_processing = 1;
+		while(continue_processing) {
+			// process the next_request object
+
+			result = readNextRequest(&next_request);
+			if(result == READ_FAILED_EOF) {
+				// we're done reading trace information, let the simulation end gracefully
+				// other components may need to drain
+				return true;
+			} else if(result == READ_SUCCESS) {
+				// Process the next request
+				if(next_request.instruction_count > tick_count) {
+					break;
+				}
+			}
+		}
+	} // else we just let the component tick forth as needed
+
+	tick_count++;
+
+	if(tick_count == max_tick_count) {
+		std::cout << "TRACE: Stopping due to max ticks reached." << std::endl;
+		return true;
+	} else {
+		return false;
+	}
 }
 
 // Element Libarary / Serialization stuff
-    
 BOOST_CLASS_EXPORT(prospero)
 
 static Component*
@@ -73,7 +211,7 @@ create_prospero(SST::ComponentId_t id,
 }
 
 static const ElementInfoComponent components[] = {
-    { "Prospero",
+    { "prospero",
       "Memory tracing component",
       NULL,
       create_prospero
--- sst/elements/prospero/prospero.h
+++ sst/elements/prospero/prospero.h
@@ -10,24 +10,49 @@
 
 #include <cstring>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 #include <stdio.h>
 #include <stdint.h>
+
+using namespace std;
+
+typedef enum {
+	READ_SUCCESS,
+	READ_FAILED_EOF
+} read_trace_return;
+
+typedef enum {
+	READ,
+	WRITE
+} op_type;
+
+typedef struct {
+	uint64_t instruction_count;
+	uint64_t memory_address;
+	op_type memory_op_type;
+	uint32_t size;
+} memory_request;
 
 class prospero : public SST::Component {
 public:
 
   prospero(SST::ComponentId_t id, SST::Component::Params_t& params);
+
   int Setup()  { return 0; }
-  int Finish() 
-  { 
+  int Finish() {
 	if(output_level > 0) 
 		std::cout << "TRACE:  Closing trace input..." << std::endl;
+
+	fclose(trace_input);
 
 	if(output_level > 0) 
 		std::cout << "TRACE:  Close of trace input complete." << std::endl;
 	return 0; 
   }
+
+  read_trace_return readNextRequest(memory_request* req);
 
 private:
   prospero();  // for serialization only
@@ -37,8 +62,12 @@ private:
   virtual bool tick( SST::Cycle_t );
 
   FILE* trace_input;
+  memory_request next_request;
   uint64_t max_trace_count;
+  uint64_t tick_count;
+  uint64_t max_tick_count;
   int output_level;
+  int pending_request_limit;
 
   friend class boost::serialization::access;
   template<class Archive>
@@ -53,9 +82,6 @@ private:
   {
     ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Component);
     ar & BOOST_SERIALIZATION_NVP(max_trace_count);
-    // This is going to be a problem, we are trying to recreate
-    // a file handle or alternatively a UNIX pipe.
-    trace_input = NULL;
   }
   BOOST_SERIALIZATION_SPLIT_MEMBER()
  
