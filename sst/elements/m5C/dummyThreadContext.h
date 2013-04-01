1a7c132580c48e1341c029427ab23e04ecdae87d
Branden J. Moore <bjmoor@sandia.gov>
2013-04-01 16:51:51 +0000

    Add dummy return arguments to clean up warnings

--- sst/elements/m5C/dummyThreadContext.h
+++ sst/elements/m5C/dummyThreadContext.h
@@ -12,83 +12,77 @@ namespace M5 {
 
 class DummyThreadContext : public ThreadContext {
     virtual BaseCPU *getCpuPtr() 
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n");  return NULL; }
 
     virtual TheISA::PCState pcState()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void pcState(const TheISA::PCState &val)
     { DBGX(2,"\n"); }
 
     virtual Addr instAddr()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual Addr nextInstAddr()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual MicroPC microPC()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual int flattenIntIndex(int reg)
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual int flattenFloatIndex(int reg)
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual int cpuId()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual int threadId()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void setThreadId(int id)
     { DBGX(2,"id=%d\n",id); }
 
     virtual int contextId()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void setContextId(int id)
     { DBGX(2,"\n"); }
 
     virtual TheISA::TLB *getITBPtr()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return NULL;}
 
     virtual TheISA::TLB *getDTBPtr()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return NULL;}
 
     virtual Decoder *getDecoderPtr()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return NULL;}
 
     virtual System *getSystemPtr()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return NULL;}
 #if FULL_SYSTEM
     virtual TheISA::Kernel::Statistics *getKernelStats()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return NULL;}
 
     virtual FunctionalPort *getPhysPort()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return NULL;}
 
     virtual VirtualPort *getVirtPort()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return NULL;}
 
     virtual void connectMemPorts(ThreadContext *tc)
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return NULL;}
 #else
     virtual TranslatingPort *getMemPort()
-    { 
-        DBGX(2,"\n");
-        return NULL; 
-    }
+    { DBGX(2,"\n"); return NULL; }
 
     virtual Process *getProcessPtr()
-    { 
-        DBGX(2,"\n");
-        return NULL;
-    }
+    { DBGX(2,"\n"); return NULL; }
 #endif
 
     virtual Status status() const
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return Status();}
 
     virtual void setStatus(Status new_status)
     { DBGX(2,"new_status=%d\n",new_status); }
@@ -130,9 +124,9 @@ class DummyThreadContext : public ThreadContext {
     // Not necessarily the best location for these...
     // Having an extra function just to read these is obnoxious
     virtual Tick readLastActivate()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
     virtual Tick readLastSuspend()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void profileClear()
     { DBGX(2,"\n"); }
@@ -143,7 +137,7 @@ class DummyThreadContext : public ThreadContext {
     // Also somewhat obnoxious.  Really only used for the TLB fault.
     // However, may be quite useful in SPARC.
     virtual TheISA::MachInst getInst()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void copyArchRegs(ThreadContext *tc)
     { DBGX(2,"\n"); }
@@ -155,19 +149,19 @@ class DummyThreadContext : public ThreadContext {
     // New accessors for new decoder.
     //
     virtual uint64_t readIntReg(int reg_idx)
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual FloatReg readFloatReg(int reg_idx, int width)
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual FloatReg readFloatReg(int reg_idx)
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual FloatRegBits readFloatRegBits(int reg_idx, int width)
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual FloatRegBits readFloatRegBits(int reg_idx)
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void setIntReg(int reg_idx, uint64_t val)
     { DBGX(2,"reg=%d val=%#lx\n",reg_idx, val); }
@@ -185,40 +179,40 @@ class DummyThreadContext : public ThreadContext {
     { DBGX(2,"\n"); }
 
     virtual uint64_t readPC()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void setPC(uint64_t val)
     { DBGX(2,"\n"); }
 
     virtual uint64_t readNextPC()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void setNextPC(uint64_t val)
     { DBGX(2,"\n"); }
 
     virtual uint64_t readNextNPC()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void setNextNPC(uint64_t val)
     { DBGX(2,"\n"); }
 
     virtual uint64_t readMicroPC()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void setMicroPC(uint64_t val)
     { DBGX(2,"\n"); }
 
     virtual uint64_t readNextMicroPC()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void setNextMicroPC(uint64_t val)
     { DBGX(2,"\n"); }
 
     virtual MiscReg readMiscRegNoEffect(int misc_reg)
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual MiscReg readMiscReg(int misc_reg)
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void setMiscRegNoEffect(int misc_reg, const MiscReg &val)
     { DBGX(2,"reg=%d val=%#lx\n",misc_reg, val); }
@@ -228,7 +222,7 @@ class DummyThreadContext : public ThreadContext {
 
     virtual uint64_t
     readRegOtherThread(int misc_reg, ThreadID tid)
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void
     setRegOtherThread(int misc_reg, const MiscReg &val, ThreadID tid)
@@ -237,28 +231,28 @@ class DummyThreadContext : public ThreadContext {
     // Also not necessarily the best location for these two.  Hopefully will go
     // away once we decide upon where st cond failures goes.
     virtual unsigned readStCondFailures()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual void setStCondFailures(unsigned sc_failures)
     { DBGX(2,"\n"); }
 
     // Only really makes sense for old CPU model.  Still could be useful though.
     virtual bool misspeculating()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return false;}
 
 #if !FULL_SYSTEM
     // Same with st cond failures.
     virtual Counter readFuncExeInst()
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return 0;}
 
     virtual bool syscall(int64_t callnum)
-    { DBGX(2,"\n"); }
+    { DBGX(2,"\n"); return false;}
 
     // This function exits the thread context in the CPU and returns
     // 1 if the CPU has no more active threads (meaning it's OK to exit);
     // Used in syscall-emulation mode when a  thread calls the exit syscall.
-    virtual int exit() 
-    { DBGX(2,"\n"); }
+    virtual int exit()
+    { DBGX(2,"\n"); return 0;}
 #endif
 
 };
