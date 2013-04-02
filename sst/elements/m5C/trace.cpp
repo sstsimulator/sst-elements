947fea3d4f9e8a8884813845c1bbc53f6cfb06ef
Branden J. Moore <bjmoor@sandia.gov>
2013-04-02 22:43:45 +0000

    Fix to allow a static build to run

--- sst/elements/m5C/trace.cpp
+++ sst/elements/m5C/trace.cpp
@@ -11,11 +11,13 @@ _Trace __trace;
 
 static __attribute__ ((constructor)) void init(void)
 {
+#if 0   /* THIS DOES NOT WORK IN A STATIC BUILD. */
     boost::mpi::communicator world;
     std::ostringstream tmp;
     tmp << world.rank();
 
 //    TRACE_SET_PREFIX( tmp.str() + ":" );
+#endif
 }
 
 
