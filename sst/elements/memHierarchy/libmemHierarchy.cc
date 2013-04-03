32c87e45571c9566be5e95cc13223959b0423837
Branden J. Moore <bjmoor@sandia.gov>
2013-04-03 22:37:56 +0000

    Added parameter documentation

--- sst/elements/memHierarchy/libmemHierarchy.cc
+++ sst/elements/memHierarchy/libmemHierarchy.cc
@@ -53,27 +53,64 @@ create_MemController(SST::ComponentId_t id,
 	return new MemController( id, params );
 }
 
+static const ElementInfoParam cache_params[] = {
+    {"num_ways",        "Associativity of the cache."},
+    {"num_rows",        "How many cache rows."},
+    {"blocksize",       "Size of a cache block in bytes."},
+    {"num_upstream",    "How many upstream ports there are. Typically 1 or 0."},
+    {"next_level",      "Name of the next level cache"},
+    {"access_time",     "Time taken to lookup data in the cache."}
+};
+
+static const ElementInfoParam bus_params[] = {
+    {"numPorts",    "Number of Ports on the bus."},
+    {"busDelay",    "Delay time for the bus."}
+};
+
+static const ElementInfoParam memctrl_params[] = {
+    {"mem_size",    "Size of physical memory in MB"},
+    {"rangeStart",  "Address Range where physical memory begins"},
+    {"memory_file", "Optional backing-store file to pre-load memory, or store resulting state"},
+    {"clock",       "Clock frequency of controller"},
+    {"use_dramsim", "0 to not use DRAMSim, 1 to use DRAMSim"},
+    {"device_ini",  "Name of DRAMSim Device config file"},
+    {"system_ini",  "Name of DRAMSim Device system file"},
+    {"access_time", "When not using DRAMSim, latency of memory operation."}
+};
+
+static const ElementInfoParam cpu_params[] = {
+    {"workPerCycle",    "How much work to do per cycle."},
+    {"commFreq",        "How often to do a memory operation."},
+    {"memSize",         "Size of physical memory."},
+    {"do_write",        "Enable writes to memory (versus just reads)."},
+    {"num_loadstore",   "Stop after this many reads and writes."}
+};
+
 
 static const ElementInfoComponent components[] = {
 	{ "Cache",
-		"Simple Cache Component",
+		"Cache Component",
 		NULL,
-		create_Cache
+		create_Cache,
+        cache_params
 	},
 	{ "Bus",
 		"Mem Hierarchy Bus Component",
 		NULL,
-		create_Bus
-	},
-	{"trivialCPU",
-		"Simple Demo Component",
-		NULL,
-		create_trivialCPU
+		create_Bus,
+        bus_params
 	},
 	{"MemController",
-		"Simple Demo Component",
+		"Memory Controller Component",
 		NULL,
-		create_MemController
+		create_MemController,
+        memctrl_params
+	},
+	{"trivialCPU",
+		"Simple Demo CPU for testing",
+		NULL,
+		create_trivialCPU,
+        cpu_params
 	},
 	{ NULL, NULL, NULL, NULL }
 };
