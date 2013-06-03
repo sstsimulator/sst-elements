85221ddbf7da6190831bbfa3c382ec630fabe965
Si Hammond <simon.hammond@gmail.com>
2013-06-03 02:06:37 +0000

    Component definition and Makefile.

---
+++ sst/elements/vanadis/vanadis.cc
@@ -0,0 +1,40 @@
+
+#include "sst_config.h"
+#include "sst/core/serialization/element.h"
+#include <assert.h>
+
+#include "sst/core/element.h"
+#include "nbprefetch.h"
+
+using namespace SST;
+using namespace SST::Vanadis;
+
+static Component*
+create_NextBlockPrefetcher(SST::ComponentId_t id, 
+                  SST::Component::Params_t& params)
+{
+    return new NextBlockPrefetcher( id, params );
+}
+
+static const ElementInfoParam component_params[] = {
+    {"pending", "Maximum pending prefetch requests which can be in flight."},
+    { NULL, NULL}
+};
+
+static const ElementInfoComponent components[] = {
+    { "NextBlockPrefetcher",
+      "Prefetches block n+1 after block n miss",
+      NULL,
+      create_NextBlockPrefetcher,
+      component_params
+    },
+    { NULL, NULL, NULL, NULL }
+};
+
+extern "C" {
+    ElementLibraryInfo vanadis_eli = {
+        "Vanadis",
+        "Vanadis RISC Core",
+        components,
+    };
+}
