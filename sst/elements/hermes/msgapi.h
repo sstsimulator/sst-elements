970375e292c97842b0d26a80629e012ae3109292
Mike Levenhagen <mjleven@sandia.gov>
2013-06-26 18:10:16 +0000

    cleanup

--- sst/elements/hermes/msgapi.h
+++ sst/elements/hermes/msgapi.h
@@ -28,106 +28,108 @@ namespace SST {
 namespace Hermes {
 
 typedef void*    Addr;
-typedef uint32_t HermesCommunicator;
-typedef uint32_t HermesRankID;
+typedef uint32_t Communicator;
+typedef uint32_t RankID;
 
 typedef struct MessageResponse {
 } MessageResponse;
 
 typedef struct MessageRequest {
     uint32_t     tag;  
-    HermesRankID src; 
+    RankID src; 
 } MessageRequest;
 
 enum PayloadDataType {
-        HERMES_CHAR,
-	HERMES_INT,
-	HERMES_LONG,
-	HERMES_DOUBLE,
-	HERMES_FLOAT,
-	HERMES_COMPLEX
+        CHAR,
+	INT,
+	LONG,
+	DOUBLE,
+	FLOAT,
+	COMPLEX
 };
 
 enum ReductionOperation {
-	HERMES_SUM,
-	HERMES_MIN,
-	HERMES_MAX
+	SUM,
+	MIN,
+	MAX
+};
+
+typedef Arg_FunctorBase< int > Functor; 
+
+static const uint32_t AnyTag = -1;
+static const uint32_t AnySrc = -1;
+static const Communicator GroupWorld = 0;
+
+enum Retval {
+    SUCCESS,
+    FAILURE
 };
 
 class MessageInterface : public Module {
     public:
 
-    typedef Arg_FunctorBase< int > AbstractFunctor; 
-
-
     unsigned sizeofDataType( PayloadDataType type ) {
         switch( type ) {
-            case HERMES_CHAR:
+            case CHAR:
                 return sizeof( char ); 
-            case HERMES_INT:
+            case INT:
                 return sizeof( int ); 
-            case HERMES_LONG:
+            case LONG:
                 return sizeof( long ); 
-            case HERMES_DOUBLE:
+            case DOUBLE:
                 return sizeof( double); 
-            case HERMES_FLOAT:
+            case FLOAT:
                 return sizeof( float ); 
         }
         assert( 0 );
     }
 
 
-    static const uint32_t AnyTag = -1;
-    static const uint32_t AnySrc = -1;
-
-    enum Retval {
-        SUCCESS
-    };
 
     MessageInterface() {}
     virtual ~MessageInterface() {}
 
-    virtual void setOwningComponent(SST::Component* owner) {}
+    virtual void setOwningComponent(SST::Component* owner, int) {}
 
-    virtual void init(AbstractFunctor*) {}
-    virtual void fini(AbstractFunctor*) {}
-    virtual void rank(HermesCommunicator group, int* rank, AbstractFunctor*) {}
-    virtual void size(HermesCommunicator group, AbstractFunctor* ) {}
+    virtual void init(Functor*) {}
+    virtual void fini(Functor*) {}
+    virtual void rank(Communicator group, int* rank, Functor*) {}
+    virtual void size(Communicator group, int* size, Functor*) {}
 
     virtual void send(Addr payload, uint32_t count, PayloadDataType dtype, 
-        HermesRankID dest, uint32_t tag, HermesCommunicator group, 
-        AbstractFunctor* ) {}
+        RankID dest, uint32_t tag, Communicator group, 
+        Functor* ) {}
 
     virtual void isend(Addr payload, uint32_t count, PayloadDataType dtype, 
-        HermesRankID dest, uint32_t tag, HermesCommunicator group, 
-        MessageRequest* req, AbstractFunctor* ) {}
+        RankID dest, uint32_t tag, Communicator group, 
+        MessageRequest* req, Functor* ) {}
 
     virtual void recv(Addr target, uint32_t count, PayloadDataType dtype,
-        HermesRankID source, uint32_t tag, HermesCommunicator group, 
-        MessageResponse* resp, AbstractFunctor*) {}
+        RankID source, uint32_t tag, Communicator group, 
+        MessageResponse* resp, Functor*) {}
 
     virtual void irecv(Addr target, uint32_t count, PayloadDataType dtype, 
-        HermesRankID source, uint32_t tag, HermesCommunicator group, 
-        MessageRequest* req, AbstractFunctor*) {}
+        RankID source, uint32_t tag, Communicator group, 
+        MessageRequest* req, Functor*) {}
 
     virtual void allreduce(Addr mydata, Addr result, uint32_t count, 
         PayloadDataType dtype, ReductionOperation op, 
-        HermesCommunicator group, AbstractFunctor*) {}
+        Communicator group, Functor*) {}
 
     virtual void reduce(Addr mydata, Addr result, uint32_t count, 
-        PayloadDataType dtype, ReductionOperation op, HermesRankID root, 
-        HermesCommunicator group, AbstractFunctor*) {}
+        PayloadDataType dtype, ReductionOperation op, RankID root, 
+        Communicator group, Functor*) {}
 
-    virtual void barrier(HermesCommunicator group, AbstractFunctor*) {}
+    virtual void barrier(Communicator group, Functor*) {}
 
-    virtual int probe( HermesRankID source, uint32_t tag, 
-        HermesCommunicator group, MessageResponse* resp, AbstractFunctor* ) {} 
+    virtual int probe( RankID source, uint32_t tag, 
+        Communicator group, MessageResponse* resp, Functor* ) {} 
 
     virtual int wait( MessageRequest* req, MessageResponse* resp,
-        AbstractFunctor* ) {};
+        Functor* ) {};
 
     virtual int test( MessageRequest* req, int& flag, MessageResponse* resp,
-        AbstractFunctor* ) {};
+        Functor* ) {};
     
 };
 
