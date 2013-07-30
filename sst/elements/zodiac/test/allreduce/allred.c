5f593585fecdd7cb6bf16aa714bb29eb91642e4a
Si Hammond <simon.hammond@gmail.com>
2013-07-30 23:05:18 +0000

    Simple test for all reduce operations.

---
+++ sst/elements/zodiac/test/allreduce/allred.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(int argc, char* argv[]) {
+
+	MPI_Init(&argc, &argv);
+
+	int rank = 0;
+	int npes = 0;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &npes);
+
+	if(rank == 0) {
+		printf("SST Sirius Allreduce Test (Ranks: %d)\n", npes);
+	}
+
+	double my_value = 5.0;
+	double total = 0;
+
+	MPI_Allreduce(&my_value, &total, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
+	if(rank == 0) {
+		printf("Value should be: %f\n", (my_value * npes));
+		printf("Value is: %f\n", total);
+	}
+
+	MPI_Finalize();
+
+	return 0;
+}
