d4e9eab233f0f3c0210ef6698ae7ce38da5eebae
Chad Kersey <cdkersey@gatech.edu>
2013-08-07 18:52:08 +0000

    QSim component initial commit.

---
+++ sst/elements/qsimComponent/gen_sdl.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+
+N=${N:=1}
+APP=${APP:=$HOME/sst-simulator/qsim/benchmarks/splash2-tar/fft.tar}
+STATE=${STATE:=$HOME/qsim/state.$N}
+L1LAT=100ps
+IPILAT=10ns
+BUSLAT=10ns
+NETLAT=5ns
+
+CACHEBLOCK=${CACHEBLOCK:=64}
+L1WAYS=${L1WAYS:=4}
+L1SETS=${L1SETS:=16}
+L1ATIME=${L1ATIME:=0ps}
+
+L2WAYS=${L2WAYS:=16}
+L2SETS=${L2SETS:=32}
+L2ATIME=${L2ATIME:=10ns}
+
+RTR_LINK_BW=${RTR_LINK_BW:=5GHz}
+RTR_XBAR_BW=${RTR_XBAR_BW:=5GHz}
+NET_BW=${NET_BW:=5GHz}
+
+DRAM_ATIME=${DRAM_ATIME:=50ns}
+DRAM_CLOCK=${DRAM_CLOCK:=1GHz}
+
+print_qsim_cpu() {
+  j=`expr \( $i + 1 \) % $N`
+  h=`expr \( $i + $N - 1 \) % $N`
+
+  echo "  <component name=\"cpu$i\" rank=\"$i\" type=\"qsimComponent\">"
+  echo "    <params>"
+  echo "      <state>$STATE</state>"
+  echo "      <app>$APP</app>"
+  echo "      <hwthread>$i</hwthread>"
+  echo "    </params>"
+  echo "    <link name=c${i}_l1cache_link port=memLink latency=$L1LAT />"
+  echo "    <link name=ipi_ring_${i}_$j port=ipiRingOut latency=$IPILAT />"
+  echo "    <link name=ipi_ring_${h}_$i port=ipiRingIn latency=$IPILAT />"
+  echo "  </component>"
+  echo
+}
+
+print_trivial_cpu() {
+  echo "  <component name=\"cpu$i\" rank=\"$i\" type=\"memHierarchy.trivialCPU\">"
+  echo "    <params>"
+  echo "      <workPerCycle>1000</workPerCycle>"
+  echo "      <commFreq>100</commFreq>"
+  echo "      <memSize>0x8000000</memSize>"
+  echo "      <doWrite>1</doWrite>"
+  echo "    </params>"
+  echo "    <link name=c$i_l1cache_link port=memLink latency=$L1LAT />"
+  echo "  </component>"
+  echo
+}
+
+print_l1_cache() {
+  echo "  <component name=\"c${i}.l1cache\" rank=\"$i\" type=\"memHierarchy.Cache\">"
+  echo "    <params>"
+  echo "      <num_ways>$L1WAYS</num_ways>"
+  echo "      <num_rows>$L1SETS</num_rows>"
+  echo "      <blocksize>$CACHEBLOCK</blocksize>"
+  echo "      <access_time>$L1ATIME</access_time>"
+  echo "      <num_upstream>1</num_upstream>"
+  echo "      <next_level>c${i}.l2cache</next_level>"
+  echo "      <printStats>1</printStats>"
+  echo "    </params>"
+  echo "    <link name=c${i}_l1cache_link port=upstream0 latency=$L1LAT />"
+  echo "    <link name=c${i}_l1l2cache_link port=downstream latency=$BUSLAT />"
+  echo "  </component>"
+  echo
+}
+
+print_l2_cache() {
+  j=`expr $i + 1`
+
+  echo "  <component name=\"c${i}.l2cache\" rank=\"$i\" type=\"memHierarchy.Cache\">"
+  echo "    <params>"
+  echo "      <num_ways>$L2WAYS</num_ways>"
+  echo "      <num_rows>$L2SETS</num_rows>"
+  echo "      <blocksize>$CACHEBLOCK</blocksize>"
+  echo "      <access_time>$L2ATIME</access_time>"
+  echo "      <num_upstream>1</num_upstream>"
+  echo "      <net_addr>$j</net_addr>"
+  echo "      <mode>INCLUSIVE</mode>"
+  echo "      <printStats>1</printStats>"
+  echo "    </params>"
+  echo "    <link name=c${i}_l1l2cache_link port=upstream0 latency=$L1LAT />"
+  echo "    <link name=cache_net_${i} port=directory_link latency=$BUSLAT />"
+  echo "  </component>"
+  echo
+}
+
+print_network() {
+  O=`expr $N + 1`
+
+  echo "  <component name=\"chiprtr\" rank=\"0\" type=\"merlin.hr_router\">"
+  echo "    <params>"
+  echo "      <num_ports>$O</num_ports>"
+  echo "      <num_vcs>3</num_vcs>"
+  echo "      <link_bw>$RTR_LINK_BW</link_bw>"
+  echo "      <xbar_bw>$RTR_XBAR_BW</xbar_bw>"
+  echo "      <topology>merlin.singlerouter</topology>"
+  echo "      <id>0</id>"
+  echo "    </params>"
+  echo "    <link name=dir_net_0 port=port0 latency=$NETLAT />"
+
+  for j in `seq 1 $N`; do
+    h=`expr $j - 1`
+    echo "    <link name=cache_net_$h port=port$j latency=$NETLAT />"
+  done
+
+  echo "  </component>"
+  echo
+}
+
+print_dirmem() {
+  echo -n "  <component name=\"dirctrl0\" rank=\"0\""
+  echo       "type=\"memHierarchy.DirectoryController\">"
+  echo "    <params>"
+  echo "      <network_addr>0</network_addr>"
+  echo "      <network_bw>$NET_BW</network_bw>"
+  echo "      <addrRangeStart>0x0</addrRangeStart>"
+  echo "      <addrRangeEnd>0x18000000</addrRangeEnd>"
+  echo "      <backingStoreSize>0x8000000</backingStoreSize>"
+  echo "    </params>"
+  echo "    <link name=dir_mem_link_0 port=memory latency=$BUSLAT />"
+  echo "    <link name=dir_net_0 port=network latency=$NETLAT />"
+  echo "  </component>"
+  echo
+  echo "  <component name=\"memory0\" rank=\"0\" type=\"memHierarchy.MemController\">"
+  echo "    <params>"
+  echo "      <access_time>$DRAM_ATIME</access_time>"
+  echo "      <mem_size>512</mem_size>"
+  echo "      <clock>$DRAM_CLOCK</clock>"
+  echo "    </params>"
+  echo "    <link name=dir_mem_link_0 port=direct_link latency=$BUSLAT />"
+  echo "  </component>"
+}
+
+print_sep() {
+  echo "  <!--<><><><><><><><><><><><><><>-<><><><><><><><><><><><><><>-->"
+  echo
+}
+
+
+echo "<?xml version=\"1.0\"?>"
+echo "<sdl version=\"2.0\"/>"
+echo
+echo "<sst>"
+
+M=`expr $N - 1`
+
+# Generate the CPUs, either trivial_cpu or qsim_cpu
+for i in `seq 0 $M`; do print_qsim_cpu; done
+#for i in `seq 0 $M`; do print_trivial_cpu; done
+print_sep
+
+# Generate the L1 cache(s)
+for i in `seq 0 $M`; do print_l1_cache; done
+print_sep
+
+# Generate the L2 cache(s)
+for i in `seq 0 $M`; do print_l2_cache; done
+print_sep
+
+# Generate the network, directory, and RAM
+print_network
+print_dirmem
+
+echo "</sst>"
---
+++ sst/elements/qsimComponent/qsimComponent.cc
@@ -0,0 +1,274 @@
+// Copyright 2009-2013 Sandia Corporation. Under the terms
+// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2013, Sandia Corporation
+// All rights reserved.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#include "qsimComponent.h"
+
+#include <sst/core/debug.h>
+#include <sst/core/simulation.h>
+
+#include <iostream>
+
+#include <qsim.h>
+#include <qsim-load.h>
+
+using namespace SST;
+using namespace SST::Interfaces;
+using SST::QsimComponent::qsimComponent;
+using namespace Qsim;
+using namespace std;
+
+class IPIEvent : public Event {
+public:
+  IPIEvent(int dest, uint8_t vec): dest(dest), vec(vec) {}
+  int dest;
+  uint8_t vec;
+
+private:
+  friend class boost::serialization::access;
+  IPIEvent() {}
+  template <class A> void serialize(A &ar, const unsigned int version) {
+    ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Event);
+    ar & BOOST_SERIALIZATION_NVP(dest);
+    ar & BOOST_SERIALIZATION_NVP(vec);
+  }
+};
+
+qsimComponent::qsimComponent(ComponentId_t id, Params &p):
+  Component(id), icount(0), rcount(0), mcount(0), stalled(false),
+  translating(false), lock(0)
+{
+  out.init("", 0, 0, Output::STDOUT);
+
+  bool found;
+  stateFile = p.find_string("state", "", found);
+  if (!found) _abort(qsimComponent, "State file not provided\n");
+
+  appFile = p.find_string("app", "", found);
+  if (!found) _abort(qsimComponent, "Application .tar file not provided\n");
+
+  clockFreq = p.find_string("clock", "5GHz", found);
+
+  hwThreadId = p.find_integer("hwthread", 0, found);
+  if (!found) hwThreadId = 0;
+
+  registerAsPrimaryComponent();
+  primaryComponentDoNotEndSim();
+
+  typedef Event::Handler<qsimComponent> qc_eh;
+  memLink = configureLink("memLink",
+                          new qc_eh(this, &qsimComponent::handleEvent));
+  iMemLink = configureLink("iMemLink",
+                           new qc_eh(this, &qsimComponent::handleEvent));
+  ipiRingIn = configureLink("ipiRingIn",
+                            new qc_eh(this, &qsimComponent::handleEvent));
+  ipiRingOut = configureLink("ipiRingOut",
+                             new qc_eh(this, &qsimComponent::handleEvent));
+  assert(memLink); assert(ipiRingIn); assert(ipiRingOut);
+
+  typedef Clock::Handler<qsimComponent> qc_ch;
+  registerClock(clockFreq, new qc_ch(this, &qsimComponent::clockTick));
+  registerClock("1kHz", new qc_ch(this, &qsimComponent::timerTick));
+}
+
+// Used by serialization.
+qsimComponent::qsimComponent(): Component(-1) {}
+
+void qsimComponent::setup() {}
+
+void qsimComponent::init(unsigned phase) {
+  if (phase != 0) return;
+
+  osd = new OSDomain(stateFile.c_str());
+  osd->connect_console(cout);
+  out.output("qsimComponent %u init: loaded state\n", hwThreadId);
+  load_file(*osd, appFile.c_str());
+  out.output("qsimComponent %u init: loaded application\n", hwThreadId);
+
+  osd->set_atomic_cb(this, &qsimComponent::atomic_cb);
+  osd->set_int_cb(this, &qsimComponent::int_cb);
+  osd->set_inst_cb(this, &qsimComponent::inst_cb);
+  osd->set_mem_cb(this, &qsimComponent::mem_cb);
+  osd->set_trans_cb(this, &qsimComponent::trans_cb);
+  osd->set_magic_cb(this, &qsimComponent::magic_cb);
+
+  out.output("qsimComponent init: callbacks set\n");
+
+  if (hwThreadId == 0) {
+    out.output("qsimComponent %u init: sending init data\n", hwThreadId);
+    MemEvent *e = new MemEvent(this, 0, WriteReq);
+    e->setPayload(1024ul*1024*osd->get_ram_size_mb(),
+                  osd->get_ramdesc().mem_ptr);
+    memLink->sendInitData(e);
+    out.output("qsimComponent %u init: sent init data\n", hwThreadId);
+  }
+}
+
+void qsimComponent::finish() {
+  unsigned long simCycle(Simulation::getSimulation()->getCurrentSimCycle());  
+
+  out.output("%lu instructions executed, ", icount);
+  out.output("%lu outstanding reqs, %lu messages received,\n", rcount, mcount);
+  out.output("%lu sim cycles\n", simCycle);
+}
+
+void qsimComponent::handleEvent(Event *event) {
+  if (MemEvent *e = dynamic_cast<MemEvent*>(event)) {
+    if (--rcount == 0) stalled = false;
+    ++mcount;
+
+    for (unsigned i = 0; i < e->getPayload().size(); ++i)
+      osd->get_ramdesc().mem_ptr[e->getAddr() + i] = e->getPayload()[i];
+
+    delete e;
+  } else if (IPIEvent *e = dynamic_cast<IPIEvent*>(event)) {
+    if (e->dest == -1) {
+      out.output("qsimComponent %u handleEvent: End message.\n", hwThreadId);
+      unregisterExit();
+      ipiRingOut->send(e);
+    } else if (unsigned(e->dest) == hwThreadId) {
+      out.output("qsimComponent %u handleEvent: IPI arrived\n", hwThreadId);
+      osd->interrupt(hwThreadId, e->vec);
+      delete e;
+    } else {
+      ipiRingOut->send(e);
+    }
+  } else {
+    _abort(qsimComponent, "Received unknown event type.\n");
+  }
+}
+
+bool qsimComponent::clockTick(Cycle_t c) {
+  if (!stalled) osd->run(hwThreadId, 1);
+
+  return false;
+}
+
+bool qsimComponent::timerTick(Cycle_t c) {
+  osd->timer_interrupt();
+
+  return false;
+}
+
+int qsimComponent::atomic_cb(int c) {
+  lock = 1;
+  return 0;
+}
+
+int qsimComponent::mem_cb(int c, uint64_t v, uint64_t p, uint8_t s, int w) {
+  if (p >= 0xf0000000) return 0; // ignore memory-mapped APIC access
+
+  for (unsigned i = 0, increment; i < s; i += increment) {
+    if      (s-i >= 8 && ((p+i) & 7) == 0) increment = 8;
+    else if (s-i >= 4 && ((p+i) & 3) == 0) increment = 4;
+    else if (s-i >= 2 && ((p+i) & 1) == 0) increment = 2;
+    else increment = 1;
+
+    MemEvent *e = new MemEvent(this, p+i, w ? WriteReq : ReadReq);
+    e->setSize(increment);
+    if (v && ((lock == 1 && !w) || (lock == 2 && w)) && i == 0) {
+      e->setFlags(MemEvent::F_LOCKED);
+      e->setLockID(hwThreadId);
+
+      if (lock == 1 && !w) lock = 2;
+      if (lock == 2 && w) lock = 0;
+    }
+
+    if (w) e->setPayload(increment, osd->get_ramdesc().mem_ptr + p + i);
+
+    if (translating && !w && iMemLink) iMemLink->send(e);
+    else memLink->send(e);
+
+    stalled = !w;
+    ++rcount;
+  }
+
+  return !w;
+}
+
+void qsimComponent::trans_cb(int c) {
+  translating = true;
+}
+
+void qsimComponent::inst_cb(int c, uint64_t v, uint64_t p, uint8_t l,
+                            const uint8_t *b, enum inst_type t)
+{
+  translating = false;
+  pc = v;
+
+#if 0
+  for (unsigned i = 0, increment; i < l; i += increment) {
+    if      (l-i >= 8 && ((p+i) & 7) == 0) increment = 8;
+    else if (l-i >= 4 && ((p+i) & 3) == 0) increment = 4;
+    else if (l-i >= 2 && ((p+i) & 1) == 0) increment = 2;
+    else increment = 1;
+
+    MemEvent *e = new MemEvent(this, p + i, ReadReq);
+    e->setSize(increment);
+    if (iMemLink) iMemLink->send(e);
+    else memLink->send(e);
+    stalled = true;
+    ++rcount;
+  }
+#endif
+
+  ++icount;
+}
+
+int qsimComponent::int_cb(int c, uint8_t v) {
+  return 0;
+}
+
+int qsimComponent::magic_cb(int c, uint64_t a) {
+  if (a == 0xfa11dead) {
+    unregisterExit();
+    ipiRingOut->send(new IPIEvent(-1, 0));
+  }
+
+  if ((a & 0xff000000) == 0x1d000000) {
+    unsigned dest((a >> 8)&0xffff), vec((a & 0xff));
+    ipiRingOut->send(new IPIEvent(dest, vec));
+  }
+
+  return 0;
+}
+
+BOOST_CLASS_EXPORT(IPIEvent);
+BOOST_CLASS_EXPORT(qsimComponent);
+
+static Component* create_qsimComponent(ComponentId_t id, Params &p) {
+  return new qsimComponent(id, p);
+}
+
+static const ElementInfoParam component_params[] = {
+  {"state",    "QSim saved state file."},
+  {"app",      "Benchmark .tar file."},
+  {"clock",    "Clock rate (default 2GHz)."},
+  {"hwthread", "Hardware thread ID (default 0)."},
+  {NULL, NULL}
+};
+
+static const ElementInfoComponent components[] = {
+  { "qsimComponent",
+    "Distributed QSim CPU model component.",
+    NULL,
+    create_qsimComponent,
+    component_params
+  },
+  {NULL, NULL, NULL, NULL}
+};
+
+extern "C" {
+  ElementLibraryInfo qsimComponent_eli = {
+    "qsimComponent",
+    "Distributed QSim CPU model component.",
+    components,
+  };
+}
