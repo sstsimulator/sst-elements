475d0b3bac5c004fee7491d71ad1d92fd4fcdede
Arun F. Rodrigues <afrodi@sandia.gov>
2013-08-14 17:07:10 +0000

    updated copywrite and fixed formatting

--- sst/elements/VaultSimC/VaultSimC.h
+++ sst/elements/VaultSimC/VaultSimC.h
@@ -2,7 +2,7 @@
 // of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
 // Government retains certain rights in this software.
 // 
-// Copyright (c) 2009-2010, Sandia Corporation
+// Copyright (c) 2009-2010,2013, Sandia Corporation
 // All rights reserved.
 // 
 // This file is part of the SST software package. For license
