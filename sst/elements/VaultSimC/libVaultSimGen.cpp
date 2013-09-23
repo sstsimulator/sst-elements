78072923261a6b83b86bb651e7db8306aab1f261
Si Hammond <simon.hammond@gmail.com>
2013-09-23 20:06:22 +0000

    Fixes an issue with segmentation faults because end of parameter lists using NULL, NULL are not specified (meaning code walks through entire binary and then seg-faults).

--- sst/elements/VaultSimC/libVaultSimGen.cpp
+++ sst/elements/VaultSimC/libVaultSimGen.cpp
@@ -14,7 +14,8 @@ static const ElementInfoParam VaultSimC_params[] = {
   {"clock",              "Vault Clock Rate."},
   {"numVaults2",         "Number of bits to determine vault address (i.e. log_2(number of vaults per cube))"},
   {"VaultID",            "Vault Unique ID (Unique to cube)."},
-  {"debug",              "0 (default): No debugging, 1: STDOUT, 2: STDERR, 3: FILE."}
+  {"debug",              "0 (default): No debugging, 1: STDOUT, 2: STDERR, 3: FILE."},
+  { NULL, NULL }
 };
 
 static const ElementInfoParam logicLayer_params[] = {
@@ -24,7 +25,8 @@ static const ElementInfoParam logicLayer_params[] = {
   {"LL_MASK",            "Bitmask to determine 'ownership' of an address by a cube. A cube 'owns' an address if ((((addr >> LL_SHIFT) & LL_MASK) == llID) || (LL_MASK == 0)). LL_SHIFT is set in vaultGlobals.h and is 8 by default."},
   {"terminal",           "Is this the last cube in the chain?"},
   {"vaults",             "Number of vaults per cube."},
-  {"debug",              "0 (default): No debugging, 1: STDOUT, 2: STDERR, 3: FILE."}
+  {"debug",              "0 (default): No debugging, 1: STDOUT, 2: STDERR, 3: FILE."},
+  { NULL, NULL }
 };
 
 static const ElementInfoParam cpu_params[] = {
@@ -32,7 +34,8 @@ static const ElementInfoParam cpu_params[] = {
   {"threads",            "Number of simulated threads in cpu."},
   {"app",                "Synthetic Application. 0:miniMD-like 1:phdMesh-like. (See app.cpp for details)."},
   {"bwlimit",            "Maximum number of memory instructions issued by the processor per cycle. Note, each thread can only have at most 2 outstanding memory references at a time. "},
-  {"seed",               "Optional random number generator seed. If not defined or 0, uses srandomdev()."}
+  {"seed",               "Optional random number generator seed. If not defined or 0, uses srandomdev()."},
+  { NULL, NULL }
 };
 
 static const ElementInfoComponent components[] = {
