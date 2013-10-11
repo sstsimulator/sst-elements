8256f68f36e924dc0eb11cdccbbcad99f925be72
Si Hammond <simon.hammond@gmail.com>
2013-10-11 20:19:11 +0000

    Working input deck for two level memory

--- sst/elements/ariel/pysdl/tlvlstream.py
+++ sst/elements/ariel/pysdl/tlvlstream.py
@@ -11,7 +11,7 @@ def sstcreatemodel():
     sst.addcompparam(id, "verbose", "4")
     sst.addcompparam(id, "executable", "/home/sdhammo/subversion/sst-simulator/sst/elements/ariel/tool/test/tlvlstream/ministream")
     sst.addcompparam(id, "arieltool", "/home/sdhammo/subversion/sst-simulator/sst/elements/ariel/tool/arieltool.so")
-    sst.addcompparam(id, "fastmempagecount", "1024")
+    sst.addcompparam(id, "fastmempagecount", "131072")
 
     corecount = 1;
 
@@ -20,7 +20,7 @@ def sstcreatemodel():
     print "Simulation core count: ", corecount
 
     membus = sst.createcomponent("membus", "memHierarchy.Bus")
-    sst.addcompparam(membus, "numPorts", str(corecount + 1))
+    sst.addcompparam(membus, "numPorts", str(corecount + 2))
     sst.addcompparam(membus, "busDelay", "1ns")
 
     # Create a series of caches all linked to the Ariel "core"
@@ -42,15 +42,23 @@ def sstcreatemodel():
 
     # Put a link to memory from the memory bus
     sst.addcomplink(membus, "mem_bus_link", "port%d"%(corecount), "50ps")
+    sst.addcomplink(membus, "memNear_bus_link", "port%d"%(corecount+1), "50ps")
 
     memory = sst.createcomponent("memory", "memHierarchy.MemController")
     sst.addcompparam(memory, "access_time", "1000ns")
+    sst.addcompparam(memory, "rangeStart", "0x20000000")
     sst.addcompparam(memory, "mem_size", "512")
     sst.addcompparam(memory, "clock", "1GHz")
-    sst.addcompparam(memory, "use_dramsim", "0")
-    sst.addcompparam(memory, "device_ini", "DDR3_micron_32M_8B_x4_sg125.ini")
-    sst.addcompparam(memory, "system_ini", "system.ini")
+    sst.addcompparam(memory, "printStats", "1")
     sst.addcomplink(memory, "mem_bus_link", "snoop_link", "50ps")
+
+    nearMemory = sst.createcomponent("nearmemory", "memHierarchy.MemController")
+    sst.addcompparam(nearMemory, "access_time", "100ns")
+    sst.addcompparam(nearMemory, "rangeStart", "0x0")
+    sst.addcompparam(nearMemory, "clock", "1GHz")
+    sst.addcompparam(nearMemory, "mem_size", "512")
+    sst.addcompparam(nearMemory, "printStats", "1")
+    sst.addcomplink(nearMemory, "memNear_bus_link", "snoop_link", "50ps")
 
     print "done configuring SST"
 
