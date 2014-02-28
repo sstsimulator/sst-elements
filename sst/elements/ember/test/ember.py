5b26ef177d4ef0987ad8b6844d14be6d9652b5b3
Mike Levenhagen <mjleven@sandia.gov>
2014-02-28 21:38:34 +0000

    add generic motif python script

---
+++ sst/elements/ember/test/ember.py
@@ -0,0 +1,112 @@
+
+import sst
+from sst.merlin import *
+ 
+import sys,getopt
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+
+iterations = 1;
+msgSize = 0;
+motif = "PingPong"
+
+def main():
+    global iterations
+    global msgSize
+    global motif
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "", ["msgSize=","iter=","motif="])
+    except getopt.GetopError as err:
+        print str(err)
+        sys.exit(2)
+    for o, a in opts:
+        if o in ("--iter"):
+            iterations = a
+        elif o in ("--msgSize"):
+            msgSize = a
+        elif o in ("--motif"):
+            motif = a
+        else:
+            assert False, "unhandle option" 
+
+main()
+
+motif = "ember.Ember" + motif + "Generator"
+
+print motif
+
+sst.merlin._params["link_lat"] = "40ns"
+sst.merlin._params["link_bw"] = "560Mhz"
+sst.merlin._params["xbar_bw"] = "560Mhz"
+sst.merlin._params["input_latency"] = "25ns"
+sst.merlin._params["output_latency"] = "25ns"
+sst.merlin._params["input_buf_size"] = 128
+sst.merlin._params["output_buf_size"] = 128
+
+sst.merlin._params["num_dims"] = 3 
+sst.merlin._params["torus:shape"] = "2x2x2"
+sst.merlin._params["torus:width"] = "1x1x1"
+sst.merlin._params["torus:local_ports"] = 1
+
+nicParams = ({ 
+		"hermesParams.nicParams.debug" : 0,
+		"hermesParams.nicParams.verboseLevel": 2,
+		"hermesParams.nicParams.module" : "merlin.linkcontrol",
+		"hermesParams.nicParams.topology" : "merlin.torus",
+		"hermesParams.nicParams.num_vcs" : 2,
+		"hermesParams.nicParams.link_bw" : "560Mhz",
+		"hermesParams.nicParams.buffer_size" : 128,
+		"hermesParams.nicParams.txBusDelay_ns" : 150,
+		"hermesParams.nicParams.rxBusDelay_ns" : 150,
+		"hermesParams.nicParams.rxMatchDelay_ns" : 100,
+		"hermesParams.nicParams.txDelay_ns" : 100,
+	})
+
+driverParams = ({
+		"debug" : 0,
+		"verbose" : 2,
+		"bufLen" : 8,
+		"hermesModule" : "firefly.hades",
+		"msgapi" : "firefly.hades",
+		"printStats" : 1,
+		"generator" : motif,
+		"generatorParams.messagesize" : msgSize,
+		"generatorParams.iterations" : iterations,
+		"hermesParams.numRanks"  : 8,
+		"hermesParams.nidListFile" : "nidlist.txt",
+		"hermesParams.nicModule" : "firefly.nic",
+		"hermesParams.policy" : "adjacent",
+		"hermesParams.nodeParams.numCores" : 1,
+		"hermesParams.nodeParams.coreNum" : 0,
+		"hermesParams.functionSM.defaultDebug" : 0,
+		"hermesParams.functionSM.defaultVerbose" : 1,
+		"hermesParams.longMsgProtocol.shortMsgLength" : 40000,
+		"hermesParams.longMsgProtocol.debug" : 0,
+		"hermesParams.longMsgProtocol.verboseLevel" : 1,
+		"hermesParams.longMsgProtocol.matchDelay_ps" : 0,
+		"hermesParams.longMsgProtocol.memcpyDelay_ps" : 200,
+		"hermesParams.longMsgProtocol.txDelay_ns" : 300,
+		"hermesParams.longMsgProtocol.rxDelay_ns" : 300,
+		"hermesParams.longMsgProtocol.regRegionBaseDelay_ps" : 10000000,
+		"hermesParams.longMsgProtocol.regRegionPerByteDelay_ps" : 28,
+		"hermesParams.longMsgProtocol.regRegionXoverLength" : 4096,
+	})
+
+class EmberEP(EndPoint):
+	def getName(self):
+		return "EmberEP"
+	def prepParams(self):
+		pass
+	def build(self, nodeID, link, extraKeys):
+		ep = sst.Component("emberEP_" + str(nodeID), "ember.EmberEngine")
+		ep.addParams(nicParams)
+		ep.addParams(driverParams)
+		ep.addParam("hermesParams.nicParams.nid", nodeID)
+		ep.addLink(link, "rtr", "10ns")
+
+topo = topoTorus()
+topo.prepParams()
+endPoint = EmberEP()
+endPoint.prepParams()
+topo.build(endPoint)
+
