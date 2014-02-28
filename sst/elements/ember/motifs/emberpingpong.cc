1f9635daa84b8249afa590a47d821a6a6803d1ce
Si Hammond <simon.hammond@gmail.com>
2014-02-28 18:19:11 +0000

    Switch back to filtered Params object.

--- sst/elements/ember/motifs/emberpingpong.cc
+++ sst/elements/ember/motifs/emberpingpong.cc
@@ -6,8 +6,8 @@ using namespace SST::Ember;
 EmberPingPongGenerator::EmberPingPongGenerator(SST::Component* owner, Params& params) :
 	EmberGenerator(owner, params) {
 
-	messageSize = (uint32_t) params.find_integer("generatorParams.messagesize", 1024);
-	iterations = (uint32_t) params.find_integer("generatorParams.iterations", 1);
+	messageSize = (uint32_t) params.find_integer("messagesize", 1024);
+	iterations = (uint32_t) params.find_integer("iterations", 1);
 }
 
 void EmberPingPongGenerator::configureEnvironment(uint32_t pRank, uint32_t worldSize) {
