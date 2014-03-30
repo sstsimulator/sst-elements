99cf0243527a350fcc77c64bed3decaf8629d1d1
Si Hammond <simon.hammond@gmail.com>
2014-03-30 23:55:14 +0000

    Add an all-ping-pong to flood network.

---
+++ sst/elements/ember/motifs/emberallpingpong.cc
@@ -0,0 +1,44 @@
+
+#include "emberallpingpong.h"
+
+using namespace SST::Ember;
+
+EmberAllPingPongGenerator::EmberAllPingPongGenerator(SST::Component* owner, Params& params) :
+	EmberGenerator(owner, params) {
+
+	iterations = (uint32_t) params.find_integer("iterations", 1);
+	messageSize = (uint32_t) params.find_integer("messagesize", 128);
+
+	assert(messageSize > 0);
+}
+
+void EmberAllPingPongGenerator::configureEnvironment(const SST::Output* output, uint32_t rank, uint32_t worldSize) {
+	halfWorld = worldSize / 2;
+	assert(halfWorld > 2);
+
+	if(rank < halfWorld) {
+		commWithRank = rank + halfWorld;
+	} else {
+		commWithRank = rank - halfWorld;
+	}
+
+	myRank = rank;
+}
+
+void EmberAllPingPongGenerator::generate(const SST::Output* output, const uint32_t phase, std::queue<EmberEvent*>* evQ) {
+	if(phase < iterations) {
+               EmberRecvEvent* recvEv = new EmberRecvEvent(commWithRank, messageSize, 0, (Communicator) 0);
+               EmberSendEvent* sendEv = new EmberSendEvent(commWithRank, messageSize, 0, (Communicator) 0);
+
+		if(myRank < halfWorld) {
+                	evQ->push(sendEv);
+                	evQ->push(recvEv);
+		} else {
+	                evQ->push(recvEv);
+       			evQ->push(sendEv);
+		}
+	} else {
+		EmberFinalizeEvent* finalize = new EmberFinalizeEvent();
+                evQ->push(finalize);
+	}
+}
---
+++ sst/elements/ember/motifs/emberallpingpong.h
@@ -0,0 +1,33 @@
+
+#ifndef _H_EMBER_ALL_PINGPONG_MOTIF
+#define _H_EMBER_ALL_PINGPONG_MOTIF
+
+#include <sst/core/params.h>
+#include "embergen.h"
+#include "embersendev.h"
+#include "emberrecvev.h"
+
+namespace SST {
+namespace Ember {
+
+class EmberAllPingPongGenerator : public EmberGenerator {
+
+public:
+	EmberAllPingPongGenerator(SST::Component* owner, Params& params);
+	void configureEnvironment(const SST::Output* output, uint32_t rank, uint32_t worldSize);
+        void generate(const SST::Output* output, const uint32_t phase, std::queue<EmberEvent*>* evQ);
+	void finish(const SST::Output* output) { }
+
+private:
+	uint32_t iterations;
+	uint32_t commWithRank;
+	uint32_t messageSize;
+	uint32_t myRank;
+	uint32_t halfWorld;
+
+};
+
+}
+}
+
+#endif
