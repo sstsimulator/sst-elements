9ea3789e895e087bb69bd1995837462450b2c7df
John Van Dyke <jpvandy@sandia.gov>
2014-04-01 18:44:33 +0000

    Deleting Portals4 and Palacios from the trunk.  (Part two)

--- sst/elements/m5C/factory.cpp
+++ sst/elements/m5C/factory.cpp
@@ -34,7 +34,6 @@ extern "C" {
 	SimObject* create_PhysicalMemory( M5* /*Component**/, string name, Params& params );
 	SimObject* create_Syscall( M5* /*Component**/, string name, Params& params );
 	SimObject* create_O3Cpu( M5* /*Component**/, string name, Params& params );
-	SimObject* create_PtlNicMMIF( M5* /*Component**/, string name, Params& params );
 #ifdef HAVE_DRAMSIM
 	SimObject* create_DRAMSimWrap( M5* /*Component**/, string name, Params& params );
 #endif
@@ -91,8 +90,6 @@ Gem5Object_t* Factory::createWrappedObject( std::string name,
 	obj->memObject = create_Syscall( m_comp, name, params );
     } else if (type == "O3Cpu") {
 	obj->memObject = create_O3Cpu( m_comp, name, params);
-    } else if (type == "PtlNicMMIF") {
-	obj->memObject = create_PtlNicMMIF( m_comp, name, params);
     } else if (type == "DRAMSimWrap") {
 #ifdef HAVE_DRAMSIM
 	obj->memObject = create_DRAMSimWrap( m_comp, name, params);
