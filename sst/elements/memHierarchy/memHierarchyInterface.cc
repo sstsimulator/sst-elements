22a3809b8a00fa3d4ce815dd7447e5dfe190a54a
Cesar De La Paz <caesar.sst@gmail.com>
2014-04-09 20:07:53 +0000

    Added memHierarchy interdace skeleton file, and copyright warnings to memHierarchy classes

---
+++ sst/elements/memHierarchy/memHierarchyInterface.cc
@@ -0,0 +1,11 @@
+//
+//  memoryInterface.cpp
+//  SST
+//
+//  Created by Caesar De la Paz III on 4/9/14.
+//  Copyright (c) 2014 De la Paz, Cesar. All rights reserved.
+//
+
+#include <sst_config.h>
+#include <sst/core/serialization.h>
+#include "memHierarchyInterface.h"
---
+++ sst/elements/memHierarchy/memHierarchyInterface.h
@@ -0,0 +1,14 @@
+//
+//  memoryInterface.h
+//  SST
+//
+//  Created by Caesar De la Paz III on 4/9/14.
+//  Copyright (c) 2014 De la Paz, Cesar. All rights reserved.
+//
+
+#ifndef __SST__memoryInterface__
+#define __SST__memoryInterface__
+
+#include <iostream>
+
+#endif /* defined(__SST__memoryInterface__) */
