f54b7678457adc0c5f2746b842e5055bd7618c1c
Cesar De La Paz <caesar.sst@gmail.com>
2014-04-15 23:50:11 +0000

    memnic clock should return bool?

--- sst/elements/memHierarchy/memNIC.h
+++ sst/elements/memHierarchy/memNIC.h
@@ -1,8 +1,8 @@
-// Copyright 2013-2014 Sandia Corporation. Under the terms
+// Copyright 2013 Sandia Corporation. Under the terms
 // of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
 // Government retains certain rights in this software.
 //
-// Copyright (c) 2013-2014, Sandia Corporation
+// Copyright (c) 2013, Sandia Corporation
 // All rights reserved.
 //
 // This file is part of the SST software package. For license
@@ -174,7 +174,7 @@ public:
     void setup(void);
     void init(unsigned int phase);
     void finish(void);
-    void clock(void);
+    bool clock(void);
 
 
     void send(MemEvent *ev);
