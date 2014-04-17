d479a8f60121b745b3a8bc5aa06fdd63a1607232
Aaron Levine <allevin@sandia.gov>
2014-04-17 16:39:32 +0000

    Delete Copyright headers added in r7064 that causes sst_mcniagara to fail, test should be passing now.

--- sst/elements/sst_mcniagara/mcniagara/perf_cnt.h
+++ sst/elements/sst_mcniagara/mcniagara/perf_cnt.h
@@ -1,14 +1,3 @@
-// Copyright 2009-2014 Sandia Corporation. Under the terms
-// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
-// Government retains certain rights in this software.
-// 
-// Copyright (c) 2009-2014, Sandia Corporation
-// All rights reserved.
-// 
-// This file is part of the SST software package. For license
-// information, see the LICENSE file in the top level directory of the
-// distribution.
-
 #define	TOTAL_CYCLES	338942565218ULL
 #define	L2_MISSES	212489158ULL
 #define	L2_I_MISSES	92712ULL
