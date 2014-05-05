7b6a48553c2ed43aa05185e61c59fb4c482d723f
Arun F. Rodrigues <afrodi@sandia.gov>
2014-05-05 22:10:22 +0000

    Updated element Info

--- sst/elements/iris/iris.cc
+++ sst/elements/iris/iris.cc
@@ -44,23 +44,90 @@ create_trig_nic(SST::ComponentId_t id,
     return new SST::Iris::iris_trig_nic( id, params );
 }
 
+static const SST::ElementInfoParam routerParams[] = {
+  {"id", "network node ID for this router", "Required"},
+  {"ports", "number of ports for this router", "5"},
+  {"vcs", "Number of Virtual Channels for this Router", "6"},
+  {"credits","Starting number of flow control credits", "Required"},
+  {"Rc_scheme", "Routing Scheme. Options: \"TWONODE_ROUTING\", \"XY\", \"TORUS_ROUTING\", \"RING_ROUTING\"", "RING_ROUTING"},
+  {"no_nodes", "Number of nodes in the network", "64"},
+  {"grid_size", "Size of grid", "8"},
+  {"buffer_size", "Router buffer size in flits", "Required"},
+  {NULL, NULL, NULL}
+};
+
+const char *ire[] = {
+  "irisRtrEvent",
+  NULL
+};
+
+static const SST::ElementInfoPort routerPorts[] = {
+  {"nic","Connection to the IRIS NIC",ire},
+  {"xPos","Postitive X direction",ire},
+  {"xNeg","Negative X direction",ire},
+  {"yPos","Positive Y direction",ire},
+  {"yNeg","Negative Y direction",ire},
+  {"zPos","Positive Z direction",ire},
+  {"zNeg","Negative Z",ire},
+  {NULL, NULL, NULL}
+};
+
+static const SST::ElementInfoParam niParams[] = {
+  {"id", "network node ID for this NIC", "Required"},
+  {"vcs", "Number of Virtual Channels", "6"},
+  {"terminal_credits","Number of terminal (e.g. towards the cpu) control credits", "Required"},
+  {"rtr_credits","Number of router flow control credits", "Required"},
+  {"buffer_size", "Router buffer size in flits", "Required"},
+  {NULL, NULL, NULL}
+};
+
+static const SST::ElementInfoPort niPorts[] = {
+  {"cpu","Terminal link from CPU/Node to NIC",ire},
+  {"rtr","Link to the router",ire},
+  {NULL, NULL, NULL}
+};
+
+
+static const SST::ElementInfoParam trigParams[] = {
+  {"latency"," Through NIC latency","Required"},
+  {"timing_set","Three options for sets of timing parameters. See tric_nic/tric_nic.cc setTimingParams() for details.","Required"},
+  {NULL, NULL, NULL}
+};
+
+static const SST::ElementInfoPort trigPorts[] = {
+  {"cpu","Link to the router",ire},
+  {NULL, NULL, NULL}
+};
+
 static const SST::ElementInfoComponent components[] = {
     { "router",
       "Iris Router",
       NULL,
       create_router,
+      routerParams, 
+      routerPorts
     },
     { "ninterface",
       "Interface for iris terminal and router",
       NULL,
       create_ninterface,
+      niParams,
+      niPorts
     },
     { "trig_nic",
-      "Trig NIC",
+      "Triggered operation NIC",
       NULL,
       create_trig_nic,
+      trigParams,
+      trigPorts
     },
-    { NULL, NULL, NULL, NULL }
+    { NULL, NULL, NULL, NULL, NULL, NULL }
+};
+
+static const SST::ElementInfoEvent events[] = {
+  { "irisRtrEvent", "Router to Router Event. Contains an iris packet.", NULL, NULL },
+  { "irisFlitEvent", "Event containing a single flit.", NULL, NULL },
+  { NULL, NULL, NULL, NULL }
 };
 
 extern "C" {
@@ -68,6 +135,12 @@ extern "C" {
         "iris",
         "IRIS Network Router",
         components,
+	events, // Events
+	NULL, // Introspectors
+	NULL, // Modules
+	NULL, // partitioners
+	NULL, // generators
+	NULL, // Python Module generator
     };
 }
 #endif   /* ----- #ifndef IRIS_SST_LIB_CC_INC  ----- */
