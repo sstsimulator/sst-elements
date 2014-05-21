8354157226d9df1f6457727e8f4ae9c2df4904d3
Si Hammond <simon.hammond@gmail.com>
2014-05-21 15:36:38 +0000

    Adds a 3-dimensional sweep motif to the Ember collection.

---
+++ sst/elements/ember/motifs/embersweep3d.cc
@@ -0,0 +1,155 @@
+// Copyright 2009-2014 Sandia Corporation. Under the terms
+// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2014, Sandia Corporation
+// All rights reserved.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+
+#include <sst_config.h>
+
+#include <sst/core/component.h>
+#include <sst/core/params.h>
+#include <sst/core/math/sqrt.h>
+
+#include "embersweep3d.h"
+
+using namespace SST::Ember;
+
+EmberSweep3DGenerator::EmberSweep3DGenerator(SST::Component* owner, Params& params) :
+	EmberMessagePassingGenerator(owner, params) {
+
+	nsCompute = (uint64_t) params.find_integer("computetime", 1000);
+
+	px = (uint32_t) params.find_integer("px", 0);
+	py = (uint32_t) params.find_integer("py", 0);
+
+	iterations = (uint32_t) params.find_integer("iterations", 1);
+
+	nx  = (uint32_t) params.find_integer("nx", 50);
+	ny  = (uint32_t) params.find_integer("ny", 50);
+	nz  = (uint32_t) params.find_integer("nz", 50);
+	kba = (uint32_t) params.find_integer("kba", 1);
+
+	// Check K-blocking factor is acceptable for dividing the Nz dimension
+	assert(nz % kba == 0);
+}
+
+void EmberSweep3DGenerator::configureEnvironment(const SST::Output* output, uint32_t pRank, uint32_t worldSize) {
+	rank = pRank;
+	size = worldSize;
+
+	// Check that we are using all the processors or else lock up will happen :(.
+	assert( (px * py) == worldSize );
+
+	int32_t myX = 0;
+	int32_t myY = 0;
+
+	// Get our position in a 2D processor array
+	getPosition(rank, px, py, &myX, &myY);
+
+	x_up   = (myX != (px - 1)) ? myX + 1 : -1;
+	x_down = (myX != 0) ? myX - 1 : -1;
+
+	y_up   = (myY != (py - 1)) ? myY + 1 : -1;
+	y_down = (myY != 0) ? myY - 1 : -1;
+
+	output->verbose(CALL_INFO, 1, 0, "Rank: %" PRIu32 " is located at coordinations of (%" PRId32 ", %" PRId32 ") in the 2D decomposition, X+: %" PRId32 ",X-:%" PRId32 ",Y+:%" PRId32 ",Y-:%" PRId32 "\n",
+		rank, myX, myY, x_up, x_down, y_up, y_down);
+}
+
+void EmberSweep3DGenerator::generate(const SST::Output* output, const uint32_t phase, std::queue<EmberEvent*>* evQ) {
+	if(phase < iterations) {
+		// Sweep from (0, 0) outwards towards (Px, Py)
+		for(uint32_t i = 0; i < nz; i+= kba) {
+			if(x_down > 0) {
+				evQ->push( new EmberRecvEvent(x_down, (nx * kba), 0, (Communicator) 0) );
+			}
+
+			if(y_down > 0) {
+				evQ->push( new EmberRecvEvent(y_down, (ny * kba), 0, (Communicator) 0) );
+			}
+
+			evQ->push(new EmberComputeEvent(nsCompute));
+
+			if(x_up > 0) {
+                                evQ->push( new EmberSendEvent(x_up, (nx * kba), 0, (Communicator) 0) );
+                        }
+
+                        if(y_up > 0) {
+                                evQ->push( new EmberRecvEvent(y_up, (ny * kba), 0, (Communicator) 0) );
+                        }
+		}
+
+		// Sweep from (Px, 0) outwards towards (0, Py)
+		for(uint32_t i = 0; i < nz; i+= kba) {
+			if(x_up > 0) {
+				evQ->push( new EmberRecvEvent(x_up, (nx * kba), 0, (Communicator) 0) );
+			}
+
+			if(y_down > 0) {
+				evQ->push( new EmberRecvEvent(y_down, (ny * kba), 0, (Communicator) 0) );
+			}
+
+			evQ->push(new EmberComputeEvent(nsCompute));
+
+			if(x_down > 0) {
+                                evQ->push( new EmberSendEvent(x_down, (nx * kba), 0, (Communicator) 0) );
+                        }
+
+                        if(y_up > 0) {
+                                evQ->push( new EmberRecvEvent(y_up, (ny * kba), 0, (Communicator) 0) );
+                        }
+		}
+
+		// Sweep from (Px,Py) outwards towards (0,0)
+		for(uint32_t i = 0; i < nz; i+= kba) {
+			if(x_up > 0) {
+				evQ->push( new EmberRecvEvent(x_up, (nx * kba), 0, (Communicator) 0) );
+			}
+
+			if(y_up > 0) {
+				evQ->push( new EmberRecvEvent(y_up, (ny * kba), 0, (Communicator) 0) );
+			}
+
+			evQ->push(new EmberComputeEvent(nsCompute));
+
+			if(x_down > 0) {
+                                evQ->push( new EmberSendEvent(x_down, (nx * kba), 0, (Communicator) 0) );
+                        }
+
+                        if(y_down > 0) {
+                                evQ->push( new EmberRecvEvent(y_down, (ny * kba), 0, (Communicator) 0) );
+                        }
+		}
+
+		// Sweep from (0, Py) outwards towards (Px, 0)
+		for(uint32_t i = 0; i < nz; i+= kba) {
+			if(x_down > 0) {
+				evQ->push( new EmberRecvEvent(x_down, (nx * kba), 0, (Communicator) 0) );
+			}
+
+			if(y_up > 0) {
+				evQ->push( new EmberRecvEvent(y_up, (ny * kba), 0, (Communicator) 0) );
+			}
+
+			evQ->push(new EmberComputeEvent(nsCompute));
+
+			if(x_up > 0) {
+                                evQ->push( new EmberSendEvent(x_up, (nx * kba), 0, (Communicator) 0) );
+                        }
+
+                        if(y_down > 0) {
+                                evQ->push( new EmberRecvEvent(y_down, (ny * kba), 0, (Communicator) 0) );
+                        }
+		}
+	} else {
+		// We are done, tell Ember to finalize the MPI and stop the processing on this motif
+                EmberFinalizeEvent* finalize = new EmberFinalizeEvent();
+                evQ->push(finalize);
+	}
+}
---
+++ sst/elements/ember/motifs/embersweep3d.h
@@ -0,0 +1,54 @@
+// Copyright 2009-2014 Sandia Corporation. Under the terms
+// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2014, Sandia Corporation
+// All rights reserved.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+
+#ifndef _H_EMBER_SWEEP_3D
+#define _H_EMBER_SWEEP_3D
+
+#include "embergen.h"
+#include "embermpigen.h"
+
+namespace SST {
+namespace Ember {
+
+class EmberSweep3DGenerator : public EmberMessagePassingGenerator {
+
+public:
+	EmberSweep3DGenerator(SST::Component* owner, Params& params);
+	void configureEnvironment(const SST::Output* output, uint32_t rank, uint32_t worldSize);
+        void generate(const SST::Output* output, const uint32_t phase, std::queue<EmberEvent*>* evQ);
+        void finish(const SST::Output* output) { }
+
+private:
+	uint32_t px;
+	uint32_t py;
+	uint32_t rank;
+	uint32_t size;
+	uint32_t nx;
+	uint32_t ny;
+	uint32_t nz;
+	uint32_t kba;
+
+	int32_t  x_up;
+	int32_t  x_down;
+	int32_t  y_up;
+	int32_t  y_down;
+
+	uint32_t nsCompute;
+	uint32_t messageSize;
+	uint32_t iterations;
+
+};
+
+}
+}
+
+#endif
