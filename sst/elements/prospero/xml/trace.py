e49db14d10462b0bd2a56c96db9c7443e74ce94d
John Van Dyke <jpvandy@sandia.gov>
2014-07-22 17:10:39 +0000

    Prospero test Suite and Input files to Python

--- sst/elements/prospero/xml/trace.py
+++ sst/elements/prospero/xml/trace.py
@@ -2,34 +2,32 @@
 import sst
 
 # Define SST core options
-sst.setProgramOption("timebase", "1 ps")
+sst.setProgramOption("timebase", "1ns")
 sst.setProgramOption("stopAtCycle", "1000000ns")
 
 # Define the simulation components
-cpu = sst.Component("cpu", "prospero.prospero")
-cpu.addParams({
-      "outputlevel" : """0""",
+comp_cpu = sst.Component("cpu", "prospero.prospero")
+comp_cpu.addParams({
+      "physlimit" : """512""",
       "tracetype" : """file""",
-      "trace" : """traces/pinatrace.out""",
+      "outputlevel" : """0""",
       "cache_line" : """64""",
-      "physlimit" : """512"""
+      "trace" : """traces/pinatrace.out"""
 })
-l1cache = sst.Component("l1cache", "memHierarchy.Cache")
-l1cache.addParams({
-      "cache_frequency" : """2 Ghz""",
-      "cache_size" : """64 KB""",
-      "coherence_protocol" : """MSI""",
-      "replacement_policy" : """lru""",
-      "associativity" : """8""",
+comp_l1cache = sst.Component("l1cache", "memHierarchy.Cache")
+comp_l1cache.addParams({
       "access_latency_cycles" : """1""",
-      "low_network_links" : """1""",
+      "cache_frequency" : """2 Ghz""",
+      "replacement_policy" : """lru""",
+      "coherence_protocol" : """MSI""",
+      "associativity" : """8""",
       "cache_line_size" : """64""",
-      "mshr_num_entries" : """4096""",
+      "statistics" : """1""",
       "L1" : """1""",
-      "statistics" : """1"""
+      "cache_size" : """64 KB"""
 })
-memory = sst.Component("memory", "memHierarchy.MemController")
-memory.addParams({
+comp_memory = sst.Component("memory", "memHierarchy.MemController")
+comp_memory.addParams({
       "coherence_protocol" : """MSI""",
       "access_time" : """1000 ns""",
       "mem_size" : """512""",
@@ -38,8 +36,8 @@ memory.addParams({
 
 
 # Define the simulation links
-cpu_cache_link = sst.Link("cpu_cache_link")
-cpu_cache_link.connect( (cpu, "cache_link", "1000ps"), (l1cache, "high_network_0", "1000ps") )
-mem_bus_link = sst.Link("mem_bus_link")
-mem_bus_link.connect( (l1cache, "low_network_0", "50ps"), (memory, "direct_link", "50ps") )
+link_cpu_cache_link = sst.Link("link_cpu_cache_link")
+link_cpu_cache_link.connect( (comp_cpu, "cache_link", "1000ps"), (comp_l1cache, "high_network_0", "1000ps") )
+link_mem_bus_link = sst.Link("link_mem_bus_link")
+link_mem_bus_link.connect( (comp_l1cache, "low_network_0", "50ps"), (comp_memory, "direct_link", "50ps") )
 # End of generated output.
