69b1dd1a0e80b67686ca6b39845438b91acd498c
Si Hammond <simon.hammond@gmail.com>
2014-07-28 01:50:45 +0000

    API to allow applications to control profiling inside Prospero tracing.

---
+++ sst/elements/prospero/tracetool/api/prospero.c
@@ -0,0 +1,11 @@
+
+#include "prospero.h"
+
+void prospero_enable_tracing() {
+
+}
+
+void prospero_disable_tracing() {
+
+}
+
---
+++ sst/elements/prospero/tracetool/api/prospero.h
@@ -0,0 +1,16 @@
+
+#ifndef _H_SST_PROSPERO_API
+#define _H_SST_PROSPERO_API
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void prospero_enable_tracing();
+void prospero_disable_tracing();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
