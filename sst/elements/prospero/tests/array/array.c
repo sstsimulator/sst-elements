67c2ed1500ebe784701d6162a034da17ecf85b36
Si Hammond <simon.hammond@gmail.com>
2014-07-29 21:15:57 +0000

    Tests for Prospero to use in checking the trace tool.

---
+++ sst/elements/prospero/tests/array/array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+	printf("Running arrays...\n");
+
+	const int SIZE = 4096;
+	int i;
+	double sum = 0;
+	double* the_array = (double*) malloc(sizeof(double) * SIZE);
+
+	for(i = 0; i < SIZE; ++i) {
+		the_array[i] = ((double) i) * 2.5;
+	}
+
+	for(i = 0; i < SIZE; ++i) {
+		sum += the_array[i];
+	}
+
+	printf("Total is %10.5f\n", sum);
+
+	free(the_array);
+}
