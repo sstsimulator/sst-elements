94f9244b76173caebdabffc204c545dd5d4c5770
K. Scott Hemmert <kshemme@sandia.gov>
2014-08-13 17:09:51 +0000

    Reworked partitioners to simply main.  Partitioners now all inherent from SSTPartitioner which requires them to provide some information about how they're used.  Each partitioner registers itself with the core.  The --help option automatically picks up the registered partitioners along with their descriptions.  The code path in main.cc is now unified for all paritioners.  Partitioners in element libraries must now also inherit from SSTPartitioner.

--- sst/elements/portals4_sm/portals4_sm.cc
+++ sst/elements/portals4_sm/portals4_sm.cc
@@ -23,6 +23,7 @@
 #include <stdarg.h>
 
 #include "portals_args.h"
+#include "part.h"
 #include <boost/tokenizer.hpp>
 #include <boost/foreach.hpp>
 
@@ -55,78 +56,6 @@ static string str(const char* format, ...) {
     return ret;
 }
 
-
-static void partition(ConfigGraph* graph, int ranks) {
-    int sx=0, sy=0, sz=0;
-
-        
-    // Need to look through components until we find a router so we
-    // can get the dimensions of the torus
-    ConfigComponentMap_t& comps = graph->getComponentMap();
-    for ( ConfigComponentMap_t::iterator iter = comps.begin();
-                            iter != comps.end(); ++iter )
-    {
-	ConfigComponent* ccomp = &(*iter);
-	if ( ccomp->type == "SS_router.SS_router" ) {
-	    sx = ccomp->params.find_integer("network.xDimSize");
-	    sy = ccomp->params.find_integer("network.yDimSize");
-	    sz = ccomp->params.find_integer("network.zDimSize");
-	    if ( sx == -1 || sy == -1 || sz == -1 ) {
-		printf("SS_router found without properly set network dimensions, aborting...\n");
-		abort();
-	    }
-	    break;
-	}
-    }
-    
-    for ( ConfigComponentMap_t::iterator iter = comps.begin();
-                            iter != comps.end(); ++iter )
-    {
-	ConfigComponent* ccomp = &(*iter);
-	int id = ccomp->params.find_integer("id");
-	if ( id == -1 ) {
-	    printf("Couldn't find id for component %s, aborting...\n",ccomp->name.c_str());
-	    abort();
-	}
-	// Get the x, y and z dimensions for the given id.
-        int x, y, z;
-
-	z = id / (sx * sy);
-	y = (id / sx) % sy;
-	x = id % sx;
-
-	/* Partition logic */
-        int rank = id % ranks;
-        if ( ranks == 2 ) {
-            if ( x < sx/2 ) rank = 0;
-            else rank = 1;
-        }
-        if ( ranks == 4 ) {
-            rank = 0;
-            if ( x >= sx/2 ) rank = rank | 1;
-            if ( y >= sy/2 ) rank = rank | (1 << 1);
-        }
-        if ( ranks == 8 ) {
-            rank = 0;
-            if ( x >= sx/2 ) rank = rank | 1;
-            if ( y >= sy/2 ) rank = rank | (1 << 1);
-            if ( z >= sz/2 ) rank = rank | (1 << 2);
-        }
-
-        if ( ranks == 16 ) {
-            rank = 0;
-	    if ( x >= 3*(sx/4) ) rank = rank | 3;
-	    else if ( x >= sx/2 && x < 3*(sx/4) ) rank = rank | 2;
-	    else if ( x >= sx/4 && x < sx/2 ) rank = rank | 1;
-            if ( y >= sy/2 ) rank = rank | (1 << 2);
-            if ( z >= sz/2 ) rank = rank | (1 << 3);
-        }
-
-	ccomp->rank = rank;
-	
-    }    
-
-}
 
 using namespace boost;
 
@@ -319,7 +248,7 @@ static const ElementInfoPartitioner partitioners[] = {
     { "partitioner",
       "Partitioner for portals4_sm simulations",
       NULL,
-      partition,
+      Portals4Partition::allocate,
     },
     { NULL, NULL, NULL, NULL }
 };
