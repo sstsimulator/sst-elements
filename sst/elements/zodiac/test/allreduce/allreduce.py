a855cf24a1b289e0da9250db61d98f710eca1528
Mike Levenhagen <mjleven@sandia.gov>
2014-09-18 15:39:59 +0000

    update configuration

--- sst/elements/zodiac/test/allreduce/allreduce.py
+++ sst/elements/zodiac/test/allreduce/allreduce.py
@@ -106,20 +106,39 @@ driverParams = ({
 		"hermesParams.nicModule" : "firefly.VirtNic",
 		"hermesParams.nicParams.debug" : 0,
 		"hermesParams.nicParams.debugLevel" : 1 ,
-		"hermesParams.functionSM.defaultDebug" : 0,
-		"hermesParams.functionSM.defaultVerbose" : 1,
-		"hermesParams.ctrlMsg.debug" : 0,
-		"hermesParams.ctrlMsg.verboseLevel" : 1,
-		"hermesParams.longMsgProtocol.shortMsgLength" : 4000,
-		"hermesParams.longMsgProtocol.debug" : 0,
-		"hermesParams.longMsgProtocol.verboseLevel" : 1,
-		"hermesParams.longMsgProtocol.matchDelay_ps" : 1,
-		"hermesParams.longMsgProtocol.memcpyDelay_ps" : 200,
-		"hermesParams.longMsgProtocol.txDelay_ns" : 300,
-		"hermesParams.longMsgProtocol.rxDelay_ns" : 300,
-		"hermesParams.longMsgProtocol.regRegionBaseDelay_ps" : 10000000,
-		"hermesParams.longMsgProtocol.regRegionPerByteDelay_ps" : 28,
-		"hermesParams.longMsgProtocol.regRegionXoverLength" : 4096,
+        "hermesParams.functionSM.defaultEnterLatency" : 30000,
+        "hermesParams.functionSM.defaultReturnLatency" : 30000,
+        "hermesParams.functionSM.defaultDebug" : debug,
+        "hermesParams.functionSM.defaultVerbose" : 2,
+        "hermesParams.ctrlMsg.debug" : debug,
+        "hermesParams.ctrlMsg.verboseLevel" : 2,
+        "hermesParams.ctrlMsg.shortMsgLength" : 12000,
+        "hermesParams.ctrlMsg.matchDelay_ns" : 150,
+
+        "hermesParams.ctrlMsg.txSetupMod" : "firefly.LatencyMod",
+        "hermesParams.ctrlMsg.txSetupModParams.range.0" : "0-:130ns",
+
+        "hermesParams.ctrlMsg.rxSetupMod" : "firefly.LatencyMod",
+        "hermesParams.ctrlMsg.rxSetupModParams.range.0" : "0-:100ns",
+
+        "hermesParams.ctrlMsg.txMemcpyMod" : "firefly.LatencyMod",
+        "hermesParams.ctrlMsg.txMemcpyModParams.op" : "Mult",
+        "hermesParams.ctrlMsg.txMemcpyModParams.range.0" : "0-:344ps",
+
+        "hermesParams.ctrlMsg.rxMemcpyMod" : "firefly.LatencyMod",
+        "hermesParams.ctrlMsg.txMemcpyModParams.op" : "Mult",
+        "hermesParams.ctrlMsg.rxMemcpyModParams.range.0" : "0-:344ps",
+
+        "hermesParams.ctrlMsg.txNicDelay_ns" : 0,
+        "hermesParams.ctrlMsg.rxNicDelay_ns" : 0,
+        "hermesParams.ctrlMsg.sendReqFiniDelay_ns" : 0,
+        "hermesParams.ctrlMsg.sendAckDelay_ns" : 0,
+        "hermesParams.ctrlMsg.regRegionBaseDelay_ns" : 3000,
+        "hermesParams.ctrlMsg.regRegionPerPageDelay_ns" : 100,
+        "hermesParams.ctrlMsg.regRegionXoverLength" : 4096,
+        "hermesParams.loadMap.0.start" : 0,
+        "hermesParams.loadMap.0.len" : 2,
+
 	})
 
 class EmberEP(EndPoint):
