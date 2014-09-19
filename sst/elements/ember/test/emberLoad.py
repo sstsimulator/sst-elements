363b0e26f1cf9915f542ebd1bb6e8fa273c9cc4e
John Van Dyke <jpvandy@sandia.gov>
2014-09-19 21:37:06 +0000

    Reverse inadvertent check in

--- sst/elements/ember/test/emberLoad.py
+++ sst/elements/ember/test/emberLoad.py
@@ -112,7 +112,7 @@ sst.merlin._params["output_buf_size"] = netBufSize
 sst.merlin._params.update( topoInfo.getNetworkParams() )
 
 _nicParams = { 
-		"debug" : 1,
+		"debug" : debug,
 		"verboseLevel": 1,
 		"module" : "merlin.linkcontrol",
 		"topology" : "merlin." + topology,
