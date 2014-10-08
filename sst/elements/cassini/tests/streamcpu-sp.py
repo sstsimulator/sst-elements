071d73d339b921c3b75edf0d2377040c8e9f817f
John Van Dyke <jpvandy@sandia.gov>
2014-10-08 18:16:20 +0000

    Switch the cassini prefetcher test input from xml to python

--- sst/elements/cassini/tests/streamcpu-sp.py
+++ sst/elements/cassini/tests/streamcpu-sp.py
@@ -1,55 +1,46 @@
+# Automatically generated SST Python input
 import sst
 
+# Define SST core options
 sst.setProgramOption("timebase", "1ns")
+sst.setProgramOption("stopAtCycle", "0 ns")
 
-cpu = sst.Component("cpu", "memHierarchy.streamCPU")
-cpu.addParams({
-		"verbose" : "0",
-		"workPerCycle" : "1000",
-		"commFreq" : "100",
-		"memSize" : "0x1000000",
-		"do_write" : "1",
-		"num_loadstore" : "100000"
-	})
+# Define the simulation components
+comp_cpu = sst.Component("cpu", "memHierarchy.streamCPU")
+comp_cpu.addParams({
+      "workPerCycle" : """1000""",
+      "do_write" : """1""",
+      "num_loadstore" : """100000""",
+      "commFreq" : """100""",
+      "memSize" : """0x1000"""
+})
+comp_l1cache = sst.Component("l1cache", "memHierarchy.Cache")
+comp_l1cache.addParams({
+      "access_latency_cycles" : """2""",
+      "cache_frequency" : """2 Ghz""",
+      "replacement_policy" : """lru""",
+      "coherence_protocol" : """MSI""",
+      "associativity" : """4""",
+      "cache_line_size" : """64""",
+      "prefetcher" : """cassini.StridePrefetcher""",
+      "debug" : """1""",
+      "low_network_links" : """1""",
+      "statistics" : """1""",
+      "L1" : """1""",
+      "cache_size" : """8 KB"""
+})
+comp_memory = sst.Component("memory", "memHierarchy.MemController")
+comp_memory.addParams({
+      "coherence_protocol" : """MSI""",
+      "access_time" : """1000 ns""",
+      "mem_size" : """512""",
+      "clock" : """1GHz"""
+})
 
-l1cache = sst.Component("l1cache", "memHierarchy.Cache")
-l1cache.addParams({
-		"cache_frequency" : "1GHz",
-		"associativity" : "4",
-		"cache_size" : "1MB", 
-		"cache_line_size" : "64",
-		"num_ways" : "4",
-		"num_rows" : "32",
-		"blocksize" : "64",
-		"access_latency_cycles" : "4",
-		"prefetcher" : "cassini.StridePrefetcher",
-		"prefetcher:verbose" : "0", 
-		"strideprefetcher:reach" : "4",
-		"coherence_protocol" : "MESI",
-#		"prefetcher" : "cassini.NextBlockPrefetcher",
-		"access_time" : "2ns",
-		"num_upstream" : "1",
-		"printStats" : "1"
-	})
 
-membus = sst.Component("membus", "memHierarchy.Bus")
-membus.addParams({
-		"numPorts" : "2",
-		"busDelay" : "20ns"
-	})
-
-memory = sst.Component("memory", "memHierarchy.MemController")
-memory.addParams({
-		"access_time" : "100ns",
-		"mem_size" : "512",
-		"clock" : "1GHz"
-	})
-
-cpu_cache_link = sst.Link("cpu_cache_link")
-cpu_cache_link.connect( (cpu, "mem_link", "50ps"), (l1cache, "upstream0", "50ps") )
-
-cache_bus_link = sst.Link("cache_bus_link")
-cache_bus_link.connect( (membus, "port0", "50ps"), (l1cache, "snoop_link", "50ps") )
-
-memory_link = sst.Link("mem_bus_link")
-memory_link.connect( (membus, "port1", "50ps"), (memory, "snoop_link", "50ps") )
+# Define the simulation links
+link_cpu_cache_link = sst.Link("link_cpu_cache_link")
+link_cpu_cache_link.connect( (comp_cpu, "mem_link", "1000ps"), (comp_l1cache, "high_network_0", "1000ps") )
+link_mem_bus_link = sst.Link("link_mem_bus_link")
+link_mem_bus_link.connect( (comp_l1cache, "low_network_0", "50ps"), (comp_memory, "direct_link", "50ps") )
+# End of generated output.
--- sst/elements/cassini/tests/streamcpu.py
+++ sst/elements/cassini/tests/streamcpu.py
@@ -2,36 +2,34 @@
 import sst
 
 # Define SST core options
-sst.setProgramOption("timebase", "1 ps")
+sst.setProgramOption("timebase", "1ns")
 sst.setProgramOption("stopAtCycle", "0 ns")
 
 # Define the simulation components
-cpu = sst.Component("cpu", "memHierarchy.streamCPU")
-cpu.addParams({
+comp_cpu = sst.Component("cpu", "memHierarchy.streamCPU")
+comp_cpu.addParams({
       "workPerCycle" : """1000""",
-      "commFreq" : """100""",
-      "memSize" : """0x1000""",
       "do_write" : """1""",
-      "num_loadstore" : """100000"""
+      "num_loadstore" : """100000""",
+      "commFreq" : """100""",
+      "memSize" : """0x1000"""
 })
-l1cache = sst.Component("l1cache", "memHierarchy.Cache")
-l1cache.addParams({
-      "cache_frequency" : """2 Ghz""",
-      "cache_size" : """8 KB""",
-      "coherence_protocol" : """MSI""",
-      "replacement_policy" : """lru""",
-      "associativity" : """4""",
+comp_l1cache = sst.Component("l1cache", "memHierarchy.Cache")
+comp_l1cache.addParams({
       "access_latency_cycles" : """2""",
-      "low_network_links" : """1""",
+      "cache_frequency" : """2 Ghz""",
+      "replacement_policy" : """lru""",
+      "coherence_protocol" : """MSI""",
+      "associativity" : """4""",
       "cache_line_size" : """64""",
-      "mshr_num_entries" : """4096""",
+      "prefetcher" : """cassini.NextBlockPrefetcher""",
+      "debug" : """1""",
+      "statistics" : """1""",
       "L1" : """1""",
-      "debug" : """0""",
-      "statistics" : """0""",
-      "prefetcher" : """cassini.NextBlockPrefetcher"""
+      "cache_size" : """8 KB"""
 })
-memory = sst.Component("memory", "memHierarchy.MemController")
-memory.addParams({
+comp_memory = sst.Component("memory", "memHierarchy.MemController")
+comp_memory.addParams({
       "coherence_protocol" : """MSI""",
       "access_time" : """1000 ns""",
       "mem_size" : """512""",
@@ -40,8 +38,8 @@ memory.addParams({
 
 
 # Define the simulation links
-cpu_cache_link = sst.Link("cpu_cache_link")
-cpu_cache_link.connect( (cpu, "mem_link", "1000ps"), (l1cache, "high_network_0", "1000ps") )
-mem_bus_link = sst.Link("mem_bus_link")
-mem_bus_link.connect( (l1cache, "low_network_0", "50ps"), (memory, "direct_link", "50ps") )
+link_cpu_cache_link = sst.Link("link_cpu_cache_link")
+link_cpu_cache_link.connect( (comp_cpu, "mem_link", "1000ps"), (comp_l1cache, "high_network_0", "1000ps") )
+link_mem_bus_link = sst.Link("link_mem_bus_link")
+link_mem_bus_link.connect( (comp_l1cache, "low_network_0", "50ps"), (comp_memory, "direct_link", "50ps") )
 # End of generated output.
