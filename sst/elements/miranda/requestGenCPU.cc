ab6675bcd38e0bba32f3ff00a393a6fb37099619
Si Hammond <simon.hammond@gmail.com>
2014-11-24 04:10:05 +0000

    Adds module loader code to Miranda.

--- sst/elements/miranda/requestGenCPU.cc
+++ sst/elements/miranda/requestGenCPU.cc
@@ -10,14 +10,18 @@ RequestGenCPU::RequestGenCPU(SST::ComponentId_t id, SST::Params& params) {
 	maxRequestsPending = (uint32_t) params.find_integer("maxmemoryreqs", 16);
 	requestsPending = 0;
 
-	out->verbose(CALL_INFO, 1, 0, "Configured CPU to allow %" PRIu32 " maximum requests to be memory to be outstanding.\n", maxRequestsPending);
+	out->verbose(CALL_INFO, 1, 0, "Configured CPU to allow %" PRIu32 " maximum requests to be memory to be outstanding.\n",
+		maxRequestsPending);
 
 	std::string reqGenModName = params.find_string("generator", "");
 
 	out->verbose(CALL_INFO, 1, 0, "Request generator to be loaded is: %s\n", reqGenModName.c_str());
-	Params genParams = params.
-	reqGen = loadModule();
+	Params genParams = params.find_prefix_params("generatorParams.");
+	reqGen = dynamic_cast<RequestGenerator*>( loadModuleWithComponent(reqGenModName, this, genParams) );
 
+	if(NULL == reqGen) {
+		out->fatal(CALL_INFO, -1, "Failed to load generator: %s\n", reqGenModName.c_str());
+	}
 }
 
 RequestGenCPU::~RequestGenCPU() {
