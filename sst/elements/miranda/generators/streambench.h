e90c8a714f38bfb384e18ff8e81ba0cf6801e359
Si Hammond <simon.hammond@gmail.com>
2014-12-11 21:46:20 +0000

    Adds the STREAM generator.

---
+++ sst/elements/miranda/generators/streambench.h
@@ -0,0 +1,39 @@
+
+#ifndef _H_SST_MIRANDA_STREAM_BENCH_GEN
+#define _H_SST_MIRANDA_STREAM_BENCH_GEN
+
+#include <sst/elements/miranda/mirandaGenerator.h>
+#include <sst/core/output.h>
+
+#include <queue>
+
+namespace SST {
+namespace Miranda {
+
+class STREAMBenchGenerator : public RequestGenerator {
+
+public:
+	STREAMBenchGenerator( Component* owner, Params& params );
+	~STREAMBenchGenerator();
+	void generate(std::queue<RequestGeneratorRequest*>* q);
+	bool isFinished();
+	void completed();
+
+private:
+	uint64_t reqLength;
+
+	uint64_t start_a;
+	uint64_t start_b;
+	uint64_t start_c;
+
+	uint64_t n;
+	uint64_t i;
+
+	Output*  out;
+
+};
+
+}
+}
+
+#endif
