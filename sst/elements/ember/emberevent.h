b8ba4dcdc78b4a8be535ec35389896c65a96b6aa
Si Hammond <simon.hammond@gmail.com>
2014-12-15 17:23:03 +0000

    Removes build warnings for 64-bit unsigned ints.

--- sst/elements/ember/emberevent.h
+++ sst/elements/ember/emberevent.h
@@ -55,16 +55,16 @@ public:
 
     virtual void issue( uint64_t time, FOO* = NULL ) {
         if ( m_output ) {
-            m_output->verbose(CALL_INFO, 2, 0, "time=%lu\n",time);    
+            m_output->verbose(CALL_INFO, 2, 0, "time=%" PRIu64 "\n",time);
         }
         m_issueTime = time;
         m_state = Complete;
     }
 
-    virtual bool complete( uint64_t time, int retval = 0 ) { 
+    virtual bool complete( uint64_t time, int retval = 0 ) {
 
         if ( m_output ) {
-            m_output->verbose(CALL_INFO, 2, 0, "time=%lu\n",time);    
+            m_output->verbose(CALL_INFO, 2, 0, "time=%" PRIu64 "\n",time);
         }
         if ( m_histo ) {
             m_histo->add( time - m_issueTime );
@@ -73,7 +73,7 @@ public:
     }
 
     virtual uint64_t completeDelayNS() {
-        m_output->verbose(CALL_INFO, 2, 0, "delay=%"PRIu64" ns\n",
+        m_output->verbose(CALL_INFO, 2, 0, "delay=%" PRIu64 " ns\n",
                                                 m_completeDelayNS);
         return m_completeDelayNS;
     }
