7cdbc4ce0f3dfc5804f1613cc21b455a8bcbb32c
Aaron Levine <allevin@sandia.gov>
2015-03-17 16:42:32 +0000

    Updating and adding copyrights

--- sst/elements/firefly/nicVirtNic.h
+++ sst/elements/firefly/nicVirtNic.h
@@ -1,4 +1,15 @@
-    class VirtNic {
+// Copyright 2009-2015 Sandia Corporation. Under the terms
+// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2015, Sandia Corporation
+// All rights reserved.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+class VirtNic {
         Nic& m_nic;
       public:
         VirtNic(Nic& nic, int _id, std::string portName) : m_nic(nic), id(_id)
