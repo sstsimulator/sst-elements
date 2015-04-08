2253ffec8335929af9d6f84622154e6cc80abb9e
Si Hammond <simon.hammond@gmail.com>
2015-04-08 03:36:47 +0000

    Adds statistics to NB and Stride prefetchers

--- sst/elements/cassini/cassini.cc
+++ sst/elements/cassini/cassini.cc
@@ -26,9 +26,16 @@ static SubComponent* load_NextBlockPrefetcher(Component* owner, Params& params){
     return new NextBlockPrefetcher(owner, params);
 }
 
+static const ElementInfoStatistic nbprefetch_statistics[] = {
+	{ "prefetches_issued",		"Number of prefetch requests issued", 1 },
+	{ "miss_events_processed",	"Number of cache misses received",    2 },
+	{ "hit_events_processed",       "Number of cache hits received",      2 },
+	{ NULL, NULL, 0 }
+};
+
 static const ElementInfoParam nextBlockPrefetcher_params[] = {
-    {"cache_line_size",             "The size of the cache line that this prefetcher is attached to, default is 64-bytes", "64"},
-    {NULL, NULL, NULL}
+    	{"cache_line_size",             "The size of the cache line that this prefetcher is attached to, default is 64-bytes", "64"},
+    	{ NULL, NULL, NULL }
 };
 
 static SubComponent* load_StridePrefetcher(Component* owner, Params& params){
@@ -36,15 +43,22 @@ static SubComponent* load_StridePrefetcher(Component* owner, Params& params){
 }
 
 static const ElementInfoParam stridePrefetcher_params[] = {
-    {"verbose",                     "Controls the verbosity of the cassini components", ""},
-    {"cache_line_size",             "Controls the cache line size of the cache the prefetcher is attached too", "64"},
-    {"history",                     "Start of Address Range, for this controller.", "16"},
-    {"reach",                       "Reach of the prefetcher (ie how far forward to make requests)", "2"},
-    {"detect_range",                "Range to detact addresses over in request-counts, default is 4.", "4"},
-    {"address_count",               "Number of addresses to keep in the prefetch table", "64"},
-    {"page_size",                   "Start of Address Range, for this controller.", "4096"},
-    {"overrun_page_boundaries",     "Allow prefetcher to run over page alignment boundaries, default is 0 (false)", "0"},
-    { NULL, NULL, NULL }
+    	{ "verbose",                     "Controls the verbosity of the cassini components", ""},
+    	{ "cache_line_size",             "Controls the cache line size of the cache the prefetcher is attached too", "64"},
+    	{ "history",                     "Start of Address Range, for this controller.", "16"},
+    	{ "reach",                       "Reach of the prefetcher (ie how far forward to make requests)", "2"},
+    	{ "detect_range",                "Range to detact addresses over in request-counts, default is 4.", "4"},
+    	{ "address_count",               "Number of addresses to keep in the prefetch table", "64"},
+    	{ "page_size",                   "Start of Address Range, for this controller.", "4096"},
+    	{ "overrun_page_boundaries",     "Allow prefetcher to run over page alignment boundaries, default is 0 (false)", "0"},
+    	{ NULL, NULL, NULL }
+};
+
+static const ElementInfoStatistic stride_statistics[] = {
+	{ "prefetches_issued",			  "Counts number of prefetches issued",	1 },
+	{ "prefetches_canceled_by_page_boundary", "Counts number of prefetches which spanned over page boundaries and so did not issue", 1 },
+	{ "prefetches_canceled_by_history",       "Counts number of prefetches which did not get issued because of prefetch history", 1 },
+	{ NULL, NULL, 0 }
 };
 
 static SubComponent* load_AddrHistogrammer(Component* owner, Params& params){
@@ -64,7 +78,7 @@ static const ElementInfoSubComponent subcomponents[] = {
       NULL,
       load_NextBlockPrefetcher,
       nextBlockPrefetcher_params,
-      NULL,
+      nbprefetch_statistics,
       "SST::MemHierarchy::CacheListener"
     },
     { "StridePrefetcher",
@@ -72,7 +86,7 @@ static const ElementInfoSubComponent subcomponents[] = {
       NULL,
       load_StridePrefetcher,
       stridePrefetcher_params,
-      NULL,
+      stride_statistics,
       "SST::MemHierarchy::CacheListener"
     },
     { "AddrHistogrammer",
