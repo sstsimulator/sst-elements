0fb5c1763026f30a309b8c6371491527666ab16f
Si Hammond <simon.hammond@gmail.com>
2015-04-08 03:04:44 +0000

    Fixes loadSubComp and loadModuleWithComp calls in Miranda CPU

--- sst/elements/miranda/mirandaCPU.cc
+++ sst/elements/miranda/mirandaCPU.cc
@@ -64,7 +64,7 @@ RequestGenCPU::RequestGenCPU(SST::ComponentId_t id, SST::Params& params) :
 	std::string reqGenModName = params.find_string("generator", "");
 	out->verbose(CALL_INFO, 1, 0, "Request generator to be loaded is: %s\n", reqGenModName.c_str());
 	Params genParams = params.find_prefix_params("generatorParams.");
-	reqGen = dynamic_cast<RequestGenerator*>( loadModuleWithComponent(reqGenModName, this, genParams) );
+	reqGen = dynamic_cast<RequestGenerator*>( loadSubComponent(reqGenModName, this, genParams) );
 
 	if(NULL == reqGen) {
 		out->fatal(CALL_INFO, -1, "Failed to load generator: %s\n", reqGenModName.c_str());
