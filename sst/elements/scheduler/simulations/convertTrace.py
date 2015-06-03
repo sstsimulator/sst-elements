53e68e7682414b5d8b855c5b0fa24b209a9879c8
Fulya Kaplan <fkaplan3@bu.edu>
2015-06-03 22:32:09 +0000

    Added/corrected simulation scripts for scheduler.

--- sst/elements/scheduler/simulations/convertTrace.py
+++ sst/elements/scheduler/simulations/convertTrace.py
@@ -8,15 +8,15 @@ import sys
 import numpy as np
 
 if __name__ == '__main__':
-  #check input
-  if len(sys.argv) != 2:
-    print "usage: python convertTrace.py <input file name>"
-    sys.exit()
-  #load data
-  print "Converting..."
-  data = np.loadtxt(sys.argv[1], comments = ';')
-  data = np.array([data[:,1], data[:,3], data[:,4], data[:,8]])
-  data = np.int_(data.T)
-  #write data
-  np.savetxt(sys.argv[1] + ".sim", data, delimiter=' ', fmt='%.0f')
-  print "Completed!"
+    #check input
+    if len(sys.argv) != 2:
+    	print "usage: python convertTrace.py <input file name>"
+    	sys.exit()
+    #load data
+    print "Converting..."
+    data = np.loadtxt(sys.argv[1], comments = ';')
+    data = np.array([data[:,1], data[:,3], data[:,4], data[:,8]])
+    data = np.int_(data.T)
+    #write data
+    np.savetxt(sys.argv[1] + ".sim", data, delimiter=' ', fmt='%.0f')
+    print "Completed!"
--- sst/elements/scheduler/simulations/makeInput.py
+++ sst/elements/scheduler/simulations/makeInput.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 '''
 SST scheduler simulation input file generator
 Input parameters are given below
@@ -62,70 +63,70 @@ Do not modify the script after this point.
 import sys
 
 if __name__ == '__main__':
-  if outFile == "" or outFile == "default":
-    print "Error: There is no default value for outFile"
-    sys.exit()
-  f = open(outFile,'w')
+    if outFile == "" or outFile == "default":
+    	print "Error: There is no default value for outFile"
+    	sys.exit()
+    f = open(outFile,'w')
   
-  f.write('# scheduler simulation input file\n')
-  f.write('import sst\n')
-  f.write('\n')
-  f.write('# Define SST core options\n')
-  f.write('sst.setProgramOption("run-mode", "both")\n')
-  f.write('\n')
-  f.write('# Define the simulation components\n')
-  f.write('scheduler = sst.Component("myScheduler", \
+    f.write('# scheduler simulation input file\n')
+    f.write('import sst\n')
+    f.write('\n')
+    f.write('# Define SST core options\n')
+    f.write('sst.setProgramOption("run-mode", "both")\n')
+    f.write('\n')
+    f.write('# Define the simulation components\n')
+    f.write('scheduler = sst.Component("myScheduler", \
             "scheduler.schedComponent")\n')
-  f.write('scheduler.addParams({\n')
+    f.write('scheduler.addParams({\n')
   
-  if traceName == "" or traceName == "default":
-    print "Error: There is no default value for traceName"
-    os.remove(outFile)
-    sys.exit()
-  f.write('      "traceName" : "' + traceName + '",\n')
-  if machine != "" and machine != "default":
-    f.write('      "machine" : "' + machine + '",\n')
-  if coresPerNode != "":
-    f.write('      "coresPerNode" : "' + coresPerNode + '",\n')
-  if scheduler != "" and scheduler != "default":
-    f.write('      "scheduler" : "' + scheduler + '",\n')
-  if FST != "" and FST != "default":
-    f.write('      "FST" : "' + FST + '",\n')
-  if allocator != "" and allocator != "default":
-    f.write('      "allocator" : "' + allocator + '",\n')
-  if taskMapper != "" and taskMapper != "default":
-    f.write('      "taskMapper" : "' + taskMapper + '",\n')
-  if timeperdistance != "" and timeperdistance != "default":
-    f.write('      "timeperdistance" : "' + timeperdistance + '",\n')
-  if dMatrixFile != "" and dMatrixFile != "default":
-    f.write('      "dMatrixFile" : "' + dMatrixFile + '",\n')
-  if randomSeed != "" and randomSeed != "default":
-    f.write('      "runningTimeSeed" : "' + randomSeed + '",\n')
-  f.seek(-2, os.SEEK_END)
-  f.truncate()
-  f.write('\n})\n')
-  f.write('\n')
+    if traceName == "" or traceName == "default":
+    	print "Error: There is no default value for traceName"
+    	os.remove(outFile)
+    	sys.exit()
+    f.write('      "traceName" : "' + traceName + '",\n')
+    if machine != "" and machine != "default":
+    	f.write('      "machine" : "' + machine + '",\n')
+    if coresPerNode != "":
+    	f.write('      "coresPerNode" : "' + coresPerNode + '",\n')
+    if scheduler != "" and scheduler != "default":
+    	f.write('      "scheduler" : "' + scheduler + '",\n')
+    if FST != "" and FST != "default":
+    	f.write('      "FST" : "' + FST + '",\n')
+    if allocator != "" and allocator != "default":
+    	f.write('      "allocator" : "' + allocator + '",\n')
+    if taskMapper != "" and taskMapper != "default":
+    	f.write('      "taskMapper" : "' + taskMapper + '",\n')
+    if timeperdistance != "" and timeperdistance != "default":
+    	f.write('      "timeperdistance" : "' + timeperdistance + '",\n')
+    if dMatrixFile != "" and dMatrixFile != "default":
+    	f.write('      "dMatrixFile" : "' + dMatrixFile + '",\n')
+    if randomSeed != "" and randomSeed != "default":
+    	f.write('      "runningTimeSeed" : "' + randomSeed + '",\n')
+    f.seek(-2, os.SEEK_END)
+    f.truncate()
+    f.write('\n})\n')
+    f.write('\n')
   
-  f.write('# nodes\n')
-  if machine.split('[')[0] == 'mesh' or machine.split('[')[0] == 'torus':
-    nums = machine.split('[')[1]
-    nums = nums.split(']')[0]
-    nums = nums.split(',')
-    numberNodes = int(nums[0])*int(nums[1])*int(nums[2])
-  for i in range(0, numberNodes):
-    f.write('n' + str(i) + ' = sst.Component("n' + str(i) + \
+    f.write('# nodes\n')
+    if machine.split('[')[0] == 'mesh' or machine.split('[')[0] == 'torus':
+    	nums = machine.split('[')[1]
+    	nums = nums.split(']')[0]
+    	nums = nums.split(',')
+    	numberNodes = int(nums[0])*int(nums[1])*int(nums[2])
+    for i in range(0, numberNodes):
+    	f.write('n' + str(i) + ' = sst.Component("n' + str(i) + \
             '", "scheduler.nodeComponent")\n')
-    f.write('n' + str(i) + '.addParams({\n')
-    f.write('      "nodeNum" : "' + str(i) + '",\n')
-    f.write('})\n')
-  f.write('\n')
+    	f.write('n' + str(i) + '.addParams({\n')
+    	f.write('      "nodeNum" : "' + str(i) + '",\n')
+    	f.write('})\n')
+    f.write('\n')
     
-  f.write('# define links\n')
-  for i in range(0, numberNodes):
-    f.write('l' + str(i) + ' = sst.Link("l' + str(i) + '")\n')
-    f.write('l' + str(i) + '.connect( (scheduler, "nodeLink' + str(i) + \
+    f.write('# define links\n')
+    for i in range(0, numberNodes):
+    	f.write('l' + str(i) + ' = sst.Link("l' + str(i) + '")\n')
+    	f.write('l' + str(i) + '.connect( (scheduler, "nodeLink' + str(i) + \
             '", "0 ns"), (n' + str(i) + ', "Scheduler", "0 ns") )\n')
-  f.write('\n')
+    f.write('\n')
     
-  f.close()
+    f.close()
 
