d9ecf08c1e2682bd1f68ba1d2cd36af337340fcd
Branden J. Moore <bjmoor@sandia.gov>
2015-06-16 14:38:01 +0000

    Remove unused gem5 connector, as Gem5 itself provides this. - m5C remains for now.

--- sst/elements/gem5/tests/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${HOME}/SST/gem5-dev/build/X86
-export GEM5=${HOME}/SST/gem5-dev
-export M5_PATH=${HOME}/SST/gem5-data
-export PYTHONPATH=${GEM5}/configs/common
-exec sst test4_ncore.py 
-#exec sst test4.py
