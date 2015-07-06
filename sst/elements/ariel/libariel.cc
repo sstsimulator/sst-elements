5cbb2a8a426b84bd4f80f8b1b7dafaec876d5bd3
Si Hammond <simon.hammond@gmail.com>
2015-07-06 17:59:50 +0000

    And of course, add the parameter to the manifest.

--- sst/elements/ariel/libariel.cc
+++ sst/elements/ariel/libariel.cc
@@ -65,6 +65,7 @@ static const ElementInfoParam ariel_params[] = {
     {"verbose", "Verbosity for debugging. Increased numbers for increased verbosity.", "0"},
     {"corecount", "Number of CPU cores to emulate", "1"},
     {"checkaddresses", "Verify that addresses are valid with respect to cache lines", "0"},
+    {"vtop_translate", "Set to yes to perform virt-phys translation (TLB) or no to disable", "yes"},
     {"translatecacheentries", "Keep a translation cache of this many entries to improve emulated core performance", "4096"},
     {"memorylevels", "Number of memory levels in the system", "1"},
     {"pagesize%(memorylevels)d", "Page size for memory Level x", "4096"},
