bbb51c18f5b70090102e7353b505b5079935da05
Nalini Kumar <nkumar@hcs.ufl.edu>
2015-08-10 22:52:44 +0000

    adding missed parameter pez=0

--- sst/elements/ember/test/defaultSim.py
+++ sst/elements/ember/test/defaultSim.py
@@ -7,7 +7,7 @@ def getWorkFlow( defaults ):
     workFlow.append( motif )
 
     motif = dict.copy( defaults )
-    motif['cmd'] = "Sweep3D nx=30 ny=30 nz=30 computetime=140 pex=4 pey=16 kba=10"
+    motif['cmd'] = "Sweep3D nx=30 ny=30 nz=30 computetime=140 pex=4 pey=16 pez=0 kba=10"     
     workFlow.append( motif )
 
     motif = dict.copy( defaults )
