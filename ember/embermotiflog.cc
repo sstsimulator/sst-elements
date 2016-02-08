a69f14a50b92bddc712ecf087299734d5eb0a436
nmhamster <sdhammo@sandia.gov>
2016-02-08 16:55:19 -0700

    Move to static logging by job ID for Ember motifs

--- ember/embermotiflog.cc
+++ ember/embermotiflog.cc
@@ -1,27 +1,50 @@
 
 #include <sst_config.h>
-#include "embermotiflog.h"
 
-static FILE* motifLogger = nullptr;
+#include <unordered_map>
+#include "embermotiflog.h"
 
 using namespace SST::Ember;
 
-EmberMotifLog::EmberMotifLog(const std::string logPath) {
-	if(nullptr == motifLogger) {
-		motifLogger = fopen(logPath.c_str(), "wt");
+static std::unordered_map<uint32_t, EmberMotifLogRecord*>* logHandles = nullptr;
+
+EmberMotifLog::EmberMotifLog(const std::string logPath, const uint32_t jobID) {
+	printf("EmberMotifLog START =======\n");
+
+	if(nullptr == logHandles) {
+		logHandles = new std::unordered_map<uint32_t, EmberMotifLogRecord*>();
 	}
+
+	auto logHandleFind = logHandles->find(jobID);
+
+	if(logHandleFind == logHandles->end()) {
+		printf("DID NOT FIND ENTRY .. CREATING ONE ..\n");
+		FILE* loggerFile = fopen(logPath.c_str(), "wt");
+		EmberMotifLogRecord* newRecord = new EmberMotifLogRecord(loggerFile);
+		logRecord->increment();
+
+		logHandles->insert( std::pair<uint32_t, EmberMotifLogRecord*>(jobID, newRecord) );
+	} else {
+		printf("FOUND ENTRY \n");
+		EmberMotifLogRecord* logRecord = logHandleFind->second;
+		logRecord->increment();
+	}
+
+	printf("EmberMotifLog DONE =====\n");
 }
 
 EmberMotifLog::~EmberMotifLog() {
-	if(nullptr != motifLogger) {
-		fclose(motifLogger);
-		motifLogger = nullptr;
+	logRecord->decrement();
+
+	if(0 == logRecord->getCount()) {
+		fclose(logRecord->getFile());
+		logRecord->invalidateFile();
 	}
 }
 
 void EmberMotifLog::logMotifStart(std::string name, int motifNum) {
-	if(nullptr != motifLogger) {
-        	fprintf(motifLogger, "%d %s %s\n", motifNum, name.c_str(),
-        		Simulation::getSimulation()->getElapsedSimTime().toStringBestSI().c_str());
-        }
+	if(nullptr != logRecord->getFile()) {
+       		fprintf(logRecord->getFile(), "%d %s %s\n", motifNum, name.c_str(),
+       			Simulation::getSimulation()->getElapsedSimTime().toStringBestSI().c_str());
+	}
 }
--- ember/embermotiflog.h
+++ ember/embermotiflog.h
@@ -8,11 +8,44 @@
 namespace SST {
 namespace Ember {
 
+class EmberMotifLogRecord {
+	public:
+		EmberMotifLogRecord(FILE* theLog) {
+			loggerFile = theLog;
+		}
+
+		void increment() {
+			motifCount++;
+		}
+
+		void decrement() {
+			motifCount--;
+		}
+
+		uint32_t getCount() const {
+			return motifCount;
+		}
+
+		FILE* getFile() {
+			return loggerFile;
+		}
+
+		void invalidateFile() {
+			loggerFile = nullptr;
+		}
+
+	protected:
+		FILE* loggerFile;
+		uint32_t motifCount;
+};
+
 class EmberMotifLog {
 	public:
-		EmberMotifLog(const std::string logPath);
+		EmberMotifLog(const std::string logPath, const uint32_t jobID);
 		~EmberMotifLog();
 		void logMotifStart(std::string name, int motifNum);
+	protected:
+		EmberMotifLogRecord* logRecord;
 
 };
 
