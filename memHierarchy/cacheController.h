d1e4852531e35c7d01c4c5b24eab15d04db42223
Gwen <grvosku@sandia.gov>
2016-04-29 15:55:08 -0600

    memH: Move isPowerOfTwo to utils

--- memHierarchy/cacheController.h
+++ memHierarchy/cacheController.h
@@ -180,9 +180,6 @@ private:
     /** Print cache line for debugging */
     void printLine(Addr addr);
 
-    /** Find out if number is a power of 2 */
-    bool isPowerOfTwo(uint x){ return (x & (x - 1)) == 0; }
-    
     /** Timestamp getter */
     uint64 getTimestamp(){ return timestamp_; }
 
