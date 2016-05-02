8ebf951e586153c999620971922ab64b84ff8ead
Si Hammond <sdhammo@sandia.gov>
2016-05-02 11:44:12 -0600

    Stringize PIN launcher

--- src/sst/elements/ariel/arielcpu.cc
+++ src/sst/elements/ariel/arielcpu.cc
@@ -148,7 +148,7 @@ ArielCPU::ArielCPU(ComponentId_t id, Params& params) :
 
     tunnel = new ArielTunnel(shmem_region_name, core_count, maxCoreQueueLen);
 
-    appLauncher = params.find<std::string>("launcher", PINTOOL_EXECUTABLE);
+    appLauncher = params.find<std::string>("launcher", STRINGIZE(PINTOOL_EXECUTABLE));
 
     const uint32_t launch_param_count = (uint32_t) params.find<uint32_t>("launchparamcount", 0);
     const uint32_t pin_arg_count = 23 + launch_param_count;
