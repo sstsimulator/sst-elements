6de29b545b2fff71748f671b80a3e03abd7d78d0
Mike <mjleven@sandia.gov>
2016-06-10 09:18:20 -0600

    new ember run support

---
+++ src/sst/elements/ember/run/lib/nicConfig.py
@@ -0,0 +1,40 @@
+# Copyright 2009-2016 Sandia Corporation. Under the terms
+# of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+# Government retains certain rights in this software.
+#
+# Copyright (c) 2009-2016, Sandia Corporation
+# All rights reserved.
+#
+# This file is part of the SST software package. For license
+# information, see the LICENSE file in the top level directory of the
+# distribution.
+
+import componentConfig 
+import myprint
+import copy
+
+def getOptions():
+	return ['netPktSize=','nicVerboseLevel=','nicVerboseMask=']
+
+class NicConfig(componentConfig.ComponentConfig):
+	def __init__( self, params, opts ):
+		self.params = params 
+		for o,a in opts:
+			if o in ('--netPktSize'):
+				self.params['packetSize'] = a
+			elif o in ('--nicVerboseLevel'):
+				self.params['verboseLevel'] = int( a )
+			elif o in ('--nicVerboseMask'):
+				self.params['verboseMask'] = int( a )
+
+		#myprint.printParams( 'NicConfig:', self.params )
+
+	def getParams( self, nodeNum, ranksPerNode ):
+		extra = copy.deepcopy(self.params)
+		extra["nid"] =  nodeNum	
+		extra["num_vNics"] = ranksPerNode	
+	
+		return extra
+
+	def getName( self, nodeNum ): 
+		return "firefly.nic"
---
+++ src/sst/elements/ember/run/lib/nullEmber.py
@@ -0,0 +1,29 @@
+# Copyright 2009-2016 Sandia Corporation. Under the terms
+# of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+# Government retains certain rights in this software.
+#
+# Copyright (c) 2009-2016, Sandia Corporation
+# All rights reserved.
+#
+# This file is part of the SST software package. For license
+# information, see the LICENSE file in the top level directory of the
+# distribution.
+
+import jobInfo as JobInfo
+import emberConfig as EmberConfig
+
+def _genWorkFlow( defaults, nodeNum = None ):
+
+    workFlow = []
+    motif = dict.copy( defaults )
+    motif['cmd'] = "Null"
+    workFlow.append( motif )
+
+    return workFlow
+
+def create( emberParams, hermesParams ):
+
+	jobInfo = JobInfo.JobInfo( -1, -1, 1, _genWorkFlow )
+	jobInfo.setNidList( 'Null' )
+
+	return EmberConfig.EmberConfig( emberParams, hermesParams, jobInfo )
