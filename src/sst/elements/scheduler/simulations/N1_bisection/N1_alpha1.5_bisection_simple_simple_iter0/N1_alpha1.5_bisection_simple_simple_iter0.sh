878b5186584822b1fe0523dba20b851c4cbafad9
fkaplan3 <fkaplan3@bu.edu>
2016-06-22 14:16:53 -0400

    Removed old simulation result folder.

--- src/sst/elements/scheduler/simulations/N1_bisection/N1_alpha1.5_bisection_simple_simple_iter0/N1_alpha1.5_bisection_simple_simple_iter0.sh
+++
@@ -1,6 +0,0 @@
-hostname
-date
-source /mnt/nokrb/fkaplan3/tools/addsimulator.sh
-export SIMOUTPUT=/mnt/nokrb/fkaplan3/SST/git/sst/sst/elements/scheduler/simulations/N1_bisection/N1_alpha1.5_bisection_simple_simple_iter0/
-python run_DetailedNetworkSim.py --emberOut ember.out --alpha 1.5 --schedPy ./simple_simple_bisection_N1.py
-date
--- src/sst/elements/scheduler/simulations/N1_bisection/N1_alpha3_bisection_simple_simple_iter0/N1_alpha3_bisection_simple_simple_iter0.sh
+++
@@ -1,6 +0,0 @@
-hostname
-date
-source /mnt/nokrb/fkaplan3/tools/addsimulator.sh
-export SIMOUTPUT=/mnt/nokrb/fkaplan3/SST/git/sst/sst/elements/scheduler/simulations/N1_bisection/N1_alpha3_bisection_simple_simple_iter0/
-python run_DetailedNetworkSim.py --emberOut ember.out --alpha 3 --schedPy ./simple_simple_bisection_N1.py
-date
