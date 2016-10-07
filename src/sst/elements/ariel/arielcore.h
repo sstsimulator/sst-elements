d5d0e98b821de7a750484e8943eb066588d34606
Aaron Levine <allevin@sandia.gov>
2016-10-07 12:02:05 -0600

    Slightly change wording in copyright

--- src/sst/elements/ariel/arielcore.h
+++ src/sst/elements/ariel/arielcore.h
@@ -7,7 +7,7 @@
 //
 // Portions are copyright of other developers:
 // See the file CONTRIBUTITORS.TXT in the top level directory
-// of this repository for more information.
+// the distribution for more information.
 //
 // This file is part of the SST software package. For license
 // information, see the LICENSE file in the top level directory of the
