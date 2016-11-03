b747c425942c99d4b4ae45c9d0eae35d9d6bc44c
Amro Awad <aawad@sst-devel.sandia.gov>
2016-11-03 09:51:09 -0600

    Modifying some include paths to be relative

--- src/sst/elements/Samba/TLBUnit.h
+++ src/sst/elements/Samba/TLBUnit.h
@@ -18,7 +18,8 @@
 #include <sst_config.h>
 #include <sst/core/component.h>
 #include <sst/core/timeConverter.h>
-#include "sst/elements/memHierarchy/memEvent.h"
+//#include <sst/elements/memHierarchy/memEvent.h>
+#include "../memHierarchy/memEvent.h"
 
 #include<map>
 #include<vector>
