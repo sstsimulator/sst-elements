2c1e1df9afed034d98719da787163e67071f497e
Amro Awad <aawad@serrano-login1.sandia.gov>
2017-05-17 16:24:33 -0600

    Fixing the copyright

--- src/sst/elements/Messier/libMessier.cpp
+++ src/sst/elements/Messier/libMessier.cpp
@@ -1,18 +1,16 @@
-// Copyright 2009-2016 Sandia Corporation. Under the terms
-// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
-// Government retains certain rights in this software.
+//Copyright 2009-2017 Sandia Corporation. Under the terms
+//// of Contract DE-AC04-94AL85000 with Sandia Corporation, the U.S.
+//// Government retains certain rights in this software.
+////
+//// Copyright (c) 2009-2017, Sandia Corporation
+//// All rights reserved.
+////
+//// This file is part of the SST software package. For license
+//// information, see the LICENSE file in the top level directory of the
+//// distribution.
+////
+// Author: Amro Awad (E-mail aawad@sandia.gov)
 //
-// Copyright (c) 2009-2016, Sandia Corporation
-// All rights reserved.
-//
-// Portions are copyright of other developers:
-// See the file CONTRIBUTORS.TXT in the top level directory
-// the distribution for more information.
-//
-// This file is part of the SST software package. For license
-// information, see the LICENSE file in the top level directory of the
-// distribution.
-
 #include <sst_config.h>
 #include "sst/core/element.h"
 #include <Messier.h>
