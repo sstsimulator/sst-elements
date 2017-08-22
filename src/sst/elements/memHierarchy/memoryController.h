d9303e66c1c8e5dcdd3ff7f05aa391b50eeaff4e
Gwen <grvosku@sandia.gov>
2017-08-22 16:15:16 -0600

    memH: Fix deadlock in directory; don't call clock() on link in mem if not needed

--- src/sst/elements/memHierarchy/memoryController.h
+++ src/sst/elements/memHierarchy/memoryController.h
@@ -72,6 +72,7 @@ private:
     Backend::Backing*       backing_; 
 
     MemLinkBase* link_;         // Link to the rest of memHierarchy 
+    bool clockLink_;            // Flag - should we call clock() on this link or not
 
     std::vector<CacheListener*> listeners_;
     
