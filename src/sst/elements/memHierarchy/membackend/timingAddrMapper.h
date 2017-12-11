177df0b4c433ec775114d6007f6e82b308655a91
Gwen <grvosku@sandia.gov>
2017-12-11 14:30:41 -0700

    memH: Fix timingDRAM
    - New address mapper to handle non-power-two channels/banks/etc.
    - Fix existing address mappers to fatal if the parameters can't be supported
    - Responses return in order generated not in global order requests were received

--- src/sst/elements/memHierarchy/membackend/timingAddrMapper.h
+++ src/sst/elements/memHierarchy/membackend/timingAddrMapper.h
@@ -61,6 +61,30 @@ class SimpleAddrMapper : public AddrMapper {
         m_columnShift( 8 )
     { }
 
+    virtual void setNumChannels( unsigned int num ) {
+        if (!isPowerOfTwo(num)) {
+            Output output("", 1, 0, Output::STDOUT);
+            output.fatal(CALL_INFO, -1, "SimpleAddrMapper, Error: memHierarchy.simpleAddrMapper does not support non-power-of-2 channels...use memHierarchy.roundRobinAddrMapper instead\n");
+        }
+        m_numChannels = num; 
+    }
+
+    virtual void setNumRanks( unsigned int num ) {
+        if (!isPowerOfTwo(num)) {
+            Output output("", 1, 0, Output::STDOUT);
+            output.fatal(CALL_INFO, -1, "SimpleAddrMapper, Error: memHierarchy.simpleAddrMapper does not support non-power-of-2 ranks...use memHierarchy.roundRobinAddrMapper instead\n");
+        }
+        m_numRanks = num;
+    }
+
+    virtual void setNumBanks( unsigned int num ) {
+        if (!isPowerOfTwo(num)) {
+            Output output("", 1, 0, Output::STDOUT);
+            output.fatal(CALL_INFO, -1, "SimpleAddrMapper, Error: memHierarchy.simpleAddrMapper does not support non-power-of-2 banks...use memHierarchy.roundRobinAddrMapper instead\n");
+        }
+        m_numBanks = num;
+    }
+
     int channelShift() { return m_baseShift; } 
     int channelMask()  { return m_numChannels - 1; }
     int channelWidth() { return log2Of( m_numChannels ); }
@@ -96,6 +120,82 @@ class SimpleAddrMapper : public AddrMapper {
     unsigned m_columnShift;
 };
 
+class RoundRobinAddrMapper: public AddrMapper {
+public:
+    /* Element Library Info */
+    SST_ELI_REGISTER_MODULE(RoundRobinAddrMapper, "memHierarchy", "roundRobinAddrMapper", SST_ELI_ELEMENT_VERSION(1,0,0),
+            "Round-robin address mapper", "SST::MemHierarchy::AddrMapper")
+
+    SST_ELI_DOCUMENT_PARAMS(
+            {"interleave_size", "(string) Granularity of interleaving in bytes (B). SI ok.", "64B"},
+            {"row_size",        "(string) Size of each row in bytes (B). SI ok.", "1KiB"})
+
+    /* Begin class definition */
+    /* Interleave blocks across channel, then bank, then rank, pack into rows */
+    /* Generic for any #ranks, channels, banks. interleave_size must be power of 2, same as row/interleave_size */
+    RoundRobinAddrMapper(Params &params) : AddrMapper() {
+        UnitAlgebra ilSize = UnitAlgebra(params.find<std::string>("interleave_size", "64B")); /* Default = 64B line interleaving */
+        UnitAlgebra rowSize = UnitAlgebra(params.find<std::string>("row_size", "1KiB"));
+        Output output("", 1, 0, Output::STDOUT);
+        
+        if (!ilSize.hasUnits("B")) {
+            output.fatal(CALL_INFO, -1, "RoundRobinAddrMapper, Invalid param - interleave_size: must have units of 'B'. SI ok.\n");
+        }
+        if (!rowSize.hasUnits("B")) {
+            output.fatal(CALL_INFO, -1, "RoundRobinAddrMapper, Invalid param - row_size: must have units of 'B'. SI ok.\n");
+        }
+        if (!isPowerOfTwo(ilSize.getRoundedValue())) {
+            output.fatal(CALL_INFO, -1, "RoundRobinAddrMapper, Invalid param - interleave_size: must be a power-of-2.\n");
+        }
+        if (!isPowerOfTwo(rowSize.getRoundedValue())) {
+            output.fatal(CALL_INFO, -1, "RoundRobinAddrMapper, Invalid param - row_size: must be a power-of-2.\n");
+        }
+        m_baseShift = log2Of( ilSize.getRoundedValue() );
+        m_rowDivider = m_numChannels * m_numRanks * m_numBanks * (rowSize.getRoundedValue() / ilSize.getRoundedValue());
+        m_rankDivider = m_numChannels * m_numBanks;
+    }
+    
+    virtual void setNumChannels( unsigned int num ) {
+        m_rowDivider = m_rowDivider * num / m_numChannels; /* Swap num for m_numChannels */
+        m_numChannels = num; 
+        m_rankDivider = m_numChannels * m_numBanks;
+    }
+
+    virtual void setNumRanks( unsigned int num ) {
+        m_rowDivider = m_rowDivider * num / m_numRanks;
+        m_numRanks = num;
+    }
+
+    virtual void setNumBanks( unsigned int num ) {
+        m_rowDivider = m_rowDivider * num / m_numBanks;
+        m_numBanks = num;
+        m_rankDivider = m_numChannels * m_numBanks;
+    }
+
+    int getChannel( Addr addr ) {
+        return (addr >> m_baseShift) % m_numChannels;
+    }
+    
+    int getRank( Addr addr ) {
+        return ((addr >> m_baseShift) / m_rankDivider) % m_numRanks;
+    }
+    
+    int getBank( Addr addr ) {
+        return ((addr >> m_baseShift) / m_numChannels) % m_numBanks;
+    }
+    
+    int getRow( Addr addr ) {
+        return (addr >> m_baseShift) / m_rowDivider;
+    }
+
+private:
+    unsigned int m_baseShift;
+    unsigned int m_rankDivider;
+    unsigned int m_rowDivider;
+    unsigned int m_rowSize;
+};
+
+
 class SandyBridgeAddrMapper : public AddrMapper {
   public:
 /* Element Library Info */
@@ -105,6 +205,15 @@ class SandyBridgeAddrMapper : public AddrMapper {
 /* Begin class definition */
     SandyBridgeAddrMapper( Params &params ) : AddrMapper() 
     { }
+    
+    virtual void setNumChannels( unsigned int num  ) { 
+        if (num != 0) {
+            Output output("", 1, 0, Output::STDOUT);
+            output.fatal(CALL_INFO, -1, "SandyBridgeAddrMapper, Error: memHierarchy.sandyBridgeAddrMapper does not support multiple channels\n");
+        }
+        m_numChannels = num;
+    }
+
 
     int getChannel( Addr addr ) { return 0; }
     int getRank( Addr addr ) {
