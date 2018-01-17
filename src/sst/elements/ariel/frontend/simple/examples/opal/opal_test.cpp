b24580e5d80b936ee0da4eba741361b96effa27c
Clayton Hughes <chughes@sandia.gov>
2018-01-17 12:49:05 -0700

    Slight changes to test program.

--- src/sst/elements/ariel/frontend/simple/examples/opal/opal_test.cpp
+++ src/sst/elements/ariel/frontend/simple/examples/opal/opal_test.cpp
@@ -4,14 +4,19 @@
 #include<cstdio>
 #include<string>
 
+#define NUM_INTS 1000000
+
 using namespace std;
 
 #pragma GCC push_options
 #pragma GCC optimize ("O0")
+
 extern "C" {
 
-void* mlm_malloc(size_t size, int level) {
-	if(size == 0) {
+void* mlm_malloc(size_t size, int level)
+{
+	if(size == 0)
+      {
 		printf("ZERO BYTE MALLOC\n");
 		void* bt_entries[64];
 		exit(-1);
@@ -32,28 +37,23 @@ void * ariel_mmap_mlm(int ID, size_t size, int level) { return malloc(size); }
 
 int main()
 {
+   ariel_enable();
+   int * x = (int *) malloc(sizeof(int) * NUM_INTS);
+   int * y = (int *) mlm_malloc(sizeof(int) * NUM_INTS, 1);
 
-ariel_enable();
-int * x = (int *) mlm_malloc(sizeof(int) * 1000000, 1);
+   ariel_mmap_mlm(660066, 4096, 2);
 
-ariel_mmap_mlm(660066, 4096, 2);
+   for (int i = 0; i < NUM_INTS; )
+   {
+      y[i] = i * 2;
+      i = i + 1024;
+   }
 
-for (int i= 0; i < 1000000; i++)
-x[i] = i*2;
+   for (int i = 0; i < NUM_INTS; i++)
+      cout << "Value is: " << y[i] << endl;
 
-
-for (int i= 0; i < 1000000; i++)
-cout<<"Value is: "<<x[i]<<endl;
-
-
-
-
-cout<<"Test MLM Malloc"<<endl;
-
+   cout << "Test MLM Malloc" << endl;
 
 
-
-
-
-return 0;
+   return 0;
 }
