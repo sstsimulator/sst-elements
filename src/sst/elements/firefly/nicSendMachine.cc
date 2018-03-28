f6fb9b42b211111aeee8438489ad27f086d02329
Mike <mjleven@sandia.gov>
2018-03-28 10:04:21 -0600

    need to change order of code because delete item is needed

--- src/sst/elements/firefly/nicSendMachine.cc
+++ src/sst/elements/firefly/nicSendMachine.cc
@@ -72,12 +72,6 @@ void Nic::SendMachine::getPayload( SendEntryBase* entry, FireflyNetworkEvent* ev
 }
 void Nic::SendMachine::streamFini( SendEntryBase* entry ) 
 {
-    if ( entry->shouldDelete() ) {
-        m_dbg.debug(CALL_INFO,1,NIC_DBG_SEND_MACHINE, "%p delete SendEntry entry, pid=%d\n",entry, entry->local_vNic());
-        delete entry;
-    } else {
-        m_dbg.debug(CALL_INFO,1,NIC_DBG_SEND_MACHINE, "%p pid=%d\n",entry);
-    }
     if ( m_I_manage ) {
         m_sendQ.pop_front();
         if ( ! m_sendQ.empty() )  {
@@ -85,8 +79,16 @@ void Nic::SendMachine::streamFini( SendEntryBase* entry )
         }
     } else {
         m_activeEntry = NULL;
-        m_nic.notifySendDone( m_id );
+        m_nic.notifySendDone( this, entry );
     }
+
+    if ( entry->shouldDelete() ) {
+        m_dbg.debug(CALL_INFO,1,NIC_DBG_SEND_MACHINE, "%p delete SendEntry entry, pid=%d\n",entry, entry->local_vNic());
+        delete entry;
+    } else {
+        m_dbg.debug(CALL_INFO,1,NIC_DBG_SEND_MACHINE, "%p pid=%d\n",entry,m_id);
+    }
+
 }
 
 void  Nic::SendMachine::InQ::enque( int unit, int pid, std::vector< MemOp >* vec,
