aae5f0b81eec7d1f666cddda3ecd54df96d0ee2a
Mike <mjleven@sandia.gov>
2018-04-10 12:39:29 -0600

    remove old ELI code

---
+++ src/sst/elements/firefly/libfirefly.cc
@@ -0,0 +1,28 @@
+// Copyright 2009-2017 Sandia Corporation. Under the terms
+// of Contract DE-NA0003525 with Sandia Corporation, the U.S.
+// Government retains certain rights in this software.
+// 
+// Copyright (c) 2009-2017, Sandia Corporation
+// All rights reserved.
+// 
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+
+#include <sst_config.h>
+#include <hades.h>
+#include <funcSM/init.h>
+#include <funcSM/fini.h>
+#include <funcSM/rank.h>
+#include <funcSM/size.h>
+#include <funcSM/makeProgress.h>
+#include <funcSM/allreduce.h>
+#include <funcSM/commDestroy.h>
+#include <rangeLatMod.h>
+#include <scaleLatMod.h>
+#include <nodePerf.h>
