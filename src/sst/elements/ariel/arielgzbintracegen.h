3a6e9b9fbfe7ab9bca1ec81d14a529669742688f
Gwen <grvosku@sandia.gov>
2018-04-18 09:20:25 -0600

    ariel: Fixes to detect libz and ariel trace modules

--- src/sst/elements/ariel/arielgzbintracegen.h
+++ src/sst/elements/ariel/arielgzbintracegen.h
@@ -35,59 +35,22 @@ public:
     
     SST_ELI_DOCUMENT_PARAMS( { "trace_prefix", "Sets the prefix for the trace file", "ariel-core-" } )
 
-	ArielCompressedBinaryTraceGenerator(Component* owner, Params& params) :
-		ArielTraceGenerator() {
+    ArielCompressedBinaryTraceGenerator(Component* owner, Params& params);
 
-		tracePrefix = params.find<std::string>("trace_prefix", "ariel-core");
-		coreID = 0;
+    ~ArielCompressedBinaryTraceGenerator();
 
-		buffer = (char*) malloc(sizeof(uint64_t) + sizeof(uint64_t) + sizeof(char) + sizeof(uint32_t));
-	}
-
-	~ArielCompressedBinaryTraceGenerator() {
-		gzclose(traceFile);
-		free(buffer);
-	}
-
-	void publishEntry(const uint64_t picoS,
-                const uint64_t physAddr,
-		const uint32_t reqLength,
-                const ArielTraceEntryOperation op) {
-
-		const char op_type = (READ == op) ? 'R' : 'W';
-
-		copy(&buffer[0], &picoS, sizeof(uint64_t));
-		copy(&buffer[sizeof(uint64_t)], &op_type, sizeof(char));
-		copy(&buffer[sizeof(uint64_t) + sizeof(char)], &physAddr, sizeof(uint64_t));
-		copy(&buffer[sizeof(uint64_t) + sizeof(char) + sizeof(uint64_t)], &reqLength, sizeof(uint32_t));
-
-		gzwrite(traceFile, buffer, sizeof(uint64_t) + sizeof(char) + sizeof(uint64_t) + sizeof(uint32_t));
-	}
-
-	void setCoreID(const uint32_t core) {
-		coreID = core;
-
-		char* tracePath = (char*) malloc(sizeof(char) * PATH_MAX);
-		sprintf(tracePath, "%s-%" PRIu32 ".trace.gz", tracePrefix.c_str(), core);
-
-		traceFile = gzopen(tracePath, "wb");
-
-		free(tracePath);
-	}
+    void publishEntry(const uint64_t picoS, const uint64_t physAddr,
+            const uint32_t reqLength, const ArielTraceEntryOperation op);
+    
+    void setCoreID(const uint32_t core);
 
 private:
-	void copy(char* dest, const void* src, const size_t length) {
-		const char* src_c = (char*) src;
+    void copy(char* dest, const void* src, const size_t length);
 
-		for(size_t i = 0; i < length; ++i) {
-			dest[i] = src_c[i];
-		}
-	}
-
-	gzFile traceFile;
-	std::string tracePrefix;
-	uint32_t coreID;
-	char* buffer;
+    gzFile traceFile;
+    std::string tracePrefix;
+    uint32_t coreID;
+    char* buffer;
 
 };
 
--- src/sst/elements/ariel/arieltexttracegen.h
+++ src/sst/elements/ariel/arieltexttracegen.h
@@ -17,8 +17,11 @@
 #ifndef _H_SST_ARIEL_TEXT_TRACE_GEN
 #define _H_SST_ARIEL_TEXT_TRACE_GEN
 
-#include <sst/core/params.h>
 #include <climits>
+
+#include <sst/core/params.h>
+#include <sst/core/elementinfo.h>
+
 #include "arieltracegen.h"
 
 namespace SST {
@@ -28,49 +31,24 @@ class ArielTextTraceGenerator : public ArielTraceGenerator {
 
 public:
     
-    SST_ELI_REGISTER_MODULE(ArielTraceGenerator, "ariel", "TextTraceGenerator", SST_ELI_ELEMENT_VERSION(1,0,0),
-	    "Provides tracing to text file capabilities", "SST::ArielComponent::ArielTraceGenerator")
+    SST_ELI_REGISTER_MODULE(ArielTextTraceGenerator, "ariel", "TextTraceGenerator", SST_ELI_ELEMENT_VERSION(1,0,0),
+            "Provides tracing to text file capabilities", "SST::ArielComponent::ArielTraceGenerator")
     
     SST_ELI_DOCUMENT_PARAMS( { "trace_prefix", "Sets the prefix for the trace file", "ariel-core-" } )
 
-	ArielTextTraceGenerator(Component* owner, Params& params) :
-		ArielTraceGenerator() {
+    ArielTextTraceGenerator(Component* owner, Params& params);
 
-		tracePrefix = params.find<std::string>("trace_prefix", "ariel-core");
-		coreID = 0;
-	}
+    ~ArielTextTraceGenerator(); 
 
-	~ArielTextTraceGenerator() {
-		fclose(textFile);
-	}
+    void publishEntry(const uint64_t picoS, const uint64_t physAddr,
+            const uint32_t reqLength, const ArielTraceEntryOperation op);
 
-	void publishEntry(const uint64_t picoS,
-                const uint64_t physAddr,
-		const uint32_t reqLength,
-                const ArielTraceEntryOperation op) {
-
-		fprintf(textFile, "%" PRIu64 " %s %" PRIu64 " %" PRIu32 "\n",
-			picoS,
-			(op == READ) ? "R" : "W",
-			physAddr,
-			reqLength);
-	}
-
-	void setCoreID(const uint32_t core) {
-		coreID = core;
-
-		char* tracePath = (char*) malloc(sizeof(char) * PATH_MAX);
-		sprintf(tracePath, "%s-%" PRIu32 ".trace", tracePrefix.c_str(), core);
-
-		textFile = fopen(tracePath, "wt");
-
-		free(tracePath);
-	}
+    void setCoreID(const uint32_t core); 
 
 private:
-	FILE* textFile;
-	std::string tracePrefix;
-	uint32_t coreID;
+    FILE* textFile;
+    std::string tracePrefix;
+    uint32_t coreID;
 
 };
 
