4ebd613ef6cd7a7e3034c57013a5354733779918
Gwen Voskuilen <grvosku@sandia.gov>
2018-06-26 10:44:53 -0600

    memH: Update ref files for new prefetch statistics

---
+++ src/sst/elements/cassini/tests/streamcpu-pp.py
@@ -0,0 +1,49 @@
+import sst
+
+# Define SST core options
+sst.setProgramOption("timebase", "1ps")
+sst.setProgramOption("stopAtCycle", "0 ns")
+
+# Tell SST what statistics handling we want
+sst.setStatisticLoadLevel(4)
+
+# Define the simulation components
+comp_cpu = sst.Component("cpu", "memHierarchy.streamCPU")
+comp_cpu.addParams({
+      "do_write" : "1",
+      "num_loadstore" : "100000",
+      "commFreq" : "100",
+      "memSize" : "524288"
+})
+
+comp_l1cache = sst.Component("l1cache", "memHierarchy.Cache")
+comp_l1cache.addParams({
+      "access_latency_cycles" : "2",
+      "cache_frequency" : "2 Ghz",
+      "replacement_policy" : "lru",
+      "coherence_protocol" : "MESI",
+      "associativity" : "4",
+      "cache_line_size" : "64",
+      "prefetcher" : "cassini.PalaPrefetcher",
+      "debug" : "1",
+      "L1" : "1",
+      "cache_size" : "8 KB"
+})
+
+# Enable statistics outputs
+comp_l1cache.enableAllStatistics({"type":"sst.AccumulatorStatistic"})
+
+comp_memory = sst.Component("memory", "memHierarchy.MemController")
+comp_memory.addParams({
+      "coherence_protocol" : "MESI",
+      "backend.access_time" : "1000 ns",
+      "backend.mem_size" : "512MiB",
+      "clock" : "1GHz"
+})
+
+
+# Define the simulation links
+link_cpu_cache_link = sst.Link("link_cpu_cache_link")
+link_cpu_cache_link.connect( (comp_cpu, "mem_link", "1000ps"), (comp_l1cache, "high_network_0", "1000ps") )
+link_mem_bus_link = sst.Link("link_mem_bus_link")
+link_mem_bus_link.connect( (comp_l1cache, "low_network_0", "50ps"), (comp_memory, "direct_link", "50ps") )
