b47f1e3207f58013eaaa3fccee62bfed98f61ef4
Arun Rodrigues <afrodri@sandia.gov>
2018-08-08 14:06:49 -0600

    initial skeleton

---
+++ src/sst/elements/STPU/STPU.cc
@@ -0,0 +1,83 @@
+// Copyright 2018 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2018, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#include <sst_config.h>
+#include "STPU.h"
+
+#include <sst/core/element.h>
+#include <sst/core/params.h>
+#include <sst/core/simulation.h>
+#include "../memHierarchy/memEvent.h"
+
+using namespace SST;
+using namespace SST::MemHierarchy;
+
+
+STPU::STPU(ComponentId_t id, Params& params) :
+    Component(id)
+{
+    uint32_t outputLevel = params.find<uint32_t>("verbose", 0);
+    out.init("STPU:@p:@l: ", outputLevel, 0, Output::STDOUT);
+
+    // get parameters
+    
+
+    // tell the simulator not to end without us
+    registerAsPrimaryComponent();
+    primaryComponentDoNotEndSim();
+
+    // init memory
+    memory = dynamic_cast<Interfaces::SimpleMem*>(loadSubComponent("memHierarchy.memInterface", this, params));
+    if (!memory) {
+        out.fatal(CALL_INFO, -1, "Unable to load memHierarchy.memInterface subcomponent\n");
+    }
+    memory->initialize("mem_link", new Interfaces::SimpleMem::Handler<STPU> (this, &STPU::handleEvent));
+
+    //set our clock
+    std::string clockFreq = params.find<std::string>("clock", "1GHz");
+    clockHandler = new Clock::Handler<STPU>(this, &STPU::clockTic);
+    clockTC = registerClock(clockFreq, clockHandler);
+}
+
+STPU::STPU() : Component(-1)
+{
+	// for serialization only
+}
+
+
+void STPU::init(unsigned int phase)
+{
+    memory->init(phase);
+}
+
+void STPU::handleEvent(Interfaces::SimpleMem::Request * req)
+{
+    std::map<uint64_t, SimTime_t>::iterator i = requests.find(req->id);
+    if (i == requests.end()) {
+	out.fatal(CALL_INFO, -1, "Request ID (%" PRIx64 ") not found in outstanding requests!\n", req->id);
+    } else {
+        requests.erase(i);
+        // handle event
+    }
+    delete req;
+}
+
+bool STPU::clockTic( Cycle_t )
+{
+    // return false so we keep going
+    return false;
+}
+
+
---
+++ src/sst/elements/STPU/STPU.h
@@ -0,0 +1,77 @@
+// Copyright 2018 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2018, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#ifndef _STPU_H
+#define _STPU_H
+
+#ifndef __STDC_FORMAT_MACROS
+#define __STDC_FORMAT_MACROS
+#endif
+#include <inttypes.h>
+
+#include <sst/core/event.h>
+#include <sst/core/sst_types.h>
+#include <sst/core/component.h>
+#include <sst/core/link.h>
+#include <sst/core/timeConverter.h>
+#include <sst/core/output.h>
+#include <sst/core/elementinfo.h>
+
+#include <sst/core/interfaces/simpleMem.h>
+#include "../memHierarchy/memEvent.h"
+
+namespace SST {
+namespace MemHierarchy {
+
+class STPU : public SST::Component {
+public:
+/* Element Library Info */
+    SST_ELI_REGISTER_COMPONENT(STPU, "STPU", "STPU", SST_ELI_ELEMENT_VERSION(1,0,0),
+            "Spiking Temportal Processing Unit", COMPONENT_CATEGORY_PROCESSOR)
+
+    SST_ELI_DOCUMENT_PARAMS(
+            {"verbose",                 "(uint) Determine how verbose the output from the CPU is", "0"},
+            {"clock",                   "(string) Clock frequency", "1GHz"}
+                            )
+
+    SST_ELI_DOCUMENT_PORTS( {"mem_link", "Connection to memory", { "memHierarchy.MemEventBase" } } )
+
+    /* Begin class definiton */
+    STPU(SST::ComponentId_t id, SST::Params& params);
+    void init();
+    void finish() {;}
+
+private:
+    STPU();  // for serialization only
+    STPU(const STPU&); // do not implement
+    void operator=(const STPU&); // do not implement
+    void init(unsigned int phase);
+    
+    void handleEvent( SST::Interfaces::SimpleMem::Request * req );
+    virtual bool clockTic( SST::Cycle_t );
+
+    Output out;
+    Interfaces::SimpleMem * memory;
+
+    std::map<uint64_t, SimTime_t> requests;
+
+    TimeConverter *clockTC;
+    Clock::HandlerBase *clockHandler;
+
+};
+
+}
+}
+#endif /* _STPU_H */
