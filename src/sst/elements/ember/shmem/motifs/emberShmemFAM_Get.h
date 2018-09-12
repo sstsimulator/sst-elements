3ff6192b6ab6aa11e0c7a1b4fb21a2c88b11b11c
Michael J. Levenhagen <mjleven@sst-devel-new.srn.sandia.gov>
2018-09-12 14:19:17 -0600

    support N FAM, N compute, etc

--- src/sst/elements/ember/shmem/motifs/emberShmemFAM_Get.h
+++ src/sst/elements/ember/shmem/motifs/emberShmemFAM_Get.h
@@ -14,22 +14,22 @@
 // distribution.
 
 
-#ifndef _H_EMBER_SHMEM_TEST1
-#define _H_EMBER_SHMEM_TEST1
+#ifndef _H_EMBER_SHMEM_FAM_GET
+#define _H_EMBER_SHMEM_FAM_GET
 
 #include <strings.h>
 #include "shmem/emberShmemGen.h"
 #include <unistd.h>
 #include <pthread.h>
 #include <sys/types.h>
-       #include <unistd.h>
-       #include <sys/syscall.h>   /* For SYS_xxx definitions */
+#include "rng/xorshift.h"
+
+#include <unistd.h>
+#include <sys/syscall.h>   /* For SYS_xxx definitions */
 
 extern pid_t gettid(void) {
 	return(syscall(SYS_gettid));
 }
-
-
 
 namespace SST {
 namespace Ember {
@@ -52,15 +52,18 @@ public:
 
 public:
 	EmberShmemFAM_GetGenerator(SST::Component* owner, Params& params) :
-		EmberShmemGenerator(owner, params, "ShmemFAM_Get" ), m_phase(Init),m_curBlock(0)
+		EmberShmemGenerator(owner, params, "ShmemFAM_Get" ), m_phase(Init),m_curBlock(0),m_rng(NULL)
 	{ 
         m_nComputeNodes = params.find<int>("arg.nComputeNodes", 0);
         m_nFAMnodes     = params.find<int>("arg.nFAMnodes", 0);
+        m_stride        = params.find<int>("arg.stride", 1);
 		m_nFAMnodeBytes = (size_t) params.find<SST::UnitAlgebra>("arg.nFAMnodeBytes").getRoundedValue(); 
 		m_blockSize	    = params.find<int>("arg.blockSize", 4096);
 		m_vectorSize    = params.find<size_t>("arg.vectorSize", (m_nFAMnodes * m_nFAMnodeBytes) / sizeof(int)); 
 		m_getLoop       = params.find<int>("arg.getLoop", 1);
 		m_partitionSize = (size_t) params.find<SST::UnitAlgebra>("arg.partitionSize","16MiB").getRoundedValue(); 
+		m_maxDelay      = params.find<int>("arg.maxDelay",20);
+		bool useRand    = params.find<int>("arg.useRand",false);
 		
 		assert( m_vectorSize * sizeof(int) <= m_nFAMnodes * m_nFAMnodeBytes );
 		assert( m_nFAMnodes );
@@ -72,6 +75,13 @@ public:
 
         m_miscLib = static_cast<EmberMiscLib*>(getLib("HadesMisc"));
         assert(m_miscLib);
+
+		if ( useRand ) {
+			m_rng = new SST::RNG::XORShiftRNG();
+        	struct timeval start;
+        	gettimeofday( &start, NULL );
+			m_rng->seed( start.tv_usec );
+		}
 	}
 
 	EmberMiscLib* m_miscLib;
@@ -88,6 +98,9 @@ public:
 	int m_curBlock;
 	int m_numBlocks;
 	int m_getLoop;
+	int m_stride;
+	int m_maxDelay;
+	SST::RNG::XORShiftRNG* m_rng;
 
     bool generate( std::queue<EmberEvent*>& evQ) 
 	{
@@ -117,27 +130,32 @@ public:
 				printf("block size:              %d\n",	m_blockSize );
 				printf("number of blocks:        %d\n",	m_numBlocks );
 				printf("vector num elements:     %d\n",	m_vectorSize );
+				if ( m_rng ) {
+					printf("using random:        %d\n",	m_maxDelay );
+				}
 			}
 
 			{
 				size_t bufSize; 
 				std::string tmp;
-				if ( m_node_num < m_nFAMnodes ) {
+				if ( iAmCompute( m_node_num ) ) {
+					tmp = "compute";
+				   	bufSize = m_partitionSize;
+					m_phase = Work;
+				} else if ( iAmFAM( m_node_num ) ) {
 					bufSize = m_nFAMnodeBytes;
 					tmp = "FAM";
 					m_phase = Wait;
 				} else {
-					tmp = "compute";
-				   	bufSize = m_partitionSize;
-					m_phase = Work;
-					printf("pe=%d compute node %d thread %d\n",m_my_pe, m_node_num, gettid());
+					bufSize = 0;
+					tmp = "idle";
+					m_phase = Wait;
 				}
 
-				if ( m_node_num == 0 || m_node_num == m_nFAMnodes ) {
-					printf("%s node buffer size: %lu\n", tmp.c_str(), bufSize );
+				if ( bufSize ) {
+					printf("physNode=%d is %s node %d thread %d\n",m_node_num, tmp.c_str(), calcVirtNum(m_node_num), gettid());
+            		enQ_malloc( evQ, &m_mem, bufSize );
 				}
-
-            	enQ_malloc( evQ, &m_mem, bufSize );
 			}
 
             enQ_barrier_all( evQ );
@@ -152,6 +170,7 @@ public:
 
         case Wait:
             enQ_barrier_all( evQ );
+            enQ_barrier_all( evQ );
 			m_phase = Fini;
             break;
 
@@ -161,12 +180,80 @@ public:
         return m_phase == Fini;
 	}
 
+	bool isStride( int node_num, int which ) {
+		bool ret = (node_num / m_stride) % 2 == which;
+		//printf("%s() node=%d which=%d %s \n",__func__, node_num, which, ret ? "yes":"no" );
+		return  ret;
+	}
+	int calcFAMnum( int node_num ) {
+		int ret = calcVirtNum(node_num);
+		//printf("%s() node=%d -> %d \n",__func__, node_num, ret );
+		return ret; 
+	}
+
+	int calcComputeNum( int node_num ) {
+		int ret = calcVirtNum(node_num);
+		//printf("%s() node=%d -> %d \n",__func__, node_num, ret );
+		return ret;
+	}
+
+	int calcVirtNum( int node_num ) {
+		if ( m_stride == 0 ) return node_num % (m_num_nodes/2);
+
+		int ret = (node_num/m_stride) / 2 * m_stride + node_num % m_stride;
+		return ret;
+	}
+
+	bool iAmFAM( int node_num ) {
+		//printf("%s() node=%d\n",__func__, node_num);
+		if ( m_stride == 0 ) return node_num < m_nFAMnodes;
+
+		bool ret = isStride(node_num,0) && calcFAMnum(node_num) < m_nFAMnodes;
+		//printf("%s() node=%d %d\n",__func__, node_num, ret );
+		return ret;
+	}
+
+	bool iAmCompute( int node_num ) {
+		//printf("%s() node=%d\n",__func__, node_num);
+
+		if ( m_stride == 0 ) return node_num >= m_nFAMnodes && node_num < m_nFAMnodes + m_nComputeNodes;
+
+		bool ret = isStride(node_num,1) && calcComputeNum(node_num) < m_nComputeNodes;
+		//printf("%s() node=%d %d\n",__func__, node_num, ret );
+		return ret; 
+	}
+
+	int calcFAMphysNode( int virt ) {
+		if ( m_stride == 0 ) return virt;
+		int rem = virt % m_stride; 
+		return (virt - rem) * 2  +  rem;
+	}
+
+	int mapPeToFAM( int linePe ) {
+		
+		int phys;
+		if ( m_stride == 0 ) {
+			phys = linePe;
+		} else if (  m_stride == 1 )  {
+			phys = linePe * 2;
+		} else {
+			phys = calcFAMphysNode(linePe);
+		}
+		//printf( "%d:%s() linePe %d -> %d\n", m_my_pe, __func__, linePe, phys );
+		return phys;
+	}
+
 	bool work(  std::queue<EmberEvent*>& evQ ) {
 
 		for ( int i = 0; i < m_getLoop && m_curBlock < m_numBlocks; i++ ) {
 
 			int srcPe = calcBlockOwner( m_curBlock );
 			int srcBlock = calcSrcBlock( m_curBlock );
+
+			if ( m_rng ) {
+				int delay = m_rng->generateNextUInt32();
+				enQ_compute( evQ, delay % m_maxDelay );
+			}
 
 			//printf("pe=%d curBlock=%d srcPe=%d srcBLock=%d\n",m_my_pe,m_curBlock,srcPe,srcBlock);
 
@@ -185,7 +272,7 @@ public:
                     m_dest,
                     m_src, 
 					m_blockSize,
-					srcPe );
+					mapPeToFAM( srcPe ) );
 
 			++m_curBlock;
 		}
