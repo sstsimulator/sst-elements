9b3f43a235f650f92ee3f65066df4d7c631c373f
Mike <mjleven@sandia.gov>
2018-10-26 09:03:11 -0600

    removed unused code

--- src/sst/elements/ember/test/EmberEP.py
+++ src/sst/elements/ember/test/EmberEP.py
@@ -1,71 +1,17 @@
+import pprint
+
 import sst
 from sst.merlin import *
 
-def calcNetMapId( nodeId, nidList ):
+from loadUtils import *
 
-    if nidList == 'Null':
-        return -1
-
-    pos = 0
-    a = nidList.split(',')
-
-    for b in a:
-        c = b.split('-')
-    
-        start = int(c[0])
-        stop = start
-
-        if 2 == len(c):
-            stop = int(c[1])
-
-        if nodeId >= start and nodeId <= stop:
-            return pos + (nodeId - start) 
-
-        pos = pos + ((stop - start) + 1)
-
-    return -1
-
-def calcNetMapSize( nidList ):
-
-    if nidList == 'Null':
-        return 0 
-
-    pos = 0
-    a = nidList.split(',')
-
-    for b in a:
-        c = b.split('-')
-    
-        xx = 1 
-        if 2 == len(c):
-            xx = int(c[1]) - int(c[0]) + 1
-
-        pos += xx
-
-    return pos
-
-def calcMaxNode( nidList ):
-
-    if nidList == 'Null':
-        return 0 
-	
-    max = 0
-    a = nidList.split(',')
-
-    for b in a:
-        c = b.split('-')
-    
-        tmp = int(c[0]) 
-        if 2 == len(c):
-           tmp  = int(c[1])
-
-        if tmp > max:
-            max = tmp
-
-    return max + 1 
 
 class EmberEP( EndPoint ):
     def __init__( self, jobId, driverParams, nicParams, numCores, ranksPerNode, statNodes, nidList, motifLogNodes, detailedModel ): # added motifLogNodes here
+        pprint.pprint(driverParams)
+        pprint.pprint(driverParams)
+        pprint.pprint(nicParams)
+
         self.driverParams = driverParams
         self.nicParams = nicParams
         self.numCores = numCores
