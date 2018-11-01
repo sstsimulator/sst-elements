c2d0036c9b328b05cfd4440e9765c29c1aaf98fd
Mike <mjleven@sandia.gov>
2018-11-01 14:38:09 -0600

    add first step of FAM calls

--- src/sst/elements/ember/shmem/emberShmemGen.h
+++ src/sst/elements/ember/shmem/emberShmemGen.h
@@ -98,6 +98,14 @@ protected:
     inline void enQ_alltoalls64( Queue&, Hermes::MemAddr dest, Hermes::MemAddr src, int dst,
             int sst, size_t nelmes, int PE_start, int logPE_stride, int PE_size, Hermes::MemAddr );
 
+
+	typedef int Fam_Region_Descriptor;
+    inline void enQ_fam_initialize( Queue&, std::string groupName );
+    inline void enQ_fam_finalize( Queue&, std::string groupName );
+    inline void enQ_fam_create_region( Queue&, std::string groupName, uint64_t size, Fam_Region_Descriptor& rd );
+    inline void enQ_fam_create_region( Queue&, Fam_Region_Descriptor rd );
+	inline void enQ_fam_get_nonblocking( Queue&, Hermes::MemAddr, Fam_Region_Descriptor rd, uint64_t offset, uint64_t nbytes );
+
 #define declareOp( type, op) \
     inline void enQ_##type##_##op##_to_all( Queue&, Hermes::MemAddr dest, Hermes::MemAddr src, int nelmes, \
             int PE_start, int logPE_stride, int PE_size, Hermes::MemAddr pSync );\
@@ -159,6 +167,38 @@ protected:
 
 private:
 };
+
+void EmberShmemGenerator::enQ_fam_initialize( Queue& q, std::string groupName ) {
+    verbose(CALL_INFO,2,0,"\n");
+	enQ_init(q);
+}
+
+void EmberShmemGenerator::enQ_fam_finalize( Queue&, std::string groupName ) {
+    verbose(CALL_INFO,2,0,"\n");
+}
+
+void EmberShmemGenerator::enQ_fam_create_region( Queue&, std::string groupName, uint64_t size, Fam_Region_Descriptor& rd ) {
+    verbose(CALL_INFO,2,0,"\n");
+}
+
+void EmberShmemGenerator::enQ_fam_create_region( Queue&, Fam_Region_Descriptor rd ) {
+    verbose(CALL_INFO,2,0,"\n");
+}
+
+void EmberShmemGenerator::enQ_fam_get_nonblocking( Queue& q, Hermes::MemAddr dest, Fam_Region_Descriptor rd, uint64_t offset, uint64_t nbytes ){
+    verbose(CALL_INFO,2,0,"\n");
+
+	assert( m_famAddrMapper );
+	uint64_t localOffset;
+	int 	 node;
+	m_famAddrMapper->getAddr( getOutput(), offset, node, localOffset );
+
+    verbose(CALL_INFO,2,0,"node=%" PRIx32 " localOffset=0x%" PRIx64 "\n", node, localOffset );
+    verbose(CALL_INFO,2,0,"0x%" PRIx64" %p\n", dest.getSimVAddr(), dest.getBacking() );
+
+   	Hermes::MemAddr src( localOffset, NULL );
+	enQ_get_nbi( q, dest, src, nbytes , node );
+}
 
 static inline Hermes::Shmem::Interface* shmem_cast( Hermes::Interface *in )
 {
