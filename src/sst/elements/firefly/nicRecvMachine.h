c5e95341b82faaa51639ca7bbd6b3cf0114ecaed
Michael J. Levenhagen <mjleven@sst-devel-new.srn.sandia.gov>
2018-11-26 14:47:11 -0700

    change verbose level

--- src/sst/elements/firefly/nicRecvMachine.h
+++ src/sst/elements/firefly/nicRecvMachine.h
@@ -110,7 +110,7 @@ class RecvMachine {
                 m_dbg.debug(CALL_INFO,1,NIC_DBG_RECV_MACHINE,"packet available\n");
                 m_nic.schedCallback( std::bind( &Nic::RecvMachine::processPkt, this, ev ));
             } else {
-                m_dbg.debug(CALL_INFO,1,NIC_DBG_RECV_MACHINE,"network idle\n");
+                m_dbg.debug(CALL_INFO,2,NIC_DBG_RECV_MACHINE,"network idle\n");
                 setNotify();
             }
         }
