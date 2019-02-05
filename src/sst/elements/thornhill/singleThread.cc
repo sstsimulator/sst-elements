cd079631d010c784f8d6befc127ba0965c2f7942
Mike <mjleven@sandia.gov>
2019-02-05 13:37:01 -0700

    add support for both a return callback and a generator finished callback

--- src/sst/elements/thornhill/singleThread.cc
+++ src/sst/elements/thornhill/singleThread.cc
@@ -50,11 +50,16 @@ void SingleThread::eventHandler( SST::Event* ev )
 }
 
 void SingleThread::start( const std::deque< std::pair< std::string, SST::Params> >& generators,
-                 std::function<int()> finiHandler )
+                 std::function<int()> retHandler, std::function<int()> finiHandler )
 {
     MirandaReqEvent* event = new MirandaReqEvent;
 	
-	event->key = (uint64_t) new Entry( finiHandler );
+	if ( finiHandler ) {
+		retHandler();
+		event->key = (uint64_t) new Entry( finiHandler );
+	} else {	
+		event->key = (uint64_t) new Entry( retHandler );
+	}
 
 	event->generators = generators;
 
