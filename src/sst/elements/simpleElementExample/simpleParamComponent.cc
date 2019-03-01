efbddcb1841f9accaa423839f1167c86258f7b6f
Simon David Hammond <sdhammo@sst-devel-new.srn.sandia.gov>
2019-02-28 21:10:26 -0700

    Additional print out to check parameter behavior

--- src/sst/elements/simpleElementExample/simpleParamComponent.cc
+++ src/sst/elements/simpleElementExample/simpleParamComponent.cc
@@ -24,32 +24,40 @@ namespace SimpleParamComponent {
 simpleParamComponent::simpleParamComponent(ComponentId_t id, Params& params) :
   Component(id) 
 {
-	int32_t i32v = params.find<int32_t>("int32t-param");
-	printf("int32_t      value=%" PRId32 "\n", i32v);
+	const std::string i32v_str = params.find<std::string>("int32t-param");
+	const int32_t i32v = params.find<int32_t>("int32t-param");
+	printf("int32_t      value = \"%s\" = %" PRId32 "\n", i32v_str.c_str(), i32v);
 
-	uint32_t u32v = params.find<uint32_t>("uint32t-param");
-	printf("uint32_t     value=%" PRIu32 "\n", u32v);
+	const std::string u32v_str = params.find<std::string>("uint32t-param");
+	const uint32_t u32v = params.find<uint32_t>("uint32t-param");
+	printf("uint32_t     value = \"%s\" = %" PRIu32 "\n", u32v_str.c_str(), u32v);
 
-	int64_t i64v = params.find<int64_t>("int64t-param");
-	printf("int64_t      value=%" PRId64 "\n", i64v);
+	const std::string i64v_str = params.find<std::string>("int64t-param");
+	const int64_t i64v = params.find<int64_t>("int64t-param");
+	printf("int64_t      value = \"%s\" = %" PRId64 "\n", i64v_str.c_str(), i64v);
 
-	uint64_t u64v = params.find<uint64_t>("uint64t-param");
-	printf("uint64_t     value=%" PRIu64 "\n", u64v);
+	const std::string u64v_str = params.find<std::string>("uint64t-param");
+	const uint64_t u64v = params.find<uint64_t>("uint64t-param");
+	printf("uint64_t     value = \"%s\" = %" PRIu64 "\n", u64v_str.c_str(), u64v);
 
-	bool btruev = params.find<bool>("bool-true-param");
-	printf("bool-true    value=%s\n", (btruev ? "true" : "false"));
+	const std::string btruev_str = params.find<std::string>("bool-true-param");
+	const bool btruev = params.find<bool>("bool-true-param");
+	printf("bool-true    value = \"%s\" = %s\n", btruev_str.c_str(), (btruev ? "true" : "false"));
 
-	bool bfalsev = params.find<bool>("bool-false-param");
-	printf("bool-false    value=%s\n", (bfalsev ? "true" : "false"));
+	const std::string bfalsev_str = params.find<std::string>("bool-false-param");
+	const bool bfalsev = params.find<bool>("bool-false-param");
+	printf("bool-false   value = \"%s\" = %s\n", bfalsev_str.c_str(), (bfalsev ? "true" : "false"));
 
-	float f32v = params.find<float>("float-param");
-	printf("float         value=%f\n", f32v);
+	const std::string f32v_str = params.find<std::string>("float-param");
+	const float f32v = params.find<float>("float-param");
+	printf("float        value = \"%s\" = %f = %e\n", f32v_str.c_str(), f32v, f32v);
 
-	double f64v = params.find<double>("double-param");
-	printf("double        value=%f\n", f64v);
+	const std::string f64v_str = params.find<std::string>("double-param");
+	const double f64v = params.find<double>("double-param");
+	printf("double       value = \"%s\" = %f = %e\n", f64v_str.c_str(), f64v, f64v);
 
-	std::string strv = params.find<std::string>("string-param");
-	printf("string        value=\"%s\"\n", strv.c_str());
+	const std::string strv = params.find<std::string>("string-param");
+	printf("string       value = \"%s\"\n", strv.c_str());
 
 }
 
--- src/sst/elements/simpleElementExample/tests/test_simpleParamComponent.py
+++ src/sst/elements/simpleElementExample/tests/test_simpleParamComponent.py
@@ -8,9 +8,9 @@ sst.setProgramOption("stopAtCycle", "25us")
 # Define the simulation components
 param_c0 = sst.Component("c0.0", "simpleElementExample.simpleParamComponent")
 param_c0.addParams({
-	"int32t-param" : 32332,
-	"uint32t-param" : 4242424,
-	"int64-param" : 9223372036854775807,
+	"int32t-param" : 2147483647,
+	"uint32t-param" : "4294967295",
+	"int64t-param" : 9223372036854775807,
 	"uint64t-param" : 18446744073709551615,
 	"bool-true-param" : True,
 	"bool-false-param" : False,
@@ -22,9 +22,9 @@ param_c0.addParams({
 # Define the simulation components
 param_c1 = sst.Component("c1.0", "simpleElementExample.simpleParamComponent")
 param_c1.addParams({
-	"int32t-param" : -32332,
-	"uint32t-param" : 4242424,
-	"int64-param" : -9223372036854775808,
+	"int32t-param" : -2147483648,
+	"uint32t-param" : 0,
+	"int64t-param" : -9223372036854775808,
 	"uint64t-param" : 18446744073709551615,
 	"bool-true-param" : "true",
 	"bool-false-param" : "false",
@@ -36,13 +36,13 @@ param_c1.addParams({
 # Define the simulation components
 param_c2 = sst.Component("c2.0", "simpleElementExample.simpleParamComponent")
 param_c2.addParams({
-	"int32t-param" : -32332,
-	"uint32t-param" : 4242424,
-	"int64-param" : -9223372036854775808,
-	"uint64t-param" : 18446744073709551615,
+	"int32t-param" : "2147483647",
+	"uint32t-param" : "4294967295",
+	"int64t-param" : "-9223372036854775808",
+	"uint64t-param" : "18446744073709551615",
 	"bool-true-param" : 1,
 	"bool-false-param" : 0,
-	"float-param" : -1.0000e-15,
-	"double-param" : -1.33e-18,
+	"float-param" : "-1.0000e-15",
+	"double-param" : "-1.33e-18",
 	"string-param" : "teststring123"
 })
