7bff06e4865a6f0ca9727988609443965f998f7f
feldergast <kshemme@sandia.gov>
2019-09-12 06:22:38 -0600

    Fixed parameter description.

--- src/sst/elements/merlin/test/nic.h
+++ src/sst/elements/merlin/test/nic.h
@@ -52,7 +52,7 @@ public:
         {"link_bw",      "Bandwidth of the router link specified in either b/s or B/s (can include SI prefix)."},
         {"in_buf_size",  "Size of linkcontrol input buffer specified in either b/s or B/s (can include SI prefix).", "1kB"},
         {"out_buf_size", "Size of linkcontrol output buffer specified in either b/s or B/s (can include SI prefix).", "1kB"},
-        {"send_untimed_data",   "Controls whether data is sent in init and finish.","true"},
+        {"send_untimed_data",   "Controls whether data is sent in init and complete.","true"},
         {"remap",        "Creates a logical to physical mapping shifted by remap amount.", "0"},
         {"group_offset",   "If dividing network into multiple groups of test nics, this is offset for this group.", "0"},
         {"group_peers",   "If dividing network into multiple groups of test nics, this is offset for this group.", "0"},
