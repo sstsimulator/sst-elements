af2b4f3da2f0c4b52a688fed9058c53436cfc9d7
Aaron Levine <allevin@sandia.gov>
2020-03-30 15:12:08 -0600

    Update copyrights for prospero

--- src/sst/elements/prospero/runprosperotrace.cc
+++ src/sst/elements/prospero/runprosperotrace.cc
@@ -1,8 +1,8 @@
-// Copyright 2009-2019 NTESS. Under the terms
+// Copyright 2009-2020 NTESS. Under the terms
 // of Contract DE-NA0003525 with NTESS, the U.S.
 // Government retains certain rights in this software.
 //
-// Copyright (c) 2009-2019, NTESS
+// Copyright (c) 2009-2020, NTESS
 // All rights reserved.
 //
 // Portions are copyright of other developers:
@@ -75,7 +75,7 @@ int main(int argc, char* argv[]) {
 	appParams.push_back(const_cast<char*>(PINTOOL_EXECUTABLE));
 	appParams.push_back(pinToolMarker);
 	appParams.push_back(toolPath);
-        
+
         bool uselucky = false;
 
 	for(int i = 1; i < argc; i++) {
