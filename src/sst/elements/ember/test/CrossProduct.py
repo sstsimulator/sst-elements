5297a42e3833c9c47936ea19a62cd8da06c7cb41
Aaron Levine <allevin@sandia.gov>
2020-04-14 15:48:57 -0600

    Fixed CrossProduct.py to be a Py3 iterator

--- src/sst/elements/ember/test/CrossProduct.py
+++ src/sst/elements/ember/test/CrossProduct.py
@@ -9,13 +9,13 @@ class CrossProduct:
     def __iter__(self):
         return self
 
-    def next(self):
+    def __next__(self):
         if self.cnts[0] == 0:
             raise StopIteration
         else:
             tmp = ''
             for i, x in enumerate(self.xxx) :
-                tmp += "{0}={1} ".format( x[0], x[1][ self.cnts[i] - 1 ] ) 
+                tmp += "{0}={1} ".format( x[0], x[1][ self.cnts[i] - 1 ] )
 
             for i in reversed(range( 0, len(self.xxx) )):
                 self.cnts[i] -= 1
@@ -26,5 +26,9 @@ class CrossProduct:
                     break
                 else :
                     self.cnts[i] = len(self.xxx[i][1])
-                 
-            return tmp  
+
+            return tmp
+
+    def next(self):
+        return self.__next__()
+
