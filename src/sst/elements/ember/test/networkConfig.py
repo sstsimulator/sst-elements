547cbe47130eded982d1a2d53f7a79117282fd6a
Aaron Levine <allevin@sandia.gov>
2020-04-15 10:41:29 -0600

    Replaced tabs with spaces to fix networkConfig.py for Py3

--- src/sst/elements/ember/test/networkConfig.py
+++ src/sst/elements/ember/test/networkConfig.py
@@ -1,62 +1,62 @@
 import sys
 
 class TopoInfo:
-	def getNumNodes(self):
-		pass
-	def getNetworkParams(self):
-		pass
-	def getNicsPerNode(self):
-		return 1 
+    def getNumNodes(self):
+        pass
+    def getNetworkParams(self):
+        pass
+    def getNicsPerNode(self):
+        return 1
 
 class TorusInfo(TopoInfo):
-	def __init__( self, shape, local_ports, nicsPerNode ):
+    def __init__( self, shape, local_ports, nicsPerNode ):
 
-		width = 1
+        width = 1
 
-		self.params = {}
-		self.params["num_dims"] = self.calcNumDim(shape)
-		self.params["torus:shape"] = shape
-		self.params["torus:width"] = self.calcWidth(shape,width)
-		self.params["torus:local_ports"] = local_ports 
-		self.numNodes = self.calcNumNodes( shape ) * local_ports
-		self.nicsPerNode = nicsPerNode
+        self.params = {}
+        self.params["num_dims"] = self.calcNumDim(shape)
+        self.params["torus:shape"] = shape
+        self.params["torus:width"] = self.calcWidth(shape,width)
+        self.params["torus:local_ports"] = local_ports
+        self.numNodes = self.calcNumNodes( shape ) * local_ports
+        self.nicsPerNode = nicsPerNode
 
-	def getNetworkParams(self):
-		return self.params
+    def getNetworkParams(self):
+        return self.params
 
-	def getNumNodes(self):
-		return self.numNodes
+    def getNumNodes(self):
+        return self.numNodes
 
-	def calcNumDim(self,shape):
-		return len( shape.split( 'x' ) )
+    def calcNumDim(self,shape):
+        return len( shape.split( 'x' ) )
 
-	def calcNumNodes(self,shape):
-		tmp = shape.split( 'x' )
-		num = 1
-		for d in tmp:
-			num = num * int(d)
-		return num
+    def calcNumNodes(self,shape):
+        tmp = shape.split( 'x' )
+        num = 1
+        for d in tmp:
+            num = num * int(d)
+        return num
 
-	def calcWidth(self,shape,width):
-		tmp = len( shape.split( 'x' ) ) - 1
-		retval = str(width) 
-		count = 0
-		while ( count < tmp ):
-			retval += "x" + str(width)
-			count  += 1
-		return retval
+    def calcWidth(self,shape,width):
+        tmp = len( shape.split( 'x' ) ) - 1
+        retval = str(width)
+        count = 0
+        while ( count < tmp ):
+            retval += "x" + str(width)
+            count  += 1
+        return retval
 
-	def getNicsPerNode(self):
-		return self.nicsPerNode 
+    def getNicsPerNode(self):
+        return self.nicsPerNode
 
 class HyperXInfo(TopoInfo):
 
     def __init__( self, param1, param2=None ):
-		self.params = {}
-		if param2:
-			self.initV1( param1, param2 )
-		else:
-			self.initV2( param1 )
+        self.params = {}
+        if param2:
+            self.initV1( param1, param2 )
+        else:
+            self.initV2( param1 )
 
     def initV1( self, shape, local_ports ):
         width = 1
@@ -103,16 +103,16 @@ class HyperXInfo(TopoInfo):
 
 
 class FattreeInfo(TopoInfo):
-	def __init__( self, shape ):
-		self.params = {}
-		self.numNodes = self.calcNumNodes(shape)
-		self.params["fattree:shape"] = shape
+    def __init__( self, shape ):
+        self.params = {}
+        self.numNodes = self.calcNumNodes(shape)
+        self.params["fattree:shape"] = shape
                 
-	def getNetworkParams(self):
-		return self.params
+    def getNetworkParams(self):
+        return self.params
 
-	def getNumNodes(self):
-		return self.numNodes 
+    def getNumNodes(self):
+        return self.numNodes
 
         def calcNumNodes(self, shape):
                 levels = shape.split(":")
@@ -125,73 +125,73 @@ class FattreeInfo(TopoInfo):
                 return total_hosts
 
 class DragonFlyLegacyInfo(TopoInfo):
-	def __init__( self, shape ):
-		radix, lcl, glbl, nRtrs = shape.split(':')
-		self.params = {}
-		hostsPerGroup = int(nRtrs) * int(lcl)
-		nGrp = int(nRtrs) * int(glbl) + 1
-		self.params["router_radix"] = radix
-		self.params["dragonfly:shape"] = "" 
-		self.params["dragonfly:hosts_per_router"] = lcl
-		self.params["dragonfly:routers_per_group"] = nRtrs
-		self.params["dragonfly:intergroup_per_router"] = glbl
-		self.params["dragonfly:num_groups"] =  nGrp
-		self.params["dragonfly:algorithm"] =  "minimal"
+    def __init__( self, shape ):
+        radix, lcl, glbl, nRtrs = shape.split(':')
+        self.params = {}
+        hostsPerGroup = int(nRtrs) * int(lcl)
+        nGrp = int(nRtrs) * int(glbl) + 1
+        self.params["router_radix"] = radix
+        self.params["dragonfly:shape"] = ""
+        self.params["dragonfly:hosts_per_router"] = lcl
+        self.params["dragonfly:routers_per_group"] = nRtrs
+        self.params["dragonfly:intergroup_per_router"] = glbl
+        self.params["dragonfly:num_groups"] =  nGrp
+        self.params["dragonfly:algorithm"] =  "minimal"
 
-		self.numNodes = nGrp * hostsPerGroup
+        self.numNodes = nGrp * hostsPerGroup
                 
-	def getNetworkParams(self):
-		return self.params
+    def getNetworkParams(self):
+        return self.params
 
-	def getNumNodes(self):
-		return self.numNodes 
+    def getNumNodes(self):
+        return self.numNodes
 
 class DragonFlyInfo(TopoInfo):
-	def __init__( self, param ):
-		self.params = {}
-		if type( param ) == dict:
-			self.initV2( param )  
-		else:
-			self.initV1( param )  
+    def __init__( self, param ):
+        self.params = {}
+        if type( param ) == dict:
+            self.initV2( param )
+        else:
+            self.initV1( param )
 
-	def initV1( self, shape ):
-		lcl, nRtrs, glbl, nGrp = shape.split(':')
-		hostsPerGroup = int(nRtrs) * int(lcl)
-		self.params["dragonfly:shape"] = "" 
-		self.params["dragonfly:hosts_per_router"] = lcl
-		self.params["dragonfly:routers_per_group"] = nRtrs
-		self.params["dragonfly:intergroup_links"] = glbl
-		self.params["dragonfly:num_groups"] =  nGrp
-		self.params["dragonfly:algorithm"] =  "minimal" 
+    def initV1( self, shape ):
+        lcl, nRtrs, glbl, nGrp = shape.split(':')
+        hostsPerGroup = int(nRtrs) * int(lcl)
+        self.params["dragonfly:shape"] = ""
+        self.params["dragonfly:hosts_per_router"] = lcl
+        self.params["dragonfly:routers_per_group"] = nRtrs
+        self.params["dragonfly:intergroup_links"] = glbl
+        self.params["dragonfly:num_groups"] =  nGrp
+        self.params["dragonfly:algorithm"] =  "minimal"
 
-		self.numNodes = int(nGrp) * hostsPerGroup
+        self.numNodes = int(nGrp) * hostsPerGroup
 
-	def initV2( self, params ):
-		numGroups = params['num_groups']
-		rtrsPerGroup = params['routers_per_group']
-		hostsPerRtr = params['hosts_per_router']
-		self.numNodes = int(rtrsPerGroup) * int(hostsPerRtr) * int(numGroups)
+    def initV2( self, params ):
+        numGroups = params['num_groups']
+        rtrsPerGroup = params['routers_per_group']
+        hostsPerRtr = params['hosts_per_router']
+        self.numNodes = int(rtrsPerGroup) * int(hostsPerRtr) * int(numGroups)
 
-		self.params["dragonfly:hosts_per_router"] = hostsPerRtr
-		self.params["dragonfly:routers_per_group"] = rtrsPerGroup
-		self.params["dragonfly:intergroup_links"] = params['intergroup_links']
-		self.params["dragonfly:num_groups"] =  numGroups
-		self.params["dragonfly:algorithm"] =  params['algorithm']
+        self.params["dragonfly:hosts_per_router"] = hostsPerRtr
+        self.params["dragonfly:routers_per_group"] = rtrsPerGroup
+        self.params["dragonfly:intergroup_links"] = params['intergroup_links']
+        self.params["dragonfly:num_groups"] =  numGroups
+        self.params["dragonfly:algorithm"] =  params['algorithm']
                 
-	def getNetworkParams(self):
-		return self.params
+    def getNetworkParams(self):
+        return self.params
 
-	def getNumNodes(self):
-		return self.numNodes 
+    def getNumNodes(self):
+        return self.numNodes
 
 class JSONInfo(TopoInfo):
-	def __init__( self, numNodes ):
-		self.params = {}
-		self.numNodes = numNodes
+    def __init__( self, numNodes ):
+        self.params = {}
+        self.numNodes = numNodes
 
-	def getNetworkParams(self):
-		return self.params
+    def getNetworkParams(self):
+        return self.params
 
-	def getNumNodes(self):
-		return self.numNodes
+    def getNumNodes(self):
+        return self.numNodes
 
