102ec07f0ac8946028379c3e2ad6dd51d9281e27
Aaron Levine <allevin@sandia.gov>
2020-07-24 11:48:33 -0600

    Adding kingsley test

---
+++ src/sst/elements/kingsley/tests/testsuite_default_kingsley.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from sst_unittest import *
+from sst_unittest_support import *
+
+################################################################################
+# Code to support a single instance module initialize, must be called setUp method
+
+module_init = 0
+module_sema = threading.Semaphore()
+
+def initializeTestModule_SingleInstance(class_inst):
+    global module_init
+    global module_sema
+
+    module_sema.acquire()
+    if module_init != 1:
+        # Put your single instance Init Code Here
+        module_init = 1
+    module_sema.release()
+
+################################################################################
+
+class testcase_kingsley_Component(SSTTestCase):
+
+    def initializeClass(self, testName):
+        super(type(self), self).initializeClass(testName)
+        # Put test based setup code here. it is called before testing starts
+        # NOTE: This method is called once for every test
+
+    def setUp(self):
+        super(type(self), self).setUp()
+        initializeTestModule_SingleInstance(self)
+        # Put test based setup code here. it is called once before every test
+
+    def tearDown(self):
+        # Put test based teardown code here. it is called once after every test
+        super(type(self), self).tearDown()
+
+#####
+
+    def test_kingsly_noc_mesh_32(self):
+        self.kingsley_test_template("noc_mesh_32_test")
+
+#####
+
+    def kingsley_test_template(self, testcase):
+        # Get the path to the test files
+        test_path = self.get_testsuite_dir()
+        outdir = self.get_test_output_run_dir()
+        tmpdir = self.get_test_output_tmp_dir()
+
+        # Set the various file paths
+        testDataFileName="test_kingsley_{0}".format(testcase)
+
+        sdlfile = "{0}/{1}.py".format(test_path, testcase)
+        reffile = "{0}/refFiles/{1}.out".format(test_path, testDataFileName)
+        outfile = "{0}/{1}.out".format(outdir, testDataFileName)
+        errfile = "{0}/{1}.err".format(outdir, testDataFileName)
+        mpioutfiles = "{0}/{1}.testfile".format(outdir, testDataFileName)
+        newoutfile = "{0}/{1}.newout".format(outdir, testDataFileName)
+        newreffile = "{0}/{1}.newref".format(outdir, testDataFileName)
+
+        #testOutFiles="${SST_TEST_OUTPUTS}/${testDataFileBase}.testFile"
+
+        self.run_sst(sdlfile, outfile, errfile, mpi_out_files=mpioutfiles)
+
+        # Perform the test
+        cmp_result = compare_sorted_diff(testcase, outfile, reffile)
+        self.assertTrue(cmp_result, "Sorted Output file {0} does not match sorted Reference File {1}".format(outfile, reffile))
