6407adbdd5331ca71d80abd75954ce9260b0f9c2
Si Hammond <sdhammo@sandia.gov>
2020-12-09 14:04:51 -0700

    Update basic Vanadis example.

--- src/sst/elements/vanadis/tests/basic_vanadis.py
+++ src/sst/elements/vanadis/tests/basic_vanadis.py
@@ -1,3 +1,4 @@
+import os
 import sst
 
 # Define SST core options
@@ -7,35 +8,44 @@ sst.setProgramOption("stopAtCycle", "0 ns")
 # Tell SST what statistics handling we want
 sst.setStatisticLoadLevel(4)
 
-verbosity = 32
+verbosity = os.getenv("VANADIS_VERBOSE", 0)
 
 v_cpu_0 = sst.Component("v0", "vanadis.VanadisCPU")
 v_cpu_0.addParams({
-       "clock" : "2.0GHz",
+       "clock" : "2.3GHz",
 #       "executable" : "./tests/hello-mips",
 #       "executable" : "./tests/hello-musl",
 #       "executable" : "./tests/core-perf-musl",
 #       "executable" : "./tests/stream-musl",
-       "executable" : "./tests/stream-mini-musl",
+#       "executable" : "./tests/stream-mini-musl",
+#       "executable" : "./tests/test-printf",
+#       "executable" : "./tests/test-env",
+#       "executable" : "./tests/lulesh2.0",
+#       "executable" : "./tests/luxtxt",
+#       "executable" : "./tests/stream-fortran",
 #       "executable" : "./tests/test-fp",
-       "app.env_count" : 1,
+       "executable" : os.getenv("EXE", "./tests/stream-mini-musl"),
+       "app.env_count" : 2,
        "app.env0" : "HOME=/home/sdhammo",
+       "app.env1" : "NEWHOME=/home/sdhammo2",
        "max_cycle" : 100000000,
        "verbose" : verbosity,
-       "physical_fp_registers" : 96,
+       "physical_fp_registers" : 168,
+       "physical_int_registers" : 180,
        "print_int_reg" : 1,
 #      "pipeline_trace_file" : "pipe.trace",
-       "reorder_slots" : 8,
-      "decodes_per_cycle" : 4,
-      "issues_per_cycle" :  4,
-      "retires_per_cycle" : 4
+       "reorder_slots" : 224,
+       "decodes_per_cycle" : 5,
+       "issues_per_cycle" :  6,
+       "retires_per_cycle" : 8
 })
 
 decode0   = v_cpu_0.setSubComponent( "decoder0", "vanadis.VanadisMIPSDecoder" )
 os_hdlr   = decode0.setSubComponent( "os_handler", "vanadis.VanadisMIPSOSHandler" )
 
 decode0.addParams({
-
+	"uop_cache_entries" : 1536,
+	"predecode_cache_entries" : 4
 })
 
 os_hdlr.addParams({
@@ -52,28 +62,34 @@ v_cpu_0_lsq.addParams({
 	"address_mask" : 0xFFFFFFFF,
 #	"address_trace" : "address-lsq2.trace",
 #	"allow_speculated_operations" : 0,
-	"load_store_entries" : 8
+	"load_store_entries" : 56,
+	"fault_non_written_loads_after" : 0,
+	"check_memory_loads" : "no"
 })
 
 dcache_if = v_cpu_0_lsq.setSubComponent( "memory_interface", "memHierarchy.memInterface" )
 
 node_os = sst.Component("os", "vanadis.VanadisNodeOS")
 node_os.addParams({
-	"verbose" : 16,
-	"cores" : 1
+	"verbose" : verbosity,
+	"cores" : 1,
+	"heap_start" : 512 * 1024 * 1024,
+	"heap_end"   : (2 * 1024 * 1024 * 1024) - 4096,
+	"page_size"  : 4096,
+	"heap_verbose" : verbosity
 })
 
 node_os_mem_if = node_os.setSubComponent( "mem_interface", "memHierarchy.memInterface" )
 
 os_l1dcache = sst.Component("node_os.l1dcache", "memHierarchy.Cache")
 os_l1dcache.addParams({
-      "access_latency_cycles" : "1",
-      "cache_frequency" : "2 GHz",
+      "access_latency_cycles" : "2",
+      "cache_frequency" : "2.3GHz",
       "replacement_policy" : "lru",
       "coherence_protocol" : "MESI",
-      "associativity" : "2",
+      "associativity" : "8",
       "cache_line_size" : "64",
-      "cache_size" : "1 KB",
+      "cache_size" : "32 KB",
       "L1" : "1",
       "debug" : 0,
       "debug_level" : 0
@@ -81,13 +97,13 @@ os_l1dcache.addParams({
 
 cpu0_l1dcache = sst.Component("cpu0.l1dcache", "memHierarchy.Cache")
 cpu0_l1dcache.addParams({
-      "access_latency_cycles" : "1",
-      "cache_frequency" : "2 GHz",
+      "access_latency_cycles" : "2",
+      "cache_frequency" : "2.3GHz",
       "replacement_policy" : "lru",
       "coherence_protocol" : "MESI",
-      "associativity" : "2",
+      "associativity" : "8",
       "cache_line_size" : "64",
-      "cache_size" : "8 KB",
+      "cache_size" : "32 KB",
       "L1" : "1",
       "debug" : 0,
       "debug_level" : 0
@@ -95,13 +111,13 @@ cpu0_l1dcache.addParams({
 
 cpu0_l1icache = sst.Component("cpu0.l1icache", "memHierarchy.Cache")
 cpu0_l1icache.addParams({
-      "access_latency_cycles" : "1",
-      "cache_frequency" : "2 GHz",
+      "access_latency_cycles" : "2",
+      "cache_frequency" : "2.3GHz",
       "replacement_policy" : "lru",
       "coherence_protocol" : "MESI",
-      "associativity" : "2",
+      "associativity" : "8",
       "cache_line_size" : "64",
-      "cache_size" : "8 KB",
+      "cache_size" : "32 KB",
       "prefetcher" : "cassini.NextBlockPrefetcher",
       "prefetcher.reach" : 1,
       "L1" : "1",
@@ -109,23 +125,23 @@ cpu0_l1icache.addParams({
 
 cpu0_l2cache = sst.Component("l2cache", "memHierarchy.Cache")
 cpu0_l2cache.addParams({
-      "access_latency_cycles" : "1",
-      "cache_frequency" : "2 GHz",
+      "access_latency_cycles" : "14",
+      "cache_frequency" : "2.3GHz",
       "replacement_policy" : "lru",
       "coherence_protocol" : "MESI",
-      "associativity" : "8",
+      "associativity" : "16",
       "cache_line_size" : "64",
-      "cache_size" : "256 KB",
+      "cache_size" : "1MB",
 })
 
 cache_bus = sst.Component("bus", "memHierarchy.Bus")
 cache_bus.addParams({
-      "bus_frequency" : "2 GHz",
+      "bus_frequency" : "2.3GHz",
 })
 
 memctrl = sst.Component("memory", "memHierarchy.MemController")
 memctrl.addParams({
-      "clock" : "1GHz",
+      "clock" : "2.3Ghz",
       "backend.mem_size" : "4GiB",
       "backing" : "malloc"
 })
