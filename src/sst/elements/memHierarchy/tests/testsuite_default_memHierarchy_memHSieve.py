f739edd2e2fe3b3d442548f963c5ebefac6f0180
Aaron Levine <allevin@sandia.gov>
2020-12-10 09:24:38 -0700

    Got memHSieve working with PIN2

--- src/sst/elements/memHierarchy/tests/testsuite_default_memHierarchy_memHSieve.py
+++ src/sst/elements/memHierarchy/tests/testsuite_default_memHierarchy_memHSieve.py
@@ -12,6 +12,7 @@ import csv
 
 module_init = 0
 module_sema = threading.Semaphore()
+pin_exec_path = ""
 
 def initializeTestModule_SingleInstance(class_inst):
     global module_init
@@ -24,6 +25,52 @@ def initializeTestModule_SingleInstance(class_inst):
         module_init = 1
 
     module_sema.release()
+
+###
+
+def is_PIN_loaded():
+    # Look to see if PIN is available
+    pindir_found = False
+    pin_path = os.environ.get('INTEL_PIN_DIRECTORY')
+    if pin_path is not None:
+        pindir_found = os.path.isdir(pin_path)
+    log_debug("memHSieve Test - Intel_PIN_Path = {0}; Valid Dir = {1}".format(pin_path, pindir_found))
+    return pindir_found
+
+def is_PIN_Compiled():
+    global pin_exec_path
+    pin_crt = sst_elements_config_include_file_get_value_int("HAVE_PINCRT", 0, True)
+    pin_exec = sst_elements_config_include_file_get_value_str("PINTOOL_EXECUTABLE", "", True)
+    log_debug("memHSieve Test - Detected PIN_CRT = {0}".format(pin_crt))
+    log_debug("memHSieve Test - Detected PIN_EXEC = {0}".format(pin_exec))
+    pin_exec_path = pin_exec
+    return pin_exec != ""
+
+def is_Pin2_used():
+    global pin_exec_path
+    if is_PIN_Compiled():
+        if "/pin.sh" in pin_exec_path:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def is_Pin3_used():
+    global pin_exec_path
+    if is_PIN_Compiled():
+        if is_Pin2_used():
+            return False
+        else:
+            # Make sure pin is at the end of the string
+            pinstr = "/pin"
+            idx = pin_exec_path.rfind(pinstr)
+            if idx == -1:
+                return False
+            else:
+                return (idx+len(pinstr)) == len(pin_exec_path)
+    else:
+        return False
 
 ################################################################################
 ################################################################################
@@ -46,20 +93,22 @@ class testcase_memHierarchy_memHSieve(SSTTestCase):
         super(type(self), self).tearDown()
 
 #####
-    # Look to see if PIN is available
-    pindir_found = False
-    pin_path = os.environ.get('INTEL_PIN_DIRECTORY')
-    if pin_path is not None:
-        pindir_found = os.path.isdir(pin_path)
-    log_debug("memHSieve Test - Intel_PIN_Path = {0}; Valid Dir = {1}".format(pin_path, pindir_found))
+    pin_compiled = is_PIN_Compiled()
+    pin_version_valid = is_Pin2_used() | is_Pin3_used()
+    pin_loaded = is_PIN_loaded()
 
-    @unittest.skipIf(not pindir_found, "memHSieve: Requires PIN, but Env Var 'INTEL_PIN_DIR' is not found or path does not exist.")
+    @unittest.skipIf(not pin_compiled, "memHSieve: Requires PIN, but PinTool is not compiled with Elements. In sst_element_config.h PINTOOL_EXECUTABLE={0}".format(pin_exec_path))
+    @unittest.skipIf(not pin_version_valid, "memHSieve: Requires PIN, but PinTool does not seem to be a valid version. PINTOOL_EXECUTABLE={0}".format(pin_exec_path))
+    @unittest.skipIf(not pin_loaded, "memHSieve: Requires PIN, but Env Var 'INTEL_PIN_DIR' is not found or path does not exist.")
     def test_memHSieve(self):
         self.memHSieve_Template("memHSieve")
 
 #####
 
     def memHSieve_Template(self, testcase):
+
+        pin2defined = is_Pin2_used()
+        pin3defined = is_Pin3_used()
 
         # Get the path to the test files
         test_path = self.get_testsuite_dir()
@@ -98,19 +147,55 @@ class testcase_memHierarchy_memHSieve(SSTTestCase):
         TestFailureMsg = ""
 
         ######
-        # Test if ANY Backtrace files exist
-        # THIS IS THE PIN3 CHECKS
-        pattern = "backtrace_*.txt"
-        file_found = False
         backtrace_good = False
-        for filename in os.listdir(testMemHSieveDir):
-            if fnmatch.fnmatch(filename, pattern):
-                file_found = True
-                break
-        if file_found == True:
-            backtrace_good = True
+        if pin3defined:
+            # FOR PIN3
+            # Make sure ANY of the backtrace*.txt files exist
+            pattern = "backtrace_*.txt"
+            file_found = False
+            for filename in os.listdir(testMemHSieveDir):
+                if fnmatch.fnmatch(filename, pattern):
+                    file_found = True
+                    break
+            if file_found == True:
+                backtrace_good = True
+            else:
+                TestFailureMsg += "Did not find any {0} files in directory {1}; ".format(pattern, testMemHSieveDir)
         else:
-            TestFailureMsg += "Did not find any {0} files in directory {1}; ".format(pattern, testMemHSieveDir)
+            # FOR PIN2
+            # Make sure ANY of the backtrace*.txt.gz files exist
+            pattern = "backtrace_*.txt.gz"
+            file_found = False
+            for filename in os.listdir(testMemHSieveDir):
+                if fnmatch.fnmatch(filename, pattern):
+                    file_found = True
+                    break
+            if file_found == True:
+                backtrace_good = True
+            else:
+                TestFailureMsg += "Did not find any {0} files in directory {1}; ".format(pattern, testMemHSieveDir)
+
+            # Unzip all expanded backtrace*.txt.gz files
+            for filename in os.listdir(testMemHSieveDir):
+                if fnmatch.fnmatch(filename, pattern):
+                    filepath = "{0}/{1}".format(testMemHSieveDir, filename)
+                    cmd = 'gzip -d {0}'.format(filepath)
+                    os.system(cmd)
+
+            # Make sure all the unzipped backtrace*.txt.gz files have data
+            pattern = "backtrace_*.txt"
+            file_found = False
+            for filename in os.listdir(testMemHSieveDir):
+                if fnmatch.fnmatch(filename, pattern):
+                    file_found = True
+                    filepath = "{0}/{1}".format(testMemHSieveDir, filename)
+                    if os_test_file(filepath, expression='-s') == False:
+                        backtrace_good = False
+                        TestFailureMsg += "Unzipped Backtrace File {0} does not contain data; ".format(filename)
+            if file_found == False:
+                TestFailureMsg += "Did not find any unzipped {0} files in directory {1}; ".format(pattern, testMemHSieveDir)
+                backtrace_good = False
+
         TestPassed &= backtrace_good
 
         ######
@@ -134,6 +219,7 @@ class testcase_memHierarchy_memHSieve(SSTTestCase):
                 TestFailureMsg += "File {0} does not contain any words; ".format(filepath)
         else:
             TestFailureMsg += "Did not find any {0} files in directory {1}; ".format(pattern, testMemHSieveDir)
+
         TestPassed &= mallocrank_good
 
         ######
@@ -163,6 +249,7 @@ class testcase_memHierarchy_memHSieve(SSTTestCase):
             if statnamefoundinfile == False:
                 stats_good = False
                 TestFailureMsg += "Statistic '{0}' Not found in any statistics file; ".format(statname, statfilepath)
+
         TestPassed &= stats_good
 
         ######
@@ -176,6 +263,7 @@ class testcase_memHierarchy_memHSieve(SSTTestCase):
         cmp_result = testing_compare_diff(testDataFileName, grep_outfile, grep_reffile)
         if cmp_result == False:
                 TestFailureMsg += "Diffed grepped (lines 23-43) compared Output file {0} does not match Reference File {1}".format(grep_outfile, grep_reffile)
+
         TestPassed &= cmp_result
 
         ######
@@ -244,6 +332,27 @@ class testcase_memHierarchy_memHSieve(SSTTestCase):
         return rtn_result
 
 
+
+    # NOTE: This is the bash code from the bamboo test system that possibly cleans up
+    #       the old ompsievetest runs.  We are not doing this unless necessary
+    #       for the new frameworks
+    #Remove_old_ompsievetest_task() {
+    #memHS_PID=$$
+    #   echo " Begin Remover -------------------"
+    #ps -ef | grep ompsievetest | grep -v -e grep
+    #echo "  remover  ==== $LINENO   "
+    #ps -ef | grep ompsievetest | grep -v -e grep > /tmp/${memHS_PID}_omps_list
+    #wc /tmp/${memHS_PID}_omps_list
+    #while read -u 3 _who _anOMP _own _rest
+    #do
+    #    if [ $_own == 1 ] ; then
+    #        echo " Attempt to remove $_anOMP "
+    #        kill -9 $_anOMP
+    #    fi
+    #done 3</tmp/${memHS_PID}_omps_list
+    #
+    #rm /tmp/${memHS_PID}_omps_list
+    #}
 
 
 
