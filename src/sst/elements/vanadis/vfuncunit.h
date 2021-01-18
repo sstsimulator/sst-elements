aa5dc44803189dbd6dd2517f165ca95de7b25eef
Simon David Hammond <sdhammo@sst-devel-new.srn.sandia.gov>
2021-01-18 16:44:01 -0700

    Update functional unit to be cleaner and have simpler design.

--- src/sst/elements/vanadis/vfuncunit.h
+++ src/sst/elements/vanadis/vfuncunit.h
@@ -5,26 +5,46 @@
 #include <cstdint>
 #include <cinttypes>
 #include <climits>
+#include <deque>
 
 #include "inst/vinst.h"
-#include "decoder/visaopts.h"
 
 namespace SST {
 namespace Vanadis {
 
-class VanadisNoInstruction : public VanadisInstruction {
+class VanadisFunctionalUnitInsRecord {
 public:
-	VanadisNoInstruction(const VanadisDecoderOptions* opts) :
-		VanadisInstruction( UINT64_MAX, UINT32_MAX, opts, 0, 0, 0, 0, 0, 0, 0, 0 ) {}
+	VanadisFunctionalUnitInsRecord( VanadisInstruction* base_ins, uint16_t cycles ) :
+		ins(base_ins), cycles_left(cycles) {}
+	~VanadisFunctionalUnitInsRecord() {}
 
-	virtual const char* getInstCode() const { return "VOID"; }
-	VanadisInstruction* clone() { return new VanadisNoInstruction( *this ); };
-	virtual VanadisFunctionalUnitType getInstFuncType() const { return INST_NOOP; }
-	virtual void execute( SST::Output* output, VanadisRegisterFile* rFile) {
-		markExecuted();
+	void markExecuted() {
+		ins->markExecuted();
 	}
 
-	virtual bool performDeleteAtFuncUnit() const { return true; }
+	uint16_t getCycles() const {
+		return cycles_left;
+	}
+
+	bool completed() const {
+		return (0 == cycles_left);
+	}
+
+	void tick() {
+		cycles_left--;
+	}
+
+	uint32_t getHardwareThread() const {
+		return ins->getHWThread();
+	}
+
+	VanadisInstruction* getInstruction() {
+		return ins;
+	}
+
+private:
+	VanadisInstruction* ins;
+	uint16_t cycles_left;
 };
 
 class VanadisFunctionalUnit {
@@ -32,20 +52,16 @@ class VanadisFunctionalUnit {
 public:
 	VanadisFunctionalUnit( uint16_t id,
 		VanadisFunctionalUnitType unit_type,
-		uint16_t latency) : fu_id(id), fu_type(unit_type) {
-
-		fu_queue = new VanadisCircularQueue<VanadisInstruction*>(latency);
-
-		for(uint16_t i = 0; i < latency; ++i) {
-			fu_queue->push(new VanadisNoInstruction(isa_options));
-		}
+		uint16_t lat) : fu_id(id), fu_type(unit_type),
+		latency(lat) {
 
 		slot_inst = nullptr;
-		isa_options = new VanadisDecoderOptions();
 	}
 
 	~VanadisFunctionalUnit() {
-		delete fu_queue;
+		for( auto q_itr = pending_execute.begin(); q_itr != pending_execute.end(); q_itr++ ) {
+			delete (*q_itr);
+		}
 	}
 
 	VanadisFunctionalUnitType getType() const {
@@ -63,29 +79,29 @@ public:
 	}
 
 	void tick(const uint64_t cycle, SST::Output* output, std::vector<VanadisRegisterFile*>& regFile) {
-		output->verbose(CALL_INFO, 8, 0, "Functional Unit %" PRIu16 " (%s) executing cycle %" PRIu64 " (func-q: %" PRIu32 ")...\n",
-			fu_id, funcTypeToString(fu_type), cycle, (uint32_t) fu_queue->size());
+		if( pending_execute.size() > 0 ) {
+			VanadisFunctionalUnitInsRecord* q_front = pending_execute.front();
 
-		VanadisInstruction* ins = fu_queue->pop();
+			if( q_front->completed() ) {
+				VanadisInstruction* inner_ins = q_front->getInstruction();
+				inner_ins->execute(output, regFile[inner_ins->getHWThread()]);
 
-		if( nullptr == ins ) {
-			output->fatal(CALL_INFO, -1, "Error - should not have a null entry in a functional unit queue, if no instruction fill with voids.\n");
+				q_front->markExecuted();
+				pending_execute.pop_front();
+
+				if( inner_ins->performDeleteAtFuncUnit() ) {
+					delete q_front;
+				}
+			}
+
+			for( auto q_itr = pending_execute.begin(); q_itr != pending_execute.end(); q_itr++ ) {
+				(*q_itr)->tick();
+			}
 		}
 
-		if( UINT32_MAX == ins->getHWThread() ) {
-			ins->execute(output, nullptr);
-		} else {
-			ins->execute(output, regFile[ins->getHWThread()]);
-		}
-
-		if( ins->performDeleteAtFuncUnit() ) {
-			delete ins;
-		}
-
-		if( nullptr == slot_inst ) {
-			fu_queue->push( new VanadisNoInstruction(isa_options) );
-		} else {
-			fu_queue->push(slot_inst);
+		// Were we given a new instruction this cycle? If yes, then add to the queue
+		if( slot_inst != nullptr ) {
+			pending_execute.push_back( new VanadisFunctionalUnitInsRecord( slot_inst, latency ) );
 			slot_inst = nullptr;
 		}
 	}
@@ -93,30 +109,25 @@ public:
 	void clearByHWThreadID( SST::Output* output, const uint16_t hw_thr ) {
 		output->verbose(CALL_INFO, 16, 0, "-> Function Unit, clearing by hardware thread %" PRIu32 "...\n", hw_thr);
 
-		VanadisCircularQueue<VanadisInstruction*>* tmp_q = new
-			VanadisCircularQueue<VanadisInstruction*>( fu_queue->capacity() );
-
-		VanadisInstruction* tmp_ins;
-
-		for( uint16_t i = 0; i < fu_queue->size(); ++i ) {
-			tmp_ins = fu_queue->pop();
-
-			if( hw_thr == tmp_ins->getHWThread() ) {
-				tmp_q->push( new VanadisNoInstruction( isa_options ) );
-			} else {
-				tmp_q->push( tmp_ins );
+		for( auto q_itr = pending_execute.begin(); q_itr != pending_execute.end(); ) {
+			// if we get a hardware thread match, remove and carry out
+			if( (*q_itr)->getHardwareThread() == hw_thr ) {
+				delete (*q_itr);
+				q_itr = pending_execute.erase( q_itr );
 			}
 		}
 
-		delete fu_queue;
-		fu_queue = tmp_q;
-
-
+		if( slot_inst != nullptr ) {
+			if( slot_inst->getHWThread() == hw_thr ) {
+				slot_inst = nullptr;
+			}
+		}
 	}
 
 private:
-	VanadisCircularQueue<VanadisInstruction*>* fu_queue;
-	const VanadisDecoderOptions* isa_options;
+	std::deque<VanadisFunctionalUnitInsRecord*> pending_execute;
+
+	const uint16_t latency;
 	VanadisFunctionalUnitType fu_type;
 	VanadisInstruction* slot_inst;
 	const uint16_t fu_id;
