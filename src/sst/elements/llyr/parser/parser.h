dd53633b4e3a4d87792ca0fb3c86674956d41bd4
hughes <chughes@sandia.gov>
2021-03-22 17:04:34 -0600

    Parser staging

---
+++ src/sst/elements/llyr/parser/parser.h
@@ -0,0 +1,28 @@
+// Copyright 2009-2020 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2020, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
+#ifndef _PARSER_H
+#define _PARSER_H
+
+#include <sst/core/sst_config.h>
+
+namespace SST {
+namespace Llyr {
+
+
+} // namespace LLyr
+} // namespace SST
+
+#endif /* _PARSER_H */
