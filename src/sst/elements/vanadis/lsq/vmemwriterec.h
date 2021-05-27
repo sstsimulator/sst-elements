08f2ee41625742eba781ae7a36646e3e0f80cd25
Hammond <sdhammo@positron.srn.sandia.gov>
2021-05-27 17:50:04 -0600

    Fix up coding style to existing SST core coding standards ready for some refactoring

--- src/sst/elements/vanadis/lsq/vmemwriterec.h
+++ src/sst/elements/vanadis/lsq/vmemwriterec.h
@@ -25,62 +25,59 @@ namespace Vanadis {
 class VanadisMemoryWrittenRecord {
 
 public:
-	VanadisMemoryWrittenRecord( int verbosity, uint64_t memory_size ) {
-		// Round up the next highest number of lines unless we evenly divide
-		uint64_t lines = (memory_size / 64) + ( (memory_size % 64) == 0 ? 0 : 1 );
+    VanadisMemoryWrittenRecord(int verbosity, uint64_t memory_size) {
+        // Round up the next highest number of lines unless we evenly divide
+        uint64_t lines = (memory_size / 64) + ((memory_size % 64) == 0 ? 0 : 1);
 
-		printf("creating a memory record with size: %" PRIu64 " = %" PRIu64 " lines.\n", memory_size, lines);
+        printf("creating a memory record with size: %" PRIu64 " = %" PRIu64 " lines.\n", memory_size, lines);
 
-		// Zero out the memory - nothing written at start
-		for( uint64_t i = 0; i < lines; ++i ) {
-			memory_state.push_back(0);
-		}
-	}
+        // Zero out the memory - nothing written at start
+        for (uint64_t i = 0; i < lines; ++i) {
+            memory_state.push_back(0);
+        }
+    }
 
-	~VanadisMemoryWrittenRecord() {
-		memory_state.clear();
-	}
+    ~VanadisMemoryWrittenRecord() { memory_state.clear(); }
 
-	void markByte( uint64_t byte_addr ) {
-		uint64_t line        = byte_addr / 64;
-		uint64_t line_offset = byte_addr % 64;
+    void markByte(uint64_t byte_addr) {
+        uint64_t line = byte_addr / 64;
+        uint64_t line_offset = byte_addr % 64;
 
-		uint64_t line_value  = memory_state[line];
-		uint64_t bit_update  = 1;
-		bit_update = bit_update << line_offset;
+        uint64_t line_value = memory_state[line];
+        uint64_t bit_update = 1;
+        bit_update = bit_update << line_offset;
 
-		line_value = (line_value | bit_update);
+        line_value = (line_value | bit_update);
 
-		//printf("mark-address: 0x%llx / line: %" PRIu64 " / offset: %" PRIu64 " / before 0x%llx / after 0x%llx\n", byte_addr,
-		//	line, line_offset, memory_state[line], line_value);
+        // printf("mark-address: 0x%llx / line: %" PRIu64 " / offset: %" PRIu64 " /
+        // before 0x%llx / after 0x%llx\n", byte_addr, 	line, line_offset,
+        // memory_state[line], line_value);
 
-		memory_state[line] = line_value;
-	}
+        memory_state[line] = line_value;
+    }
 
-	bool isMarked( uint64_t byte_addr ) {
-		uint64_t line        = byte_addr / 64;
-		uint64_t line_offset = byte_addr % 64;
+    bool isMarked(uint64_t byte_addr) {
+        uint64_t line = byte_addr / 64;
+        uint64_t line_offset = byte_addr % 64;
 
-		uint64_t line_value  = memory_state[line];
-		uint64_t bit_value   = 1;
-		bit_value = bit_value << line_offset;
+        uint64_t line_value = memory_state[line];
+        uint64_t bit_value = 1;
+        bit_value = bit_value << line_offset;
 
-		const bool is_marked = (line_value & bit_value) != 0;
+        const bool is_marked = (line_value & bit_value) != 0;
 
-		printf("check-address: 0x%llx / line: %" PRIu64 " / offset: %" PRIu64 " / value: 0x%llx / bit_value: 0x%llx / marked: %3s\n",
-			byte_addr, line, line_offset, line_value, bit_value,
-			is_marked ? "yes" : "no");
+        printf("check-address: 0x%llx / line: %" PRIu64 " / offset: %" PRIu64
+               " / value: 0x%llx / bit_value: 0x%llx / marked: %3s\n",
+               byte_addr, line, line_offset, line_value, bit_value, is_marked ? "yes" : "no");
 
-		return is_marked;
-	}
+        return is_marked;
+    }
 
 private:
-	std::vector<uint64_t> memory_state;
-
-
+    std::vector<uint64_t> memory_state;
 };
 
-}
-}
+} // namespace Vanadis
+} // namespace SST
 
 #endif
