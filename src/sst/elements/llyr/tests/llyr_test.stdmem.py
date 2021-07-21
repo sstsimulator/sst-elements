77a8bb20582b71180df330c4a921ade730e98b89
hughes <chughes>
2021-07-20 21:09:33 -0600

    More tests

---
+++ src/sst/elements/llyr/tests/llyr_test.stdmem.py
@@ -0,0 +1,68 @@
+# Automatically generated SST Python input
+import sst
+
+# Define SST core options
+sst.setProgramOption("timebase", "1 ps")
+sst.setProgramOption("stopAtCycle", "10000s")
+
+statLevel = 16
+max_addr_gb = 1
+tile_clk_mhz = 1
+
+# Define the simulation components
+df_0 = sst.Component("df_0", "llyr.LlyrDataflow")
+df_0.addParams({
+    "verbose": 20,
+    "clock" : str(tile_clk_mhz) + "GHz",
+    "mem_init"      : "spmm-mem.in",
+    "application"   : "spmm.in",
+    "hardware_graph": "hardware.cfg",
+    "mapper"        : "llyr.mapper.simple"
+})
+iface = df_0.setSubComponent("memory", "memHierarchy.standardInterface")
+
+df_l1cache = sst.Component("l1cache", "memHierarchy.Cache")
+df_l1cache.addParams({
+    "access_latency_cycles" : "2",
+    "cache_frequency" : str(tile_clk_mhz) + "GHz",
+    "replacement_policy" : "lru",
+    "coherence_protocol" : "MESI",
+    "associativity" : "1",
+    "cache_line_size" : "16",
+    "verbose" : 10,
+    "debug" : 1,
+    "debug_level" : 100,
+    "L1" : "1",
+    "cache_size" : "512B"
+})
+
+df_memory = sst.Component("memory", "memHierarchy.MemController")
+df_memory.addParams({
+    "backing" : "mmap",
+    "verbose" : 10,
+    "debug" : 1,
+    "debug_level" : 100,
+    "clock" : str(tile_clk_mhz) + "GHz",
+})
+
+backend = df_memory.setSubComponent("backend", "memHierarchy.simpleMem")
+backend.addParams({
+    "access_time" : "4 ns",
+    #"mem_size" : str(max_addr_gb) + "GiB",
+    "mem_size" : str(16384) + "B",
+})
+
+# Enable SST Statistics Outputs for this simulation
+sst.setStatisticLoadLevel(statLevel)
+sst.enableAllStatisticsForAllComponents({"type":"sst.AccumulatorStatistic"})
+sst.setStatisticOutput("sst.statOutputTXT", { "filepath" : "output.csv" })
+
+# Define the simulation links
+link_df_cache_link = sst.Link("link_cpu_cache_link")
+link_df_cache_link.connect( (iface, "port", "1ps"), (df_l1cache, "high_network_0", "1ps") )
+link_df_cache_link.setNoCut()
+
+link_mem_bus_link = sst.Link("link_mem_bus_link")
+link_mem_bus_link.connect( (df_l1cache, "low_network_0", "5ps"), (df_memory, "direct_link", "5ps") )
+
+
