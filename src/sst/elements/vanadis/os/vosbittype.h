e550ab33eabe6fa53d696e5a57cc6e0d30634a18
Simon David Hammond <sdhammo@sst-devel-new.srn.sandia.gov>
2021-11-24 14:47:26 -0700

    Add a way to communicate word size/bit widths for OS calls

---
+++ src/sst/elements/vanadis/os/vosbittype.h
@@ -0,0 +1,16 @@
+
+#ifndef _H_VANADIS_OS_BIT_TYPE
+#define _H_VANADIS_OS_BIT_TYPE
+
+namespace SST {
+namespace Vanadis {
+
+enum class VanadisOSBitType {
+   VANADIS_OS_32B,
+   VANADIS_OS_64B
+};
+
+}
+}
+
+#endif
