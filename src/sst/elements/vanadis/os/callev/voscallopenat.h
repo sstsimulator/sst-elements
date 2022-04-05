2ca06a5d9b2ed97b6a4f17fdc1d91cfdcc67f27a
Michael J. Levenhagen <mjleven@sst-devel-new.srn.sandia.gov>
2022-04-05 13:37:45 -0600

    update to include the openat mode parameter

--- src/sst/elements/vanadis/os/callev/voscallopenat.h
+++ src/sst/elements/vanadis/os/callev/voscallopenat.h
@@ -24,19 +24,21 @@ namespace Vanadis {
 class VanadisSyscallOpenAtEvent : public VanadisSyscallEvent {
 public:
     VanadisSyscallOpenAtEvent() : VanadisSyscallEvent() {}
-    VanadisSyscallOpenAtEvent(uint32_t core, uint32_t thr, VanadisOSBitType bittype, uint64_t dirfd, uint64_t path_ptr, uint64_t flags)
-        : VanadisSyscallEvent(core, thr, bittype), openat_dirfd(dirfd), openat_path_ptr(path_ptr), openat_flags(flags) {}
+    VanadisSyscallOpenAtEvent(uint32_t core, uint32_t thr, VanadisOSBitType bittype, uint64_t dirfd, uint64_t path_ptr, uint64_t flags, uint64_t mode)
+        : VanadisSyscallEvent(core, thr, bittype), openat_dirfd(dirfd), openat_path_ptr(path_ptr), openat_flags(flags), openat_mode(mode) {}
 
     VanadisSyscallOp getOperation() { return SYSCALL_OP_OPENAT; }
 
     int64_t getDirectoryFileDescriptor() const { return openat_dirfd; }
     uint64_t getPathPointer() const { return openat_path_ptr; }
     int64_t getFlags() const { return openat_flags; }
+    int64_t getMode() const { return openat_mode; }
 
 private:
     int64_t openat_dirfd;
     uint64_t openat_path_ptr;
     int64_t openat_flags;
+    int64_t openat_mode;
 };
 
 } // namespace Vanadis
