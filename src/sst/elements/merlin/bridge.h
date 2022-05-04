5d542cb68356b4673bef896e1056c9f1e25d74d9
Joseph Kenny <jpkenny@sandia.gov>
2022-05-04 10:57:28 -0700

    move merlin bridge code into header, addresses issue #1898

--- src/sst/elements/merlin/bridge.h
+++ src/sst/elements/merlin/bridge.h
@@ -69,11 +69,79 @@ public:
          "SST::Interfaces::SimpleNetwork" }
     )
 
-    Bridge(SST::ComponentId_t id, SST::Params &params);
-    ~Bridge();
-    void init(unsigned int);
-    void setup(void);
-    void finish(void);
+    Bridge(SST::ComponentId_t id, SST::Params &params) :
+        SST::Component(id)
+    {
+        int debugLevel = params.find<int>("debug_level", 0);
+        dbg.init("@t:Bridge::@p():@l " + getName() + ": ",
+                debugLevel, 0, (Output::output_location_t)params.find<int>("debug", 0));
+
+        Params transParams = params.get_scoped_params("translator");
+        // translator = dynamic_cast<Translator*>(loadSubComponent(
+        //             params.find<std::string>("translator"),
+        //             this, transParams));
+        translator = loadAnonymousSubComponent<Translator>(params.find<std::string>("translator"),"translator",0,
+                                                           ComponentInfo::SHARE_NONE, transParams, this);
+        if ( !translator ) dbg.fatal(CALL_INFO, 1, "Must specify a 'translator' subcomponent.");
+
+        configureNIC(0, params);
+        configureNIC(1, params);
+    }
+
+    ~Bridge()
+    {
+        for ( int i = 0 ; i < 2 ; i++ ) {
+            delete interfaces[i].nic;
+        }
+        delete translator;
+    }
+
+    void init(unsigned int phase)
+    {
+
+        bool ready = true;
+        for ( int i = 0 ; i < 2 ; i++ ) {
+            Nic_t &nic = interfaces[i];
+            nic.nic->init(phase);
+            ready &= nic.nic->isNetworkInitialized();
+        }
+
+        translator->init(phase);
+
+        dbg.debug(CALL_INFO, 10, 0, "Init Phase %u.  Network %sready\n", phase, ready ? "" : "NOT ");
+
+        if ( ! ready ) return;
+
+        for ( int i = 0 ; i < 2 ; i++ ) {
+            Nic_t &nic = interfaces[i];
+            Nic_t &otherNic = interfaces[i^1];
+
+
+            while ( SimpleNetwork::Request *req = nic.nic->recvInitData() ) {
+                dbg.debug(CALL_INFO, 2, 0, "Received init phase event on interface %d\n", i);
+                SimpleNetwork::Request *res = translator->initTranslate(req, i);
+                if ( res ) {
+                    otherNic.nic->sendInitData(res);
+                }
+            }
+        }
+    }
+
+    void setup(void)
+    {   
+        interfaces[0].nic->setup();
+        interfaces[1].nic->setup();
+    
+        translator->setup();
+    }
+
+    void finish(void)
+    {   
+        interfaces[0].nic->finish();
+        interfaces[1].nic->finish();
+    
+        translator->finish();
+    }
 
     SimpleNetwork::nid_t getAddrForNetwork(uint8_t netID) { return interfaces[netID].getAddr(); }
 
@@ -129,9 +197,75 @@ private:
 
     Translator *translator;
 
-    void configureNIC(uint8_t nic, SST::Params &params);
-    bool handleIncoming(int vn, uint8_t nic);
-    bool spaceAvailable(int vn, uint8_t nic);
+    void configureNIC(uint8_t id, SST::Params &params)
+    {
+        dbg.debug(CALL_INFO, 2, 0, "Initializing network interface %d\n", id);
+        Nic_t &nic = interfaces[id];
+
+        Params if_params;
+
+        if_params.insert("link_bw",params.find<std::string>("network_bw","80GiB/s"));
+        if_params.insert("input_buf_size",params.find<std::string>("network_input_buffer_size", "1KiB"));
+        if_params.insert("output_buf_size",params.find<std::string>("network_output_buffer_size", "1KiB"));
+        if_params.insert("port_name","network" + std::to_string(id));
+
+        nic.nic = loadAnonymousSubComponent<SST::Interfaces::SimpleNetwork>
+            ("merlin.linkcontrol", "networkIF", id,
+             ComponentInfo::SHARE_PORTS | ComponentInfo::INSERT_STATS, if_params, 1 /* vns */);
+
+
+        // nic.nic = (SimpleNetwork*)loadSubComponent("merlin.linkcontrol", this, params);
+        // nic.nic->initialize( "network" + std::to_string(id),
+        //         params.find<SST::UnitAlgebra>("network_bw", SST::UnitAlgebra("80GiB/s")),
+        //         1, /* should be num VN */
+        //         params.find<SST::UnitAlgebra>("network_input_buffer_size", SST::UnitAlgebra("1KiB")),
+        //         params.find<SST::UnitAlgebra>("network_output_buffer_size", SST::UnitAlgebra("1KiB")));
+
+        nic.nic->setNotifyOnReceive(new SimpleNetwork::Handler<Bridge, uint8_t>(this, &Bridge::handleIncoming, id));
+        nic.nic->setNotifyOnSend(new SimpleNetwork::Handler<Bridge, uint8_t>(this, &Bridge::spaceAvailable, id));
+
+        nic.stat_recv = registerStatistic<uint64_t>("pkts_received_net" + std::to_string(id));
+        nic.stat_send = registerStatistic<uint64_t>("pkts_sent_net" + std::to_string(id));
+    }
+
+    bool handleIncoming(int vn, uint8_t id)
+    {
+        Nic_t &inNIC = interfaces[id];
+        Nic_t &outNIC = interfaces[id^1];
+
+        SimpleNetwork::Request* req = inNIC.nic->recv(vn);
+
+        if ( NULL == req ) return false;
+        inNIC.stat_recv->addData(1);
+
+        dbg.debug(CALL_INFO, 5, 0, "Received event on interface %u\n", id);
+
+        SimpleNetwork::Request *res = translator->translate(req, id);
+        if ( res ) {
+            if ( outNIC.nic->send(res, 0) ) {
+                outNIC.stat_send->addData(1);
+            } else {
+                /* We failed to send. */
+                outNIC.sendQueue.push_back(res);
+            }
+        }
+        return true;
+    }
+
+    bool spaceAvailable(int vn, uint8_t id)
+    {
+        Nic_t &nic = interfaces[id];
+        while ( !nic.sendQueue.empty() ) {
+            if ( nic.nic->send(nic.sendQueue.front(), 0) ) {
+                nic.stat_send->addData(1);
+                nic.sendQueue.pop_front();
+            } else {
+                /* Not enough room yet.  */
+                break;
+            }
+        }
+        return false;
+    }
 
 };
 
