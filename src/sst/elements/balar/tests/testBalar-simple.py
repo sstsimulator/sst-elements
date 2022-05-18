79bb2fa2ad194ec5a3457f4c49a34150b32d9d5d
Weili An <an107@purdue.edu>
2022-05-18 11:51:11 -0400

    balar: copy original script for gpu cache config

--- src/sst/elements/balar/tests/testBalar-simple.py
+++ src/sst/elements/balar/tests/testBalar-simple.py
@@ -63,7 +63,7 @@ clock = "2GHz"
 mmio_addr = 1024
 
 
-# Define the simulation components
+# Test CPU components and mem hierachy
 cpu = sst.Component("cpu", "balar.BalarTestCPU")
 cpu.addParams({
       "opCount" : "1000",
@@ -166,17 +166,10 @@ memory.addParams({
       "mem_size" : "512MiB"
 })
 
-# Enable statistics
-sst.setStatisticLoadLevel(7)
-sst.setStatisticOutput("sst.statOutputConsole")
-# for a in componentlist:
-#     sst.enableAllStatisticsForComponentType(a)
-
-
-# Define the simulation links
+# Define the simulation links for CPU
 #          cpu/cpu_nic
 #                 |
-#  l1/l1_nic - chiprtr - mem_nic/mem
+#  l1/l1_nic - chiprtr - mem_nic/mem/mmio
 #
 link_cpu_rtr = sst.Link("link_cpu")
 link_cpu_rtr.connect( (cpu_nic, "port", "1000ps"), (chiprtr, "port0", "1000ps") )
@@ -189,3 +182,153 @@ link_mmio_rtr.connect( (mmio_nic, "port", "500ps"), (chiprtr, "port2", "500ps"))
 
 link_mem_rtr = sst.Link("link_mem")
 link_mem_rtr.connect( (mem_nic, "port", "1000ps"), (chiprtr, "port3", "1000ps") )
+
+# GPU Memory hierarchy
+#          mmio/GPU
+#           |
+#           L1
+#           |
+#           L2
+#           |
+#           mem port
+#           
+# GPU Memory hierarchy configuration
+print ("Configuring GPU Network-on-Chip...")
+gpu_router_ports = config.gpu_cores + config.gpu_l2_parts
+
+GPUrouter = sst.Component("gpu_xbar", "shogun.ShogunXBar")
+GPUrouter.addParams(config.getGPUXBarParams())
+GPUrouter.addParams({
+      "verbose" : 1,
+      "port_count" : gpu_router_ports,
+})
+
+# Connect Cores & l1caches to the router
+for next_core_id in range(config.gpu_cores):
+    print ("Configuring GPU core %d..."%next_core_id)
+
+    gpuPort = "requestGPUCacheLink%d"%next_core_id
+
+    l1g = sst.Component("l1gcache_%d"%(next_core_id), "memHierarchy.Cache")
+    l1g.addParams(config.getGPUL1Params())
+
+    connect("gpu_cache_link_%d"%next_core_id,
+            mmio, gpuPort,
+            l1g, "high_network_0",
+            config.default_link_latency).setNoCut()
+
+    connect("l1gcache_%d_link"%next_core_id,
+            l1g, "cache",
+            GPUrouter, "port%d"%next_core_id,
+            config.default_link_latency)
+
+# Connect GPU L2 caches to the routers
+num_L2s_per_stack = config.gpu_l2_parts // config.hbmStacks
+sub_mems = config.gpu_memory_controllers
+total_mems = config.hbmStacks * sub_mems
+
+interleaving = 256
+next_mem = 0
+cacheStartAddr = 0x00
+memStartAddr = 0x00
+
+if (config.gpu_l2_parts % total_mems) != 0:
+   print ("FAIL Number of L2s (%d) must be a multiple of the total number memory controllers (%d)."%(config.gpu_l2_parts, total_mems))
+   raise SystemExit
+
+# L2 and memory backend
+for next_group_id in range(config.hbmStacks):
+   next_cache = next_group_id * sub_mems
+
+   mem_l2_bus = sst.Component("mem_l2_bus_" + str(next_group_id), "memHierarchy.Bus")
+   mem_l2_bus.addParams({
+      "bus_frequency" : "4GHz",
+      "drain_bus" : "1"
+   })
+
+   for sub_group_id in range(sub_mems):
+      memStartAddr = 0 + (256 * next_mem)
+      endAddr = memStartAddr + config.gpu_memory_capacity_inB - (256 * total_mems)
+
+      if backend == "simple":
+         # Create SimpleMem
+         print ("Configuring Simple mem part" + str(next_mem) + " out of " + str(config.hbmStacks) + "...")
+         mem = sst.Component("Simplehbm_" + str(next_mem), "memHierarchy.MemController")
+         mem.addParams(config.get_GPU_simple_mem_params(total_mems, memStartAddr, endAddr))
+      elif backend == "ddr":
+         # Create DDR (Simple)
+         print ("Configuring DDR-Simple mem part" + str(next_mem) + " out of " + str(config.hbmStacks) + "...")
+         mem = sst.Component("DDR-shbm_" + str(next_mem), "memHierarchy.MemController")
+         mem.addParams(config.get_GPU_simple_ddr_params(total_mems, memStartAddr, endAddr, next_mem))
+      elif backend == "timing":
+         # Create DDR (Timing)
+         print ("Configuring DDR-Timing mem part" + str(next_mem) + " out of " + str(config.hbmStacks) + "...")
+         mem = sst.Component("DDR-thbm_" + str(next_mem), "memHierarchy.MemController")
+         mem.addParams(config.get_GPU_ddr_timing_params(total_mems, memStartAddr, endAddr, next_mem))
+      else :
+         # Create CramSim HBM
+         print ("Creating HBM controller " + str(next_mem) + " out of " + str(config.hbmStacks) + "...")
+
+         mem = sst.Component("GPUhbm_" + str(next_mem), "memHierarchy.MemController")
+         mem.addParams(config.get_GPU_hbm_memctrl_cramsim_params(total_mems, memStartAddr, endAddr))
+
+         cramsimBridge = sst.Component("hbm_cs_bridge_" + str(next_mem), "CramSim.c_MemhBridge")
+         cramsimCtrl = sst.Component("hbm_cs_ctrl_" + str(next_mem), "CramSim.c_Controller")
+         cramsimDimm = sst.Component("hbm_cs_dimm_" + str(next_mem), "CramSim.c_Dimm")
+
+         cramsimBridge.addParams(config.get_GPU_hbm_cramsim_bridge_params(next_mem))
+         cramsimCtrl.addParams(config.get_GPU_hbm_cramsim_ctrl_params(next_mem))
+         cramsimDimm.addParams(config.get_GPU_hbm_cramsim_dimm_params(next_mem))
+
+         linkMemBridge = sst.Link("memctrl_cramsim_link_" + str(next_mem))
+         linkMemBridge.connect( (mem, "cube_link", "2ns"), (cramsimBridge, "cpuLink", "2ns") )
+         linkBridgeCtrl = sst.Link("cramsim_bridge_link_" + str(next_mem))
+         linkBridgeCtrl.connect( (cramsimBridge, "memLink", "1ns"), (cramsimCtrl, "txngenLink", "1ns") )
+         linkDimmCtrl = sst.Link("cramsim_dimm_link_" + str(next_mem))
+         linkDimmCtrl.connect( (cramsimCtrl, "memLink", "1ns"), (cramsimDimm, "ctrlLink", "1ns") )
+
+      print (" - Capacity: " + str(config.gpu_memory_capacity_inB // config.hbmStacks) + " per HBM")
+      print (" - Start Address: " + str(hex(memStartAddr)) + " End Address: " + str(hex(endAddr)))
+
+      connect("bus_mem_link_%d"%next_mem,
+      mem_l2_bus, "low_network_%d"%sub_group_id,
+      mem, "direct_link",
+      "50ps").setNoCut()
+
+      next_mem = next_mem + 1
+
+   for next_mem_id in range(num_L2s_per_stack):
+      cacheStartAddr = 0 + (256 * next_cache)
+      endAddr = cacheStartAddr + config.gpu_memory_capacity_inB - (256 * total_mems)
+
+      print ("Creating L2 controller %d-%d (%d)..."%(next_group_id, next_mem_id, next_cache))
+      print (" - Start Address: " + str(hex(cacheStartAddr)) + " End Address: " + str(hex(endAddr)))
+
+      l2g = sst.Component("l2gcache_%d"%(next_cache), "memHierarchy.Cache")
+      l2g.addParams(config.getGPUL2Params(cacheStartAddr, endAddr))
+
+      connect("l2g_xbar_link_%d"%(next_cache),
+      GPUrouter, "port%d"%(config.gpu_cores+(next_cache)),
+      l2g, "cache",
+      config.default_link_latency).setNoCut()
+
+      connect("l2g_mem_link_%d"%(next_cache),
+      l2g, "low_network_0",
+      mem_l2_bus, "high_network_%d"%(next_mem_id),
+      config.default_link_latency).setNoCut()
+
+      print ("++ %d-%d (%d)..."%(next_cache, sub_mems, (next_cache + 1) % sub_mems))
+      if (next_cache + 1) % sub_mems == 0:
+         next_cache = next_cache + total_mems - (sub_mems - 1)
+      else:
+         next_cache = next_cache + 1
+
+
+
+# ===========================================================
+# Enable statistics
+sst.setStatisticLoadLevel(statLevel)
+sst.enableAllStatisticsForAllComponents({"type":"sst.AccumulatorStatistic"})
+sst.setStatisticOutput("sst.statOutputTXT", { "filepath" : statFile })
+
+print ("Completed configuring the cuda-test model")
