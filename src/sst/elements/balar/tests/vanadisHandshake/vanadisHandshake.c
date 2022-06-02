6b6a15328109c3d5705fffaa6ddfc16633550eb8
Weili An <an107@purdue.edu>
2022-06-02 14:45:19 -0400

    balar: Add testvanadis file

---
+++ src/sst/elements/balar/tests/vanadisHandshake/vanadisHandshake.c
@@ -0,0 +1,16 @@
+/**
+ * @file vanadisHandshake.c
+ * @author Weili An (an107@purdue.edu)
+ * @brief A simple program to test balar and vanadis handshake
+ *        by writing packet to the MMIO address of balar
+ * @version 0.1
+ * @date 2022-06-01
+ * 
+ */
+
+#include <stdio.h>
+
+int main( int argc, char* argv[] ) {
+	printf("Hello World from Vanadis\n");
+	return 0;
+}
