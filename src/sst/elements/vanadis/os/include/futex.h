cf8bac69d3e08589dfa0165c77aed8e02555ca51
mjleven <mjleven@sandia.gov>
2023-01-10 14:35:32 -0700

    make debug better

--- src/sst/elements/vanadis/os/include/futex.h
+++ src/sst/elements/vanadis/os/include/futex.h
@@ -22,6 +22,13 @@
 namespace SST {
 namespace Vanadis {
 
+
+#if 0
+#define OSFutexDbg( format, ... ) printf( "OS Futex::%s() " format, __func__, ##__VA_ARGS__ )
+#else
+#define OSFutexDbg( ... )
+#endif
+
 class VanadisSyscall;
 
 namespace OS {
@@ -29,32 +36,32 @@ namespace OS {
 class Futex {
 public:
     void addWait( uint64_t addr, VanadisSyscall* syscall ) {
-        printf("Futex::%s() addr=%#" PRIx64 " syscall=%p\n", __func__,addr,syscall );
+        OSFutexDbg("Futex::%s() addr=%#" PRIx64 " syscall=%p\n", addr,syscall );
 
         auto& tmp = m_futexMap[addr]; 
         assert( tmp.find( syscall ) == tmp.end() );
         tmp.insert( syscall );
-        printf("Futex::%s() %p %zu\n",__func__,this,tmp.size());
+        OSFutexDbg("Futex::%s() %p %zu\n",this,tmp.size());
     }
 
     size_t getNumWaiters( uint64_t addr ) {
-        printf("Futex::%s() addr=%#" PRIx64 "\n", __func__,addr );
+        OSFutexDbg("Futex::%s() addr=%#" PRIx64 "\n", addr );
         if ( m_futexMap.find( addr ) == m_futexMap.end() ) {
             return 0;
         }
         auto& tmp = m_futexMap[addr]; 
-        printf("Futex::%s() %p %zu\n",__func__,this,tmp.size());
+        OSFutexDbg("Futex::%s() %p %zu\n",this,tmp.size());
         return tmp.size();
     }
     VanadisSyscall* findWait( uint64_t addr ) {
-        printf("Futex::%s() addr=%#" PRIx64 "\n", __func__,addr );
+        OSFutexDbg("Futex::%s() addr=%#" PRIx64 "\n", addr );
         if (  m_futexMap.find( addr ) == m_futexMap.end() ) {
             return nullptr;
         }
         
         auto& tmp = m_futexMap[addr]; 
 
-        printf("Futex::%s() %p %zu\n",__func__,this,tmp.size());
+        OSFutexDbg("Futex::%s() %p %zu\n",this,tmp.size());
 
         assert( ! tmp.empty() );
         VanadisSyscall* syscall = *tmp.begin();
@@ -62,7 +69,7 @@ public:
         if ( tmp.empty() ) {
             m_futexMap.erase( addr );
         }
-        printf("Futex::%s() found addr=%#" PRIx64 " syscall=%p\n", __func__,addr,syscall );
+        OSFutexDbg("Futex::%s() found addr=%#" PRIx64 " syscall=%p\n", addr,syscall );
         return syscall;
     }
 
