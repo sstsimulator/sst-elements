352dab39da86cea6612f46fc9b1785ca7072813e
grvosku <grvosku@sandia.gov>
2023-04-13 11:00:50 -0600

    Copyright date update: CramSim-kingsley

--- src/sst/elements/ariel/tests/testIO/testio.cpp
+++ src/sst/elements/ariel/tests/testIO/testio.cpp
@@ -1,3 +1,18 @@
+// Copyright 2009-2023 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2023, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// of the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
 // This is a simple program that reads from stdin and writes to the specified output stream.
 // Specify -o to write to stdout and -e to write to stderr.
 #include <iostream>
