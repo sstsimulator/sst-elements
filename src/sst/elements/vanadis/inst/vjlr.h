5e990ae96ba37e935118a8b49ecff39c10481c6c
mjleven <mjleven@sandia.gov>
2023-05-23 14:05:08 -0600

    fix bug where imm was not added to jump

--- src/sst/elements/vanadis/inst/vjlr.h
+++ src/sst/elements/vanadis/inst/vjlr.h
@@ -67,6 +67,7 @@ public:
         const uint64_t link_value = calculateStandardNotTakenAddress();
 
         regFile->setIntReg<uint64_t>(phys_int_regs_out[0], link_value);
+        regFile->setIntReg<uint64_t>(phys_int_regs_in[0], jump_to);
 
 #ifdef VANADIS_BUILD_DEBUG
         output->verbose(CALL_INFO, 16, 0, "Execute JLR jump-to: 0x%0llx link-value: 0x%0llx\n", jump_to, link_value);
