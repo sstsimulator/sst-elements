eac4db7897d5351de19337d704504158bad3df12
kartiklakhotia <kartik.lakhotia@intel.com>
2023-05-24 17:01:30 -0700

    updating license to BSD-3

--- src/sst/elements/merlin/tests/polarstar_504_test.py
+++ src/sst/elements/merlin/tests/polarstar_504_test.py
@@ -1,22 +1,7 @@
 #!/usr/bin/env python
 
-# Copyright 2009-2022 NTESS. Under the terms
-# of Contract DE-NA0003525 with NTESS, the U.S.
-# Government retains certain rights in this software.
-#
-# Copyright (c) 2009-2022, NTESS
-# All rights reserved.
-#
-# Portions are copyright of other developers:
-# See the file CONTRIBUTORS.TXT in the top level directory
-# of the distribution for more information.
-#
-# This file is part of the SST software package. For license
-# information, see the LICENSE file in the top level directory of the
-# distribution.
-
 # Copyright 2023 Intel Corporation
-# SPDX-License-Identifier: MIT License
+# SPDX-License-Identifier: BSD-3-Clause
 
 # Authors: Kartik Lakhotia
 
