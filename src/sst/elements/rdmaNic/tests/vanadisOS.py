212221e9b38b6bc4db08b554a4369bbf8283616d
mjleven <mjleven@sandia.gov>
2023-08-24 10:28:11 -0600

    set reduce

--- src/sst/elements/rdmaNic/tests/vanadisOS.py
+++ src/sst/elements/rdmaNic/tests/vanadisOS.py
@@ -50,13 +50,15 @@ class Builder:
 
         if os.getenv("RDMANIC_IMB",False):
             self.nodeOS.addParams({
-                "process0.argc" : 7,
+                "process0.argc" : 6,
                 "process0.arg1" : "-iter",
                 "process0.arg2" : "1",
                 "process0.arg3" : "-msglen",
                 "process0.arg4" : "msglen.txt",
-                "process0.arg5" : "-exclude",
-                "process0.arg6" : "Reduce,Reduce_scatter",
+#                "process0.arg5" : "-exclude",
+#                "process0.arg5" : "-include",
+#                "process0.arg6" : "Reduce,Reduce_scatter",
+                "process0.arg5" : "Reduce",
             })
 
         self.mmu = self.nodeOS.setSubComponent( "mmu", "mmu.simpleMMU" )
