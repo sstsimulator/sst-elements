89d5af3eb183c4cb42e3ee31f7fe7d50f92876c7
Weili An <an107@purdue.edu>
2024-02-01 17:02:00 -0500

    balar-mmio: make a real vecadd summing sin^2 and cos^2

--- src/sst/elements/balar/tests/vanadisLLVMRISCV/vecadd.cu
+++ src/sst/elements/balar/tests/vanadisLLVMRISCV/vecadd.cu
@@ -1,34 +1,103 @@
+// Copyright 2009-2023 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2023, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// of the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-__global__
-void helloWorld(int* src, int N) {
-    for (int i = 0; i < N; i++) {
-        src[i] = i + i;
-    }
+// CUDA kernel. Each thread takes care of one element of c
+__global__ void vecAdd(double *a, double *b, double *c, int n)
+{
+    // Get our global thread ID
+    int id = blockIdx.x*blockDim.x+threadIdx.x;
+
+    // Make sure we do not go out of bounds
+    if (id < n)
+        c[id] = a[id] + b[id];
 }
 
-int main() {
-    int* src;
-    int* src_device;
-    int N = 32;
+int main()
+{
+    // Size of vectors
+    int n = 131072;
 
-    src = (int*)malloc(N * sizeof(int));
-    cudaMalloc(&src_device, N*sizeof(int));
+    // Host input vectors
+    double *h_a;
+    double *h_b;
+	// Host output vector
+    double *h_c;
 
-    for(int i = 0; i < N; i++) {
-        src[i] = i;
+    // Device input vectors
+    double *d_a;
+    double *d_b;
+    // Device output vector
+    double *d_c;
+
+    // Size, in bytes, of each vector
+    size_t bytes = n * sizeof(double);
+
+    // Allocate memory for each vector on host
+    h_a = (double *)malloc(bytes);
+    h_b = (double *)malloc(bytes);
+    h_c = (double *)malloc(bytes);
+
+    // Allocate memory for each vector on GPU
+    cudaMalloc(&d_a, bytes);
+    cudaMalloc(&d_b, bytes);
+    cudaMalloc(&d_c, bytes);
+
+    int i;
+    // Initialize vectors on host
+    for( i = 0; i < n; i++ ) {
+        h_a[i] = sin(i) * sin(i);
+        h_b[i] = cos(i) * cos(i);
     }
 
-    cudaMemcpy(src_device, src, N*sizeof(int), cudaMemcpyHostToDevice);
+    // Copy host vectors to device
+    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
 
-        helloWorld<<<1, 1, 1>>>(src_device, N);
+    int blockSize, gridSize;
 
-    cudaMemcpy(src, src_device, N*sizeof(int), cudaMemcpyDeviceToHost);
+    // Number of threads in each thread block
+    blockSize = 256;
 
-    for(int i = 0; i < N; i++) {
-        printf("src[%d] = %d\n", i, src[i]);
-    }
+    // Number of thread blocks in grid
+    gridSize = (int)ceil((float)n/blockSize);
+
+    // Execute the kernel
+    vecAdd<<<gridSize, blockSize>>>(d_a, d_b, d_c, n);
+
+    // Copy array back to host
+    cudaMemcpy( h_c, d_c, bytes, cudaMemcpyDeviceToHost );
+
+    // Sum up vector c and print result divided by n, this should equal 1 within error
+    double sum = 0;
+    for(i = 0; i < n; i++)
+        sum += h_c[i];
+    printf("final result: %.5f\n", sum / n);
+
+    // Release device memory
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    // Release host memory
+    free(h_a);
+    free(h_b);
+    free(h_c);
 
     return 0;
 }
