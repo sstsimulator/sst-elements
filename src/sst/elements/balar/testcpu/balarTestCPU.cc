97920868ab5cce06e401555094789d10f63edf57
Weili An <an107@purdue.edu>
2024-08-25 08:10:12 -0400

    balar-mmio: remove include to sst/core/simulation.h

--- src/sst/elements/balar/testcpu/balarTestCPU.cc
+++ src/sst/elements/balar/testcpu/balarTestCPU.cc
@@ -16,7 +16,6 @@
 #include <sst_config.h>
 
 #include <sst/core/params.h>
-#include <sst/core/simulation.h>
 #include <sst/core/interfaces/stringEvent.h>
 
 #include <sst/elements/memHierarchy/util.h>
