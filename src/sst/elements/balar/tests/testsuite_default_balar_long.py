66bab787615fc4d7ef184bd423acaaacf4d1172d
Weili An <an107@purdue.edu>
2024-09-24 14:12:54 -0400

    balar-mmio: create subdirectory for each testcase

--- src/sst/elements/balar/tests/testsuite_default_balar_long.py
+++ src/sst/elements/balar/tests/testsuite_default_balar_long.py
@@ -24,3 +24,7 @@ class testcase_balar_long(BalarTestCase):
     @BalarTestCase.balar_gpuapp_unittest
     def test_balar_vanadis_clang_rodinia_20_srad_v2_128x128(self):
         self.balar_vanadis_clang_template("rodinia-2.0-srad_v2-128x128", 80 * 60)
+        
+    @BalarTestCase.balar_gpuapp_unittest
+    def test_balar_vanadis_clang_rodinia_20_streamcluster_3_6_16_1024_1024_100_none_1(self):
+        self.balar_vanadis_clang_template("rodinia-2.0-streamcluster-3_6_16_1024_1024_100_none_1", 60 * 60)
