ca793a4acd06a82a8b4511e888bd2b862d75f456
Eric Berquist <ejberqu@sandia.gov>
2025-01-17 09:54:55 -0700

    unittest: replace assertEquals with assertEqual

--- src/sst/elements/memHierarchy/tests/testsuite_sweep_memHierarchy_dirSweepI.py
+++ src/sst/elements/memHierarchy/tests/testsuite_sweep_memHierarchy_dirSweepI.py
@@ -219,7 +219,7 @@ class testcase_memH_sweep_dirsweepI(SSTTestCase):
                     # Grep the ref file for the count of this line occuring
                     cmd = 'grep -c "{0}" {1}'.format(testline, reffile)
                     rtn = OSCommand(cmd).run()
-                    self.assertEquals(rtn.result(), 0, "Sweep dirsweepI Test failed running cmdline {0} - grepping reffile {1}".format(cmd, reffile))
+                    self.assertEqual(rtn.result(), 0, "Sweep dirsweepI Test failed running cmdline {0} - grepping reffile {1}".format(cmd, reffile))
                     refcount = int(rtn.output())
 
                     # Grep the out file for the count of this line occuring
@@ -233,7 +233,7 @@ class testcase_memH_sweep_dirsweepI(SSTTestCase):
                     log_debug("Testline='{0}'; refcount={1}; outcount={2}".format(testline, refcount, outcount))
 
                     # Compare the count
-                    self.assertEquals(outcount, refcount, "Sweep dirsweepI testing line '{0}': outfile count = {1} does not match reffile count = {2}".format(testline, outcount, refcount))
+                    self.assertEqual(outcount, refcount, "Sweep dirsweepI testing line '{0}': outfile count = {1} does not match reffile count = {2}".format(testline, outcount, refcount))
 
 ###############################################
 
