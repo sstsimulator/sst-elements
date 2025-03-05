5a95ba8192615f921b4ada305c3c54a655150c93
Kenneth Griesser <kgriesser@tclcomplabs.com>
2025-03-05 07:54:50 -0700

    subcomponent fix for issue #1228

--- src/sst/elements/simpleElementExample/basicSubComponent_subcomponent.h
+++ src/sst/elements/simpleElementExample/basicSubComponent_subcomponent.h
@@ -89,7 +89,11 @@ public:
 
     int compute( int num) override;
     std::string compute( std::string comp ) override;
+
+    // serialization
+    basicSubComponentIncrement();
     void serialize_order(SST::Core::Serialization::serializer& ser) override;
+    ImplementSerializable(SST::simpleElementExample::basicSubComponentIncrement);
 
 private:
     int amount;
@@ -119,7 +123,11 @@ public:
 
     int compute( int num) override;
     std::string compute( std::string comp ) override;
+
+    //  serialization
+    basicSubComponentDecrement();
     void serialize_order(SST::Core::Serialization::serializer& ser) override;
+    ImplementSerializable(SST::simpleElementExample::basicSubComponentDecrement);
 
 private:
     int amount;
@@ -147,7 +155,11 @@ public:
 
     int compute( int num) override;
     std::string compute( std::string comp ) override;
+
+    // Serialization
+    basicSubComponentMultiply();
     void serialize_order(SST::Core::Serialization::serializer& ser) override;
+    ImplementSerializable(SST::simpleElementExample::basicSubComponentMultiply);
 
 private:
     int amount;
@@ -175,7 +187,11 @@ public:
 
     int compute( int num) override;
     std::string compute( std::string comp ) override;
+
+    // Serialization
+    basicSubComponentDivide();
     void serialize_order(SST::Core::Serialization::serializer& ser) override;
+    ImplementSerializable(SST::simpleElementExample::basicSubComponentDivide);
 
 private:
     int amount;
