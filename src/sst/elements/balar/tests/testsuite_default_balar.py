0bd81312379d8005ce9e536ed481d9778faba78b
C. Hughes <chughes@sandia.gov>
2025-10-23 07:56:35 -0600

    Removing some tests

--- src/sst/elements/balar/tests/testsuite_default_balar.py
+++ src/sst/elements/balar/tests/testsuite_default_balar.py
@@ -8,27 +8,32 @@ import shutil
 
 from testBalar_testsuite_util import *
 
+## TODO
+## Commenting-out some of these tests temporarily
+## Should re-enable once the basic vectorAdd test is integrated
+##
 class testcase_balar_simple(BalarTestCase):
-    @BalarTestCase.balar_basic_unittest
-    def test_balar_runvecadd_testcpu(self):
-        self.balar_testcpu_template("vectorAdd")
-
-    @BalarTestCase.balar_basic_unittest
-    def test_balar_runvecadd_vanadis(self):
-        self.balar_vanadis_template("vanadisHandshake")
-
-    @BalarTestCase.balar_basic_unittest
-    def test_balar_vanadis_clang_helloworld(self):
-        self.balar_vanadis_clang_template("helloworld")
+    # @BalarTestCase.balar_basic_unittest
+    # def test_balar_runvecadd_testcpu(self):
+    #     self.balar_testcpu_template("vectorAdd")
+    #
+    # @BalarTestCase.balar_basic_unittest
+    # def test_balar_runvecadd_vanadis(self):
+    #     self.balar_vanadis_template("vanadisHandshake")
+    #
+    # @BalarTestCase.balar_basic_unittest
+    # def test_balar_vanadis_clang_helloworld(self):
+    #     self.balar_vanadis_clang_template("helloworld")
 
     @BalarTestCase.balar_basic_unittest
     def test_balar_vanadis_clang_vecadd(self):
         self.balar_vanadis_clang_template("vecadd", 60 * 30)
+    #
+    # @BalarTestCase.balar_gpuapp_unittest
+    # def test_balar_vanadis_clang_rodinia_20_backprop_short(self):
+    #     self.balar_vanadis_clang_template("rodinia-2.0-backprop-short", 60 * 30)
+    #
+    # @BalarTestCase.balar_gpuapp_unittest
+    # def test_balar_vanadis_clang_rodinia_20_bfs_SampleGraph(self):
+    #     self.balar_vanadis_clang_template("rodinia-2.0-bfs-SampleGraph")
 
-    @BalarTestCase.balar_gpuapp_unittest
-    def test_balar_vanadis_clang_rodinia_20_backprop_short(self):
-        self.balar_vanadis_clang_template("rodinia-2.0-backprop-short", 60 * 30)
-
-    @BalarTestCase.balar_gpuapp_unittest
-    def test_balar_vanadis_clang_rodinia_20_bfs_SampleGraph(self):
-        self.balar_vanadis_clang_template("rodinia-2.0-bfs-SampleGraph")
