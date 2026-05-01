88e359ac8f5cf73d74755538ab587a06446510b9
gvoskuilen <3968723+gvoskuilen@users.noreply.github.com>
2026-04-30 18:43:17 -0600

    Copyright updates for carcosa (#2672)

--- src/sst/elements/carcosa/tests/pingpong.c
+++ src/sst/elements/carcosa/tests/pingpong.c
@@ -1,3 +1,18 @@
+// Copyright 2009-2026 NTESS. Under the terms
+// of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Copyright (c) 2009-2026, NTESS
+// All rights reserved.
+//
+// Portions are copyright of other developers:
+// See the file CONTRIBUTORS.TXT in the top level directory
+// of the distribution for more information.
+//
+// This file is part of the SST software package. For license
+// information, see the LICENSE file in the top level directory of the
+// distribution.
+
 /*
  * Ping-pong executable for Vanadis + Hali MMIO coordination.
  * Uses hyades.h for all Vanadis control logic (MMIO and run loop).
